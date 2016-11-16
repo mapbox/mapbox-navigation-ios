5415735e057fa1de262e37874f3c0cdc96bbd8a2
bsudekum <bobby@mapbox.com>
2016-11-16 15:06:48 -0800

    readd

---
+++ MapboxNavigation/Constants.swift
@@ -0,0 +1,37 @@
+//
+//  Constants.swift
+//  MapboxNavigation
+//
+//  Created by Bobby Sudekum on 11/16/16.
+//  Copyright © 2016 Mapbox. All rights reserved.
+//
+
+import Foundation
+import CoreLocation
+
+public let NavigationControllerProgressDidChangeNotificationProgressKey = "progress"
+public let NavigationControllerProgressDidChangeNotificationSecondsRemainingOnStepKey = "seconds"
+
+public let NavigationControllerNotificationApproachingIncident = "incident"
+public let NavigationControllerAlertLevelDidChangeNotificationRouteProgressKey = "progress"
+public let NavigationControllerAlertLevelDidChangeNotificationDistanceToEndOfManeuverKey = "distance"
+public let NavigationControllerAlertLevelDidChangeNotificationDistanceToIncidentKey = "distance"
+
+public let NavigationControllerMaximumMetersBeforeRecalculating: CLLocationDistance = 50
+public let NavigationControllerMaximumAllowedDegreeOffsetForTurnCompletion: Double = 30
+
+// Alert distances
+public let NavigationControllerMediumAlertNumberOfSeconds: Double = 70
+public let NavigationControllerHighAlertNumberOfSeconds: Double = 15
+public let NavigationControllerManeuverZoneRadius: CLLocationDistance = 40
+
+public let NavigationControllerMinimumDistanceForMediumAlert: CLLocationDistance = 400
+public let NavigationControllerMinimumDistanceForHighAlert: CLLocationDistance = 100
+
+public let NavigationControllerDeadReckoningTimeInterval:TimeInterval = 1.0
+
+public struct NavigationControllerNotification {
+    static public let progressDidChange = Notification.Name("NavigationControllerProgressDidChange")
+    static public let alertLevelDidChange = Notification.Name("NavigationControllerAlertLevelDidChange")
+    static public let rerouted = Notification.Name("NavigationControllerShouldRerouted")
+}
