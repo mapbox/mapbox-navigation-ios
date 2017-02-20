0bc9fa2b7b7e59e275aeff7364d3aeab29d63cd6
Fredrik Karlsson <bjorn.fredrik.karlsson@gmail.com>
2017-02-20 14:18:29 -0800

    Removed unused code

--- UI/MapboxNavigationUI/RouteStepFormatter.swift
+++ UI/MapboxNavigationUI/RouteStepFormatter.swift
@@ -6,27 +6,11 @@ class RouteStepFormatter: Formatter {
     let instructions = OSRMInstructionFormatter(version: "v5")
     
     override func string(for obj: Any?) -> String? {
-        return string(for: obj, markUpWithSSML: false)
-    }
-    
-    func string(for obj: Any?, markUpWithSSML: Bool) -> String? {
         guard let step = obj as? RouteStep else {
             return nil
         }
         
-        guard markUpWithSSML else {
-            return instructions.string(for: step)
-        }
-        
-        return instructions.string(for: step, modifyValueByKey: { (key, value) -> String in
-            switch key {
-            case .wayName, .destination, .rotaryName:
-                return "<say-as interpret-as=\"address\">\(value.addingXMLEscapes)</say-as>"
-            default:
-                return value
-            }
-        })
-
+        return instructions.string(for: step)
     }
     
     override func getObjectValue(_ obj: AutoreleasingUnsafeMutablePointer<AnyObject?>?, for string: String, errorDescription error: AutoreleasingUnsafeMutablePointer<NSString?>?) -> Bool {
