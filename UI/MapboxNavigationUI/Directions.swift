2327b8146cbdec30bc855fbc2558bac9eb34eb54
Fredrik Karlsson <bjorn.fredrik.karlsson@gmail.com>
2017-02-20 12:37:43 -0800

    Switched to Carthage

--- UI/MapboxNavigationUI/Directions.swift
+++ UI/MapboxNavigationUI/Directions.swift
@@ -4,12 +4,12 @@ import MapboxDirections
 import UIKit
 
 extension RouteOptions {
-    class func preferredOptions(from origin: CLLocationCoordinate2D, to destination: CLLocationCoordinate2D, heading: CLLocationDirection? = nil, profileIdentifier: String? = nil) -> RouteOptions {
+    class func preferredOptions(from origin: CLLocationCoordinate2D, to destination: CLLocationCoordinate2D, heading: CLLocationDirection? = nil, profileIdentifier: MBDirectionsProfileIdentifier? = nil) -> RouteOptions {
         let options = RouteOptions(coordinates: [origin, destination])
         
         options.includesSteps = true
         options.routeShapeResolution = .full
-        options.profileIdentifier = profileIdentifier ?? MBDirectionsProfileIdentifierAutomobileAvoidingTraffic
+        options.profileIdentifier = profileIdentifier ?? .automobileAvoidingTraffic
         
         if let heading = heading, heading >= 0, let firstWaypoint = options.waypoints.first {
             firstWaypoint.heading = heading
