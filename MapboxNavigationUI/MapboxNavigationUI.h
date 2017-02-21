720fa48321a3427cc27e1ae7dbff2fcc302747a6
Fredrik Karlsson <bjorn.fredrik.karlsson@gmail.com>
2017-02-20 17:57:46 -0800

    Merged UI into Nav proj

---
+++ MapboxNavigationUI/MapboxNavigationUI.h
@@ -0,0 +1,10 @@
+#import <UIKit/UIKit.h>
+
+//! Project version number for MapboxNavigationUI.
+FOUNDATION_EXPORT double MapboxNavigationUIVersionNumber;
+
+//! Project version string for MapboxNavigationUI.
+FOUNDATION_EXPORT const unsigned char MapboxNavigationUIVersionString[];
+
+
+
