e13d15f22b9a187632459054ec556e93aede3659
Fredrik Karlsson <bjorn.fredrik.karlsson@gmail.com>
2017-02-21 10:31:42 -0800

    Remove bundle

--- MapboxNavigationUI/Bundle.swift
+++ MapboxNavigationUI/Bundle.swift
@@ -2,11 +2,7 @@ import Foundation
 
 extension Bundle {
     class var navigationUI: Bundle {
-        get {
-            let bundle = Bundle(for: RouteViewController.self)
-            let resourceBundlePath = "\(bundle.bundlePath)/MapboxNavigationUI.bundle"
-            return Bundle(path: resourceBundlePath)!
-        }
+        get { return Bundle(for: NavigationUI.self) }
     }
     
     var backgroundModeLocationSupported: Bool {
