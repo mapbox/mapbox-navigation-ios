46b6723079ea1710c25929b793df09cea5696750
Bobby Sudekum <bobby@mapbox.com>
2017-04-06 12:38:43 -0700

    Make route line more configurable (#116)

--- MapboxNavigationUI/MGLMapView.swift
+++ MapboxNavigationUI/MGLMapView.swift
@@ -12,61 +12,6 @@ let arrowLayerIdentifier = "arrowLayer"
 
 extension MGLMapView {
     
-    public func annotate(_ route: Route) {
-        guard var coordinates = route.coordinates else {
-            return
-        }
-        
-        guard let style = style else {
-            return
-        }
-        
-        if let line = style.layer(withIdentifier: routeLayerIdentifier) {
-            style.removeLayer(line)
-        }
-        
-        if let lineCasing = style.layer(withIdentifier: routeLayerCasingIdentifier) {
-            style.removeLayer(lineCasing)
-        }
-        
-        if let source = style.source(withIdentifier: sourceIdentifier) {
-            style.removeSource(source)
-        }
-        
-        let polyline = MGLPolylineFeature(coordinates: &coordinates, count: route.coordinateCount)
-        
-        if let source = style.source(withIdentifier: sourceIdentifier) as? MGLShapeSource {
-            source.shape = polyline
-        } else {
-            let geoJSONSource = MGLShapeSource(identifier: sourceIdentifier, shape: polyline, options: nil)
-            let line = MGLLineStyleLayer(identifier: routeLayerIdentifier, source: geoJSONSource)
-            let lineCasing = MGLLineStyleLayer(identifier: routeLayerCasingIdentifier, source: geoJSONSource)
-            
-            line.lineColor = MGLStyleValue(rawValue: NavigationUI.shared.tintStrokeColor.withAlphaComponent(0.6))
-            line.lineWidth = MGLStyleValue(rawValue: 5)
-            lineCasing.lineColor = MGLStyleValue(rawValue: NavigationUI.shared.tintStrokeColor)
-            lineCasing.lineWidth = MGLStyleValue(rawValue: 9)
-            
-            let cap = NSValue(mglLineCap: .round)
-            let join = NSValue(mglLineJoin: .round)
-            
-            line.lineCap = MGLStyleValue(rawValue: cap)
-            line.lineJoin = MGLStyleValue(rawValue: join)
-            lineCasing.lineCap = MGLStyleValue(rawValue: cap)
-            lineCasing.lineJoin = MGLStyleValue(rawValue: join)
-            
-            style.addSource(geoJSONSource)
-            for layer in style.layers.reversed() {
-                if let layer = layer as? MGLStyleLayer, !(layer is MGLSymbolStyleLayer) &&
-                    layer.identifier != arrowLayerIdentifier && layer.identifier != arrowSourceIdentifier {
-                    style.insertLayer(line, above: layer)
-                    style.insertLayer(lineCasing, below: line)
-                    return
-                }
-            }
-        }
-    }
-    
     public var showsTraffic: Bool {
         get {
             if let style = style {
--- MapboxNavigationUI/NavigationMapView.swift
+++ MapboxNavigationUI/NavigationMapView.swift
@@ -1,10 +1,11 @@
 import Foundation
+import MapboxDirections
 
-public class NavigationMapView: MGLMapView {
+open class NavigationMapView: MGLMapView {
     
     var navigationMapDelegate: NavigationMapViewDelegate?
     
-    public override func locationManager(_ manager: CLLocationManager!, didUpdateLocations locations: [Any]!) {
+    open override func locationManager(_ manager: CLLocationManager!, didUpdateLocations locations: [Any]!) {
         guard let location = locations.first as? CLLocation else { return }
         
         if let modifiedLocation = navigationMapDelegate?.navigationMapView(self, shouldUpdateTo: location) {
@@ -12,5 +13,87 @@ public class NavigationMapView: MGLMapView {
         } else {
             super.locationManager(manager, didUpdateLocations: locations)
         }
+    }
+    
+    /**
+     Annotates the map with a route line.
+     */
+    open func annotate(_ route: Route) {
+        guard let style = style else {
+            return
+        }
+        
+        if let line = style.layer(withIdentifier: routeLayerIdentifier) {
+            style.removeLayer(line)
+        }
+        
+        if let lineCasing = style.layer(withIdentifier: routeLayerCasingIdentifier) {
+            style.removeLayer(lineCasing)
+        }
+        
+        if let source = style.source(withIdentifier: sourceIdentifier) {
+            style.removeSource(source)
+        }
+        
+        let polyline = shape(describing: route)
+        
+        if let source = style.source(withIdentifier: sourceIdentifier) as? MGLShapeSource {
+            source.shape = polyline
+        } else {
+            let lineSource = MGLShapeSource(identifier: sourceIdentifier, shape: polyline, options: nil)
+            style.addSource(lineSource)
+            
+            let line = routeStyleLayer(identifier: routeLayerIdentifier, source: lineSource)
+            let lineCasing = routeCasingStyleLayer(identifier: routeLayerCasingIdentifier, source: lineSource)
+            
+            for layer in style.layers.reversed() {
+                if !(layer is MGLSymbolStyleLayer) &&
+                    layer.identifier != arrowLayerIdentifier && layer.identifier != arrowSourceIdentifier {
+                    style.insertLayer(line, above: layer)
+                    style.insertLayer(lineCasing, below: line)
+                    return
+                }
+            }
+        }
+    }
+    
+    open func shape(describing route: Route) -> MGLShape? {
+        guard var coordinates = route.coordinates else {
+            return nil
+        }
+        
+        return MGLPolylineFeature(coordinates: &coordinates, count: route.coordinateCount)
+    }
+    
+    /**
+     Function for overriding the default route line style.
+     */
+    open func routeStyleLayer(identifier: String, source: MGLSource) -> MGLStyleLayer {
+        
+        let line = MGLLineStyleLayer(identifier: identifier, source: source)
+        
+        line.lineColor = MGLStyleValue(rawValue: NavigationUI.shared.tintStrokeColor.withAlphaComponent(0.6))
+        line.lineWidth = MGLStyleValue(rawValue: 5)
+        
+        line.lineCap = MGLStyleValue(rawValue: NSValue(mglLineCap: .round))
+        line.lineJoin = MGLStyleValue(rawValue: NSValue(mglLineJoin: .round))
+        
+        return line
+    }
+    
+    /**
+     Function for overriding the default route line casing style.
+     */
+    open func routeCasingStyleLayer(identifier: String, source: MGLSource) -> MGLStyleLayer {
+        
+        let lineCasing = MGLLineStyleLayer(identifier: identifier, source: source)
+        
+        lineCasing.lineColor = MGLStyleValue(rawValue: NavigationUI.shared.tintStrokeColor)
+        lineCasing.lineWidth = MGLStyleValue(rawValue: 9)
+        
+        lineCasing.lineCap = MGLStyleValue(rawValue: NSValue(mglLineCap: .round))
+        lineCasing.lineJoin = MGLStyleValue(rawValue: NSValue(mglLineJoin: .round))
+        
+        return lineCasing
     }
 }
