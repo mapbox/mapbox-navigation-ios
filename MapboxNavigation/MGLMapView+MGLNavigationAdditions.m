f9fbfd77af77f6698df751d301144637fa9afdca
Bobby Sudekum <bobby@mapbox.com>
2017-05-01 15:55:11 -0700

    Comment

--- MapboxNavigation/MGLMapView+MGLNavigationAdditions.m
+++ MapboxNavigation/MGLMapView+MGLNavigationAdditions.m
@@ -2,6 +2,7 @@
 
 #pragma clang diagnostic push
 #pragma clang diagnostic ignored "-Wincomplete-implementation"
+// Supressing compiler warning until https://github.com/mapbox/mapbox-gl-native/issues/6867 is implemented
 @implementation MGLMapView (MGLNavigationAdditions)
 #pragma clang diagnostic pop
 
