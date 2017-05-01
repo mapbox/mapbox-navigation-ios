da8f28e69cee472896cb87b4389f56f537184b1f
Bobby Sudekum <bobby@mapbox.com>
2017-05-01 16:02:46 -0700

    Silence locationManager warning (#190)

--- MapboxNavigation/MGLMapView+MGLNavigationAdditions.m
+++ MapboxNavigation/MGLMapView+MGLNavigationAdditions.m
@@ -1,6 +1,10 @@
 #import "MGLMapView+MGLNavigationAdditions.h"
 
+#pragma clang diagnostic push
+#pragma clang diagnostic ignored "-Wincomplete-implementation"
+// Supressing compiler warning until https://github.com/mapbox/mapbox-gl-native/issues/6867 is implemented
 @implementation MGLMapView (MGLNavigationAdditions)
+#pragma clang diagnostic pop
 
 @dynamic locationManager;
 
