cc09cfec249f920d617d698d0df5d74ef4a356f7
Bobby Sudekum <bobby@mapbox.com>
2017-05-23 14:35:22 -0700

    Correctly draw uturns

--- MapboxNavigation/TurnArrowView.swift
+++ MapboxNavigation/TurnArrowView.swift
@@ -93,6 +93,7 @@ public class TurnArrowView: UIView {
                 flip = true
             case .uTurn:
                 StyleKitArrows.drawArrow180(scale: scale)
+                flip = [.left, .slightLeft, .sharpLeft].contains(ManeuverDirection(angle: angle))
             default:
                 StyleKitArrows.drawArrow0(scale: scale)
             }
