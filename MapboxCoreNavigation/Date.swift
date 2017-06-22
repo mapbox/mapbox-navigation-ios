530ae770c434effcd33b3f32ace6cbbcb0d06151
Bobby Sudekum <bobby@mapbox.com>
2017-06-22 17:31:12 +0200

    add timezone

--- MapboxCoreNavigation/Date.swift
+++ MapboxCoreNavigation/Date.swift
@@ -2,7 +2,7 @@ extension Date {
     var ISO8601: String {
         let formatter = DateFormatter()
         formatter.dateFormat = "yyyy-MM-dd'T'HH:mm:ss.SSSZZZZZ"
-        formatter.timeZone = TimeZone(secondsFromGMT: 0)
+        formatter.timeZone = TimeZone(abbreviation: TimeZone.current.abbreviation() ?? "GMT")
         return formatter.string(from: self)
     }
 }
