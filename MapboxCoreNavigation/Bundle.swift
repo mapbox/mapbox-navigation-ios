da79d0bbadb4e99f6ca055c28fa13a8ded68a1d9
Fredrik Karlsson <bjorn.fredrik.karlsson@gmail.com>
2017-07-07 15:38:17 +0200

    Fix location permissions and adopt iOS 11 description

--- MapboxCoreNavigation/Bundle.swift
+++ MapboxCoreNavigation/Bundle.swift
@@ -1,12 +1,31 @@
 import Foundation
 
 extension Bundle {
+    
     var backgroundModeLocationSupported: Bool {
         get {
-            if let modes = Bundle.main.object(forInfoDictionaryKey: "UIBackgroundModes") as? [String] {
+            if let modes = object(forInfoDictionaryKey: "UIBackgroundModes") as? [String] {
                 return modes.contains("location")
             }
             return false
+        }
+    }
+    
+    var locationAlwaysAndWhenInUseUsageDescription: String? {
+        get {
+            return object(forInfoDictionaryKey: "NSLocationAlwaysAndWhenInUseUsageDescription") as? String
+        }
+    }
+    
+    var locationWhenInUseUsageDescription: String? {
+        get {
+            return object(forInfoDictionaryKey: "NSLocationWhenInUseUsageDescription") as? String
+        }
+    }
+    
+    var locationAlwaysUsageDescription: String? {
+        get {
+            return object(forInfoDictionaryKey: "NSLocationAlwaysUsageDescription") as? String
         }
     }
 }
