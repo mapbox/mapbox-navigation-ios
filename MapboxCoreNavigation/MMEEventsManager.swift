1f2ac20da2dcad811b3245bfc58abeed8428825f
Eric Wolfe <eric.r.wolfe@gmail.com>
2017-07-25 13:57:57 -0700

    Add audio type to telemetry events (#420)

--- MapboxCoreNavigation/MMEEventsManager.swift
+++ MapboxCoreNavigation/MMEEventsManager.swift
@@ -4,7 +4,7 @@ import AVFoundation
 import MapboxMobileEvents
 
 let SecondsBeforeCollectionAfterFeedbackEvent: TimeInterval = 20
-let EventVersion = 4
+let EventVersion = 5
 
 struct EventDetails {
     var originalRequestIdentifier: String?
@@ -30,6 +30,7 @@ struct EventDetails {
     var durationRemaining: TimeInterval
     var rerouteCount: Int
     var volumeLevel: Int
+    var audioType: String
     var screenBrightness: Int
     var batteryPluggedIn: Bool
     var batteryLevel: Float
@@ -75,6 +76,7 @@ struct EventDetails {
         rerouteCount = session.numberOfReroutes
         
         volumeLevel = Int(AVAudioSession.sharedInstance().outputVolume * 100)
+        audioType = AVAudioSession.sharedInstance().audioType
         screenBrightness = Int(UIScreen.main.brightness * 100)
         
         batteryPluggedIn = UIDevice.current.batteryState == .charging || UIDevice.current.batteryState == .full
@@ -127,12 +129,13 @@ struct EventDetails {
         modifiedEventDictionary["rerouteCount"] = rerouteCount
         
         modifiedEventDictionary["volumeLevel"] = volumeLevel
+        modifiedEventDictionary["audioType"] = audioType
         modifiedEventDictionary["screenBrightness"] = screenBrightness
         
         modifiedEventDictionary["batteryPluggedIn"] = batteryPluggedIn
         modifiedEventDictionary["batteryLevel"] = batteryLevel
         modifiedEventDictionary["applicationState"] = applicationState.telemetryString
-        
+
         return modifiedEventDictionary
     }
 }
@@ -215,6 +218,48 @@ extension UIApplicationState {
                 return "Background"
             }
         }
+    }
+}
+
+extension AVAudioSession {
+    var audioType: String {
+        if isOutputBluetooth() {
+            return "bluetooth"
+        }
+        if isOutputHeadphones() {
+            return "headphones"
+        }
+        if isOutputSpeaker() {
+            return "speaker"
+        }
+        return "unknown"
+    }
+    
+    func isOutputBluetooth() -> Bool{
+        for output in currentRoute.outputs {
+            if [AVAudioSessionPortBluetoothA2DP, AVAudioSessionPortBluetoothLE].contains(output.portType) {
+                return true
+            }
+        }
+        return false
+    }
+    
+    func isOutputHeadphones() -> Bool{
+        for output in currentRoute.outputs {
+            if [AVAudioSessionPortHeadphones, AVAudioSessionPortAirPlay, AVAudioSessionPortHDMI, AVAudioSessionPortLineOut].contains(output.portType) {
+                return true
+            }
+        }
+        return false
+    }
+    
+    func isOutputSpeaker() -> Bool{
+        for output in currentRoute.outputs {
+            if [AVAudioSessionPortBuiltInSpeaker, AVAudioSessionPortBuiltInReceiver].contains(output.portType) {
+                return true
+            }
+        }
+        return false
     }
 }
 
