4c5c4d8675da3d5b34219892195b2012f1035cfa
Bobby Sudekum <bobby@mapbox.com>
2017-08-03 10:20:44 -0700

    move

--- MapboxCoreNavigation/Date.swift
+++ MapboxCoreNavigation/Date.swift
@@ -1,13 +1,13 @@
 import Foundation
 
 extension Date {
-    public var ISO8601: String {
+    var ISO8601: String {
         return DateFormatter.ISO8601.string(from: self)
     }
 }
 
 extension DateFormatter {
-    public class var ISO8601: DateFormatter {
+    class var ISO8601: DateFormatter {
         let formatter = DateFormatter()
         formatter.locale = Locale(identifier: "en_US_POSIX")
         formatter.dateFormat = "yyyy-MM-dd'T'HH:mm:ss.SSSZ"
