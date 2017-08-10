5907c9ad945ef8aa9cbede8422e94ef006e560a8
Bobby Sudekum <bobby@mapbox.com>
2017-08-10 10:18:46 -0700

    Add translation

--- MapboxNavigation/RouteTableViewController.swift
+++ MapboxNavigation/RouteTableViewController.swift
@@ -81,7 +81,7 @@ extension RouteTableViewController: UITableViewDelegate, UITableViewDataSource {
         let legToFrom = leg.name.components(separatedBy: ", ")
         
         if let destinationName = destinationName?.nonEmptyString, legToFrom.count > 1 {
-            return String.localizedStringWithFormat(NSLocalizedString("WAYPOINT_DESTINATION_VIA_WAYPOINTS_FORMAT", bundle: .mapboxNavigation, value: "%@, via %@", comment: "Format for displaying destination and intermediate waypoints; 1 = source ; 2 = destinations"), destinationName, leg.name.replacingOccurrences(of: ", ", with: " and "))
+            return String.localizedStringWithFormat(NSLocalizedString("WAYPOINT_DESTINATION_VIA_WAYPOINTS_FORMAT", bundle: .mapboxNavigation, value: "%@, via %@", comment: "Format for displaying destination and intermediate waypoints; 1 = source ; 2 = destinations"), destinationName, leg.name.replacingOccurrences(of: ", ", with: " \(String.localizedStringWithFormat(NSLocalizedString("AND", bundle: .mapboxNavigation, value: "and", comment: "Format string for and;"))) "))
         } else if let sourceName = sourceName?.nonEmptyString, let destinationName = destinationName?.nonEmptyString {
             return String.localizedStringWithFormat(NSLocalizedString("WAYPOINT_SOURCE_DESTINATION_FORMAT", bundle: .mapboxNavigation, value: "%@ and %@", comment: "Format for displaying start and endpoint for leg; 1 = source ; 2 = destination"), sourceName, destinationName)
         } else {
