2f0aba4680c63eaf6b7ab71f2ff52efdb255c8c5
Fredrik Karlsson <bjorn.fredrik.karlsson@gmail.com>
2017-08-14 17:24:50 +0200

    Fix storyboard warnings

--- MapboxNavigation/Style.swift
+++ MapboxNavigation/Style.swift
@@ -222,6 +222,7 @@ open class Style: NSObject {
             ProgressBar.appearance(for: traitCollection).barColor = color
             Button.appearance(for: traitCollection).tintColor = color
             HighlightedButton.appearance(for: traitCollection).setTitleColor(color, for: .normal)
+            ResumeButton.appearance(for: traitCollection).tintColor = color
         }
         
         if let color = buttonTextColor {
@@ -386,6 +387,54 @@ public class LanesView: UIView { }
  */
 @objc(MBHighlightedButton)
 public class HighlightedButton: Button { }
+
+@IBDesignable
+@objc(MBResumeButton)
+public class ResumeButton: UIControl {
+    public override var tintColor: UIColor! {
+        didSet {
+            imageView.tintColor = tintColor
+            titleLabel.textColor = tintColor
+        }
+    }
+    
+    let imageView = UIImageView(image: UIImage(named: "location", in: .mapboxNavigation, compatibleWith: nil)!.withRenderingMode(.alwaysTemplate))
+    let titleLabel = UILabel()
+    
+    public override init(frame: CGRect) {
+        super.init(frame: frame)
+        commonInit()
+    }
+    
+    public required init?(coder aDecoder: NSCoder) {
+        super.init(coder: aDecoder)
+        commonInit()
+    }
+    
+    public override func prepareForInterfaceBuilder() {
+        super.prepareForInterfaceBuilder()
+        commonInit()
+    }
+    
+    func commonInit() {
+        titleLabel.text = NSLocalizedString("RESUME", bundle: .mapboxNavigation, value: "Resume", comment: "Button title for resume tracking")
+        titleLabel.sizeToFit()
+        addSubview(imageView)
+        addSubview(titleLabel)
+        
+        titleLabel.translatesAutoresizingMaskIntoConstraints = false
+        imageView.translatesAutoresizingMaskIntoConstraints = false
+        translatesAutoresizingMaskIntoConstraints = false
+        
+        let views = ["label": titleLabel, "imageView": imageView]
+        addConstraints(NSLayoutConstraint.constraints(withVisualFormat: "H:|-8-[imageView]-8-[label]-8-|", options: [], metrics: nil, views: views))
+        addConstraints(NSLayoutConstraint.constraints(withVisualFormat: "V:|->=12-[imageView]->=12-|", options: [], metrics: nil, views: views))
+        addConstraints(NSLayoutConstraint.constraints(withVisualFormat: "V:|->=12-[label]->=12-|", options: [], metrics: nil, views: views))
+        setNeedsUpdateConstraints()
+        
+        applyDefaultCornerRadiusShadow()
+    }
+}
 
 /// :nodoc:
 @objc(MBStylableLabel)
