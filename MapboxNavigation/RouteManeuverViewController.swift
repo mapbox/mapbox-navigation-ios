4290de466f3d01c62c26470762dfe0bdf414fad0
Minh Nguyễn <mxn@1ec5.org>
2017-08-23 02:43:17 -0700

    Added Spanish cardinal directions

--- MapboxNavigation/RouteManeuverViewController.swift
+++ MapboxNavigation/RouteManeuverViewController.swift
@@ -54,7 +54,7 @@ class RouteManeuverViewController: UIViewController {
                 return
             }
             
-            if components.count == 2 || (components.count == 3 && ["North", "South", "East", "West", "Nord", "Sud", "Est", "Ouest"].contains(components[2])) {
+            if components.count == 2 || (components.count == 3 && ["North", "South", "East", "West", "Nord", "Sud", "Est", "Ouest", "Norte", "Sur", "Este", "Oeste"].contains(components[2])) {
                 shieldAPIDataTask = dataTaskForShieldImage(network: components[0], number: components[1], height: 32 * UIScreen.main.scale) { [weak self] (image) in
                     self?.shieldImage = image
                 }
