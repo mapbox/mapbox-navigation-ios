d9d407cc7ca14655a7110f8bc87c8e817298cc32
Bobby Sudekum <bobby@mapbox.com>
2017-08-26 09:13:33 -0700

    Reset floating button color properly

--- MapboxNavigation/DefaultStyle.swift
+++ MapboxNavigation/DefaultStyle.swift
@@ -68,6 +68,7 @@ open class DefaultStyle: Style {
         laneViewSecondaryColor = .defaultLaneArrowSecondary
         
         floatingButtonBackgroundColor = #colorLiteral(red: 1, green: 1, blue: 1, alpha: 1)
+        floatingButtonTintColor = tintColor
         lanesViewBackgroundColor = #colorLiteral(red: 0.968627451, green: 0.968627451, blue: 0.968627451, alpha: 1)
         
         // Maneuver view (Page view)
