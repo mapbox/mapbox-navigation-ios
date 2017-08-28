f03b07cb74c91fe0ab7b1d25da4eb3e2b560e353
Bobby Sudekum <bobby@mapbox.com>
2017-08-28 09:09:57 -0400

    Super

--- MapboxNavigation/RouteTableViewController.swift
+++ MapboxNavigation/RouteTableViewController.swift
@@ -28,6 +28,7 @@ class RouteTableViewController: UIViewController {
     }
     
     override func viewDidLoad() {
+        super.viewDidLoad()
         defaultTimeRemainingColor = headerView.timeRemaining.textColor
     }
     
