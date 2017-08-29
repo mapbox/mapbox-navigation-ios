0237c8ea898a1ec8333de7bec2076d6a27270283
Bobby Sudekum <bobby@mapbox.com>
2017-08-29 12:24:34 -0400

    Subclass route options (#531)

---
+++ MapboxCoreNavigation/NavigationRouteOptions.swift
@@ -0,0 +1,50 @@
+import Foundation
+import MapboxDirections
+
+/**
+ A `NavigationRouteOptions` object specifies turn-by-turn-optimized criteria for results returned by the Mapbox Directions API.
+ 
+ `NavigationRouteOptions` is a subclass of `RouteOptions` that has been optimized for navigation. Pass an instance of this class into the `Directions.calculate(_:completionHandler:)` method.
+ */
+@objc(MBNavigationRouteOptions)
+open class NavigationRouteOptions: RouteOptions {
+    
+    /**
+     Initializes a navigation route options object for routes between the given waypoints and an optional profile identifier optimized for navigation.
+     
+     - SeeAlso:
+     [RouteOptions](https://www.mapbox.com/mapbox-navigation-ios/directions/0.10.1/Classes/RouteOptions.html)
+     */
+    public override init(waypoints: [Waypoint], profileIdentifier: MBDirectionsProfileIdentifier? = .automobileAvoidingTraffic) {
+        super.init(waypoints: waypoints, profileIdentifier: profileIdentifier)
+        includesSteps = true
+        routeShapeResolution = .full
+        attributeOptions = [.congestionLevel, .expectedTravelTime]
+    }
+    
+    
+    /**
+     Initializes a navigation route options object for routes between the given locations and an optional profile identifier optimized for navigation.
+     
+     - SeeAlso:
+     [RouteOptions](https://www.mapbox.com/mapbox-navigation-ios/directions/0.10.1/Classes/RouteOptions.html)
+     */
+    public convenience init(locations: [CLLocation], profileIdentifier: MBDirectionsProfileIdentifier? = .automobileAvoidingTraffic) {
+        self.init(waypoints: locations.map { Waypoint(location: $0) }, profileIdentifier: profileIdentifier)
+    }
+    
+    
+    /**
+     Initializes a route options object for routes between the given geographic coordinates and an optional profile identifier optimized for navigation.
+     
+     - SeeAlso:
+     [RouteOptions](https://www.mapbox.com/mapbox-navigation-ios/directions/0.10.1/Classes/RouteOptions.html)
+     */
+    public convenience init(coordinates: [CLLocationCoordinate2D], profileIdentifier: MBDirectionsProfileIdentifier? = .automobileAvoidingTraffic) {
+        self.init(waypoints: coordinates.map { Waypoint(coordinate: $0) }, profileIdentifier: profileIdentifier)
+    }
+    
+    public required init?(coder decoder: NSCoder) {
+        super.init(coder: decoder)
+    }
+}
--- MapboxNavigation/NavigationViewController.swift
+++ MapboxNavigation/NavigationViewController.swift
@@ -321,8 +321,12 @@ public class NavigationViewController: NavigationPulleyViewController, RouteMapV
         
         tableViewController.routeController = routeController
         tableViewController.headerView.delegate = self
-        
+
         self.currentStyleType = styleTypeForTimeOfDay
+        
+        if !(route.routeOptions is NavigationRouteOptions) {
+            print("`Route` was created using `RouteOptions` and not `NavigationRouteOptions`. Although not required, this may lead to a suboptimal navigation experience. Without `NavigationRouteOptions`, it is not guaranteed you will get congestion along the route line, better ETAs and ETA label color dependent on congestion.")
+        }
     }
     
     deinit {
