622dc4c1b4981e24800f12ce0484f4eb0ff17bbb
Fredrik Karlsson <bjorn.fredrik.karlsson@gmail.com>
2017-08-30 16:42:41 -0400

    Add stylable marker view (#563)

--- MapboxNavigation/Style.swift
+++ MapboxNavigation/Style.swift
@@ -367,3 +367,51 @@ public class StatusView: UIView {
         }
     }
 }
+
+
+/// :nodoc:
+@objc(MBMarkerView)
+public class MarkerView: UIView {
+    
+    // Sets the inner color on the pin.
+    public dynamic var innerColor: UIColor = .white {
+        didSet {
+            setNeedsDisplay()
+        }
+    }
+    
+    // Sets the shadow color under the marker view.
+    public dynamic var shadowColor: UIColor = #colorLiteral(red: 0, green: 0, blue: 0, alpha: 0.1) {
+        didSet {
+            setNeedsDisplay()
+        }
+    }
+    
+    // Sets the color on the marker view.
+    public dynamic var pinColor: UIColor = #colorLiteral(red: 0.1493228376, green: 0.2374534607, blue: 0.333029449, alpha: 1) {
+        didSet {
+            setNeedsDisplay()
+        }
+    }
+    
+    // Sets the stroke color on the marker view.
+    public dynamic var strokeColor: UIColor = #colorLiteral(red: 1, green: 1, blue: 1, alpha: 0) {
+        didSet {
+            setNeedsDisplay()
+        }
+    }
+    
+    override public var intrinsicContentSize: CGSize {
+        return CGSize(width: 39, height: 51)
+    }
+    
+    public override func layoutSubviews() {
+        super.layoutSubviews()
+        backgroundColor = .clear
+    }
+    
+    override public func draw(_ rect: CGRect) {
+        super.draw(rect)
+        StyleKitMarker.drawMarker(innerColor: innerColor, shadowColor: shadowColor, pinColor: pinColor, strokeColor: strokeColor)
+    }
+}
