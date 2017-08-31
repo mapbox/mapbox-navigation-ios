163e26821f64bbf12a63ad0266c42c7e7cbd4f3e
Bobby Sudekum <bobby@mapbox.com>
2017-08-31 09:31:15 -0400

    remove

--- MapboxCoreNavigationTests/SpokenInstructionFormatter.swift
+++
@@ -1,9 +0,0 @@
-//
-//  SpokenInstructionFormatter.swift
-//  MapboxCoreNavigationTests
-//
-//  Created by Bobby Sudekum on 8/30/17.
-//  Copyright © 2017 Mapbox. All rights reserved.
-//
-
-import Foundation
