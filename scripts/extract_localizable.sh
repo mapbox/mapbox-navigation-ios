0dd31b7b624eda3177fbe2a3259ee0e68f7f8fc0
Fredrik Karlsson <bjorn.fredrik.karlsson@gmail.com>
2017-09-11 16:41:57 +0200

    Extract strings from storyboard

--- scripts/extract_localizable.sh
+++ scripts/extract_localizable.sh
@@ -13,6 +13,14 @@ do
     find ${NAVIGATION} -name "*.swift" -print0 | xargs -0 xcrun extractLocStrings -o "${NAVIGATION}/Resources/${lang}.lproj"
     STRINGS_FILE="${NAVIGATION}/Resources/${lang}.lproj/Localizable.strings"
 
+    # Extract localizable strings from storyboard
+    ibtool ${NAVIGATION}/Resources/${lang}.lproj/Navigation.storyboard --generate-strings-file ${NAVIGATION}/Resources/${lang}.lproj/Navigation.strings
+
+    # Remove strings that should not be translated
+    source ./file_conversion.sh
+    convertIfNeeded "${NAVIGATION}/Resources/${lang}.lproj/Navigation.strings"
+    sed -i '' -e '/DO NOT TRANSLATE/{N;N;d;}' "${NAVIGATION}/Resources/${lang}.lproj/Navigation.strings"
+
     # Convert UTF-16LE generated files to UTF-8
     iconv -f UTF-16LE -t UTF-8 ${STRINGS_FILE} > ${STRINGS_FILE}.new
     mv -f ${STRINGS_FILE}.new ${STRINGS_FILE}
