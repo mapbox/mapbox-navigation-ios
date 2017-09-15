3907aa757bc2e1f3e1e26b5938d8d89e373e6bec
Fredrik Karlsson <bjorn.fredrik.karlsson@gmail.com>
2017-09-15 16:12:17 +0200

    Remove unused methods

--- MapboxNavigation/MGLMapView+MGLNavigationAdditions.m
+++ MapboxNavigation/MGLMapView+MGLNavigationAdditions.m
@@ -1,23 +1,7 @@
 #import "MGLMapView+MGLNavigationAdditions.h"
 
-#pragma clang diagnostic push
-#pragma clang diagnostic ignored "-Wincomplete-implementation"
-// Supressing compiler warning until https://github.com/mapbox/mapbox-gl-native/issues/6867 is implemented
 @implementation MGLMapView (MGLNavigationAdditions)
-#pragma clang diagnostic pop
 
-- (void)setCenterCoordinate:(CLLocationCoordinate2D)centerCoordinate
-                edgePadding:(UIEdgeInsets)edgePadding
-                  zoomLevel:(double)zoomLevel
-                  direction:(CLLocationDirection)direction
-{
-    [self _setCenterCoordinate:centerCoordinate
-                   edgePadding:edgePadding
-                     zoomLevel:zoomLevel
-                     direction:direction
-                      duration:0
-       animationTimingFunction:nil
-             completionHandler:nil];
-}
+- (void)mapViewDidFinishRenderingFrameFullyRendered:(BOOL)fullyRendered { }
 
 @end
