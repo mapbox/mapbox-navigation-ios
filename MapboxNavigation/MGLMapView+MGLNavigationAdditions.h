a1b6ab22b498e1dfd8108fa3cf9d7596f06163fe
Fredrik Karlsson <bjorn.fredrik.karlsson@gmail.com>
2017-09-15 16:12:17 +0200

    Remove unused methods

--- MapboxNavigation/MGLMapView+MGLNavigationAdditions.h
+++ MapboxNavigation/MGLMapView+MGLNavigationAdditions.h
@@ -2,19 +2,6 @@
 
 @interface MGLMapView (MGLNavigationAdditions) <CLLocationManagerDelegate>
 
-- (void)_setCenterCoordinate:(CLLocationCoordinate2D)centerCoordinate
-                 edgePadding:(UIEdgeInsets)insets
-                   zoomLevel:(double)zoomLevel
-                   direction:(CLLocationDirection)direction
-                    duration:(NSTimeInterval)duration
-     animationTimingFunction:(nullable CAMediaTimingFunction *)function
-           completionHandler:(nullable void (^)(void))completion;
-
-- (void)setCenterCoordinate:(CLLocationCoordinate2D)centerCoordinate
-                edgePadding:(UIEdgeInsets)edgePadding
-                  zoomLevel:(double)zoomLevel
-                  direction:(CLLocationDirection)direction;
-
 - (void)mapViewDidFinishRenderingFrameFullyRendered:(BOOL)fullyRendered;
 
 @end
