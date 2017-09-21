d5a637570e0af7824464c7f278c15dadc0fa8643
Fredrik Karlsson <bjorn.fredrik.karlsson@gmail.com>
2017-09-21 18:01:00 +0200

    Clean up user puck

--- MapboxNavigation/UserCourseView.swift
+++ MapboxNavigation/UserCourseView.swift
@@ -18,106 +18,7 @@ public protocol UserCourseView {
 
 class UserPuckCourseView: UIView, UserCourseView {
     
-    var puckDot: CALayer?
-    var puckArrow: CAShapeLayer?
-    var location: CLLocation = CLLocation()
-    var heading: CLHeading = CLHeading()
-    
-    var pitch: CLLocationDegrees = 0 {
-        didSet {
-            if oldValue != pitch {
-                updatePitch()
-            }
-        }
-    }
-    
-    override func layoutSubviews() {
-        super.layoutSubviews()
-        bounds = CGRect(origin: .zero, size: CGSize(width: PuckSize, height: PuckSize))
-    }
-    
-    func update(location: CLLocation, pitch: CGFloat, direction: CLLocationDegrees, animated: Bool) {
-        drawPuck(location, pitch, direction)
-        self.pitch = CLLocationDegrees(pitch)
-    }
-    
-    func updatePitch() {
-        CATransaction.begin()
-        CATransaction.setValue(kCFBooleanTrue, forKey: kCATransactionDisableActions)
-        
-        let t = CATransform3DRotate(CATransform3DIdentity, CGFloat(pitch.toRadians()), 1, 0, 0)
-        layer.sublayerTransform = t
-        
-        updateFaux3DEffect()
-        
-        CATransaction.commit()
-    }
-    
-    func updateFaux3DEffect() {
-        puckDot?.shadowOffset = CGSize(width: 0, height: CGFloat(fmaxf(Float(pitch.toRadians() * 10), 1)))
-        puckDot?.shadowRadius = CGFloat(fmaxf(Float(pitch.toRadians()) * 5, 0.75))
-    }
-    
-    func circleLayer(with size: CGSize) -> CALayer {
-        let layer = CALayer()
-        layer.bounds = CGRect(origin: .zero, size: size)
-        layer.position = CGPoint(x: bounds.midX, y: bounds.midY)
-        layer.cornerRadius = size.width / 2
-        layer.shouldRasterize = true
-        layer.rasterizationScale = UIScreen.main.scale
-        layer.drawsAsynchronously = true
-        return layer
-    }
-    
-    var puckArrowPath: UIBezierPath {
-        let max = ArrowSize
-        let path = UIBezierPath()
-        path.move(to:       CGPoint(x: max * 0.5, y: 0))
-        path.addLine(to:    CGPoint(x: max * 0.1, y: max))
-        path.addLine(to:    CGPoint(x: max * 0.5, y: max * 0.65))
-        path.addLine(to:    CGPoint(x: max * 0.9, y: max))
-        path.addLine(to:    CGPoint(x: max * 0.5, y: 0))
-        path.close()
-        return path
-    }
-    
-    func drawPuck(_ location: CLLocation,_ pitch: CGFloat, _ direction: CLLocationDegrees) {
-        
-        bounds = CGRect(origin: .zero, size: CGSize(width: PuckSize, height: PuckSize))
-        
-        if puckDot == nil {
-            puckDot = circleLayer(with: CGSize(width: PuckSize, height: PuckSize))
-            puckDot?.backgroundColor = UIColor.white.cgColor
-            puckDot?.shadowColor = UIColor.black.cgColor
-            puckDot?.shadowOpacity = 0.25
-            puckDot?.shadowPath = UIBezierPath(ovalIn: puckDot!.bounds).cgPath
-            layer.addSublayer(puckDot!)
-            
-            updateFaux3DEffect()
-        }
-        
-        if puckArrow == nil {
-            puckArrow = CAShapeLayer()
-            puckArrow?.path = puckArrowPath.cgPath
-            puckArrow?.fillColor = tintColor.cgColor
-            puckArrow?.bounds = CGRect(origin: .zero, size: CGSize(width: ArrowSize, height: ArrowSize))
-            puckArrow?.position = CGPoint(x: bounds.midX, y: bounds.midY)
-            puckArrow?.shouldRasterize = true
-            puckArrow?.rasterizationScale = UIScreen.main.scale
-            puckArrow?.drawsAsynchronously = true
-            layer.addSublayer(puckArrow!)
-        }
-        
-        if location.course >= 0 {
-            let angle = CLLocationDegrees(direction - location.course)
-            puckArrow?.setAffineTransform(CGAffineTransform.identity.rotated(by: -CGFloat(angle.toRadians())))
-        }
-    }
-}
-
-class NewUserPuckCourseView: UIView, UserCourseView {
-    
-    var puckLayerView: UserPuckStyleKitView!
+    var puckView: UserPuckStyleKitView!
     
     override init(frame: CGRect) {
         super.init(frame: frame)
@@ -130,38 +31,26 @@ class NewUserPuckCourseView: UIView, UserCourseView {
     }
     
     func commonInit() {
-        puckLayerView = UserPuckStyleKitView(frame: bounds)
+        puckView = UserPuckStyleKitView(frame: bounds)
         backgroundColor = .clear
-        puckLayerView.backgroundColor = .clear
-        addSubview(puckLayerView)
+        puckView.backgroundColor = .clear
+        addSubview(puckView)
     }
     
-    var shouldAnimateTransformations: Bool = false
+    var shouldAnimatePitch: Bool = false
+    var shouldAnimateDirection: Bool = false
     
     var location: CLLocation?
     
     var pitch: CLLocationDegrees = 0 {
         didSet {
-            if oldValue != pitch {
-                let newPitch = pitch
-                UIView.animate(withDuration: 1, delay: 0, options: [.beginFromCurrentState, .curveLinear], animations: {
-                    var transform = CATransform3DRotate(CATransform3DIdentity, CGFloat(CLLocationDegrees(newPitch).toRadians()), 1.0, 0, 0)
-                    transform.m34 = -1.0 / 1000 // (-1 / distance to projection plane)
-                    self.layer.sublayerTransform = transform
-                }, completion: nil)
-            }
+            shouldAnimatePitch = oldValue != pitch
         }
     }
     
     var direction: CLLocationDirection = 0 {
         didSet {
-            if oldValue != direction {
-                guard let location = location else { return }
-                let angle = CLLocationDegrees(direction - location.course)
-                UIView.animate(withDuration: 1, delay: 0, options: [.beginFromCurrentState, .curveLinear], animations: {
-                    self.puckLayerView.layer.setAffineTransform(CGAffineTransform.identity.rotated(by: -CGFloat(angle.toRadians())))
-                }, completion: nil)
-            }
+            shouldAnimateDirection = oldValue != direction
         }
     }
     
@@ -169,6 +58,20 @@ class NewUserPuckCourseView: UIView, UserCourseView {
         self.location = location
         self.direction = direction
         self.pitch = CLLocationDegrees(pitch)
+        
+        UIView.animate(withDuration: 1, delay: 0, options: [.beginFromCurrentState, .curveLinear], animations: {
+            if self.shouldAnimateDirection {
+                guard let location = self.location else { return }
+                let angle = CLLocationDegrees(direction - location.course)
+                self.puckView.layer.setAffineTransform(CGAffineTransform.identity.rotated(by: -CGFloat(angle.toRadians())))
+            }
+            
+            if self.shouldAnimatePitch {
+                var transform = CATransform3DRotate(CATransform3DIdentity, CGFloat(self.pitch.toRadians()), 1.0, 0, 0)
+                transform.m34 = -1.0 / 1000 // (-1 / distance to projection plane)
+                self.layer.sublayerTransform = transform
+            }
+        }, completion: nil)
     }
 }
 
