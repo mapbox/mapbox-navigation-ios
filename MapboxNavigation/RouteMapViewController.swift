eba531e9efab2f5d11c965a10a2296c4d9909c50
Bobby Sudekum <bobby@mapbox.com>
2017-10-09 15:10:35 -0700

    set tracksUserCourse = true when swiping to current step

--- MapboxNavigation/RouteMapViewController.swift
+++ MapboxNavigation/RouteMapViewController.swift
@@ -559,6 +559,8 @@ extension RouteMapViewController: RoutePageViewControllerDelegate {
             if didSwipe, step != routeController.routeProgress.currentLegProgress.upComingStep {
                 mapView.tracksUserCourse = false
                 mapView.setCenter(step.maneuverLocation, zoomLevel: mapView.zoomLevel, direction: step.initialHeading!, animated: true, completionHandler: nil)
+            } else {
+                mapView.tracksUserCourse = true
             }
         }
         
