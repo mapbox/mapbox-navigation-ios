95483d0ad4c45f1a9b77a20305a20a3c3d9f728a
Eric Wolfe <eric.r.wolfe@gmail.com>
2017-10-20 17:14:58 -0700

    Tweak feedback categories

--- MapboxNavigation/FeedbackItem.swift
+++ MapboxNavigation/FeedbackItem.swift
@@ -22,18 +22,18 @@ struct FeedbackItem {
     
     // Row 1
     static let closure = FeedbackItem(title: closureTitle, image: .feedbackImage(named:"feedback-closed-road"), feedbackType: .roadClosed)
-    static let turnNotAllowed = FeedbackItem(title: notAllowedTitle, image:  .feedbackImage(named:"feedback-no-turn-allowed"), feedbackType: .unallowedTurn)
+    static let turnNotAllowed = FeedbackItem(title: notAllowedTitle, image:  .feedbackImage(named:"feedback-no-turn-allowed"), feedbackType: .notAllowed)
     static let reportTraffic = FeedbackItem(title: reportTrafficTitle, image: .feedbackImage(named:"feedback-traffic"), feedbackType: .reportTraffic)
     
     // Row 2
     static let confusingInstructions = FeedbackItem(title: confusingInstructionTitle, image: .feedbackImage(named:"feedback-confusing-directions"), feedbackType: .confusingInstruction)
     static let GPSInaccurate = FeedbackItem(title: gpsInaccurateTitle, image: .feedbackImage(named:"feedback-gps"), feedbackType: .inaccurateGPS)
-    static let badRoute = FeedbackItem(title: badRouteTitle, image: .feedbackImage(named:"feedback-wrong-directions"), feedbackType: .badRoute)
+    static let badRoute = FeedbackItem(title: badRouteTitle, image: .feedbackImage(named:"feedback-wrong-directions"), feedbackType: .routingError)
     
-    // Not used
-    static let missingRoad = FeedbackItem(title: instructionTimingTitle, image: .feedbackImage(named:"feedback-missing-road"), feedbackType: .badRoute)
-    static let missingExit = FeedbackItem(title: instructionTimingTitle, image: .feedbackImage(named:"feedback-exit"), feedbackType: .badRoute)
-    static let genermalMapError = FeedbackItem(title: instructionTimingTitle, image: .feedbackImage(named:"feedback-map-error"), feedbackType: .general)
+    // Not currently used
+    static let missingRoad = FeedbackItem(title: missingExitTitle, image: .feedbackImage(named:"feedback-missing-road"), feedbackType: .missingRoad)
+    static let missingExit = FeedbackItem(title: missingRoadTitle, image: .feedbackImage(named:"feedback-exit"), feedbackType: .missingExit)
+    static let generalMapError = FeedbackItem(title: generalIssueTitle, image: .feedbackImage(named:"feedback-map-error"), feedbackType: .general)
 }
 
 
@@ -43,8 +43,7 @@ fileprivate let reportTrafficTitle = NSLocalizedString("FEEDBACK_REPORT_TRAFFIC"
 fileprivate let confusingInstructionTitle = NSLocalizedString("FEEDBACK_CONFUSING_INSTRUCTION", bundle: .mapboxNavigation, value: "Confusing \nInstruction", comment: "Feedback type for Confusing Instruction")
 fileprivate let gpsInaccurateTitle = NSLocalizedString("FEEDBACK_GPS_INACCURATE", bundle: .mapboxNavigation, value: "GPS \nInaccurate", comment: "Feedback type for inaccurate GPS")
 fileprivate let badRouteTitle = NSLocalizedString("FEEDBACK_BAD_ROUTE", bundle: .mapboxNavigation, value: "Bad \nRoute", comment: "Feedback type for Bad Route")
-
-fileprivate let instructionTimingTitle = NSLocalizedString("FEEDBACK_INSTRUCTION_TIMING", bundle: .mapboxNavigation, value: "Instruction \nTiming", comment: "Feedback type for Instruction Timing")
-fileprivate let instructionIssueTitle = NSLocalizedString("FEEDBACK_INSTRUCTION_ISSUE", bundle: .mapboxNavigation, value: "Instruction \nIssue", comment: "Feedback type for Instruction Issue")
-fileprivate let heavyTrafficTitle = NSLocalizedString("FEEDBACK_HEAVY_TRAFFIC", bundle: .mapboxNavigation, value: "Heavy \nTraffic", comment: "Feedback type for Heavy Traffic")
+fileprivate let missingExitTitle = NSLocalizedString("FEEDBACK_MISSING_EXIT", bundle: .mapboxNavigation, value: "Missing \nExit", comment: "Feedback type for Missing Exit")
+fileprivate let missingRoadTitle = NSLocalizedString("FEEDBACK_MISSING_ROAD", bundle: .mapboxNavigation, value: "Missing \nRoad", comment: "Feedback type for Missing Road")
+fileprivate let generalIssueTitle = NSLocalizedString("FEEDBACK_GENERAL_ISSUE", bundle: .mapboxNavigation, value: "General \nIssue", comment: "Feedback type for General Issue")
 
