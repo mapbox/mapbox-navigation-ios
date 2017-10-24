18c41921f57803fe6211768c741f8347d08bb949
Bobby Sudekum <bobby@mapbox.com>
2017-10-24 16:19:19 -0700

    prefix

--- MapboxCoreNavigation/Constants.swift
+++ MapboxCoreNavigation/Constants.swift
@@ -151,4 +151,4 @@ public var RouteControllerMinimumNumberLocationUpdatesBackwards = 3
  */
 public var RouteControllerNumberOfSecondsForRerouteFeedback: TimeInterval = 10
 
-let FasterRouteFoundEvent = "fasterRouteFound"
+let FasterRouteFoundEvent = "navigation.fasterRouteFound"
