549834dbc3c4dc7b7b8047fcaca4043634e25ac9
Fredrik Karlsson <bjorn.fredrik.karlsson@gmail.com>
2017-10-26 16:26:59 +0200

    Add missing instruction

--- MapboxCoreNavigation/VisualInstructionFormatter.swift
+++ MapboxCoreNavigation/VisualInstructionFormatter.swift
@@ -28,12 +28,20 @@ public class VisualInstructionFormatter: NSObject {
         }
     }
     
+    /**
+     Creates the optimal text to be displayed given a `RouteLeg` and `RouteStep` divided into primary and secondary string.
+     */
     public func strings(leg: RouteLeg?, step: RouteStep?) -> (String?, String?) {
         if let currentLeg = leg, let destinationName = currentLeg.destination.name, let step = step, step.maneuverType == .arrive {
-            return("You have arrived", destinationName)
+            let primary = NSLocalizedString("ARRIVED_AT_DESTINATION", bundle: .mapboxCoreNavigation, value: "You have arrived", comment: "Instruction when arrived at a destination")
+            return(primary, destinationName)
         } else if let step = step, step.isNumberedMotorway, let codes = step.codes, let destinations = step.destinations {
             let primary = codes.joined(separator: NSLocalizedString("REF_DELIMITER", bundle: .mapboxCoreNavigation, value: " / ", comment: "Delimiter between route numbers in a road concurrency"))
             let secondary = destinations.joined(separator: NSLocalizedString("DESTINATION_DELIMITER", bundle: .mapboxCoreNavigation, value: " / ", comment: "Delimiter between multiple destinations"))
+            return (primary, secondary)
+        } else if let destinations = step?.destinations, let names = step?.names {
+            let primary = names.first
+            let secondary = destinations.suffix(from: 1).joined(separator: NSLocalizedString("DESTINATION_DELIMITER", bundle: .mapboxCoreNavigation, value: " / ", comment: "Delimiter between multiple destinations"))
             return (primary, secondary)
         } else if let step = step, let names = step.names {
             let primary = names.first
@@ -41,8 +49,8 @@ public class VisualInstructionFormatter: NSObject {
             return (primary, secondary)
         } else if let currentLeg = leg, let destinationName = currentLeg.destination.name {
             return (destinationName, nil)
+        } else {
+            return (step?.instructions, nil)
         }
-        
-        return (step?.instructions, nil)
     }
 }
