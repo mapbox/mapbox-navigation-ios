7289cf56a66df73f397b95624f7d511451d5d6dd
Fredrik Karlsson <bjorn.fredrik.karlsson@gmail.com>
2017-10-26 16:47:45 +0200

    wip inline shield

--- MapboxNavigation/RouteManeuverViewController.swift
+++ MapboxNavigation/RouteManeuverViewController.swift
@@ -42,7 +42,9 @@ class RouteManeuverViewController: UIViewController {
             
             if components.count == 2 || (components.count == 3 && ["North", "South", "East", "West", "Nord", "Sud", "Est", "Ouest", "Norte", "Sur", "Este", "Oeste"].contains(components[2])) {
                 
-                guard let shieldURL = URL.shieldURL(network: components[0], number: components[1], height: 32 * UIScreen.main.scale) else {
+                let height = ("|" as NSString).size(attributes: [NSFontAttributeName: self.instructionsBannerView.primaryLabel.font]).height
+                
+                guard let shieldURL = URL.shieldURL(network: components[0], number: components[1], height: height) else {
                     return
                 }
                 
@@ -50,7 +52,8 @@ class RouteManeuverViewController: UIViewController {
                     // TODO: Cache imageURL
                     SDWebImageDownloader.shared().downloadImage(with: imageURL, options: [], progress: nil, completed: { (image, data, error, successful) in
                         // TODO: Cache image
-                        
+                        guard let shieldImage = image else { return }
+                        self.instructionsBannerView.primaryLabel.shieldImage = shieldImage
                     })
                 })
                 
