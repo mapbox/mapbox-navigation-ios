12062ff8dc452a923321a422c9f8a45c6e5e30bb
Eric Wolfe <eric.r.wolfe@gmail.com>
2017-10-27 15:22:17 -0700

    Tweak instruction formatter

--- MapboxCoreNavigation/VisualInstructionFormatter.swift
+++ MapboxCoreNavigation/VisualInstructionFormatter.swift
@@ -35,20 +35,18 @@ public class VisualInstructionFormatter: NSObject {
         if let currentLeg = leg, let destinationName = currentLeg.destination.name, let step = step, step.maneuverType == .arrive {
             let primary = NSLocalizedString("ARRIVED_AT_DESTINATION", bundle: .mapboxCoreNavigation, value: "You have arrived", comment: "Instruction when arrived at a destination")
             return(primary, destinationName)
-        } else if let step = step, step.isNumberedMotorway, let codes = step.codes, let destinations = step.destinations {
+        } else if let step = step, let codes = step.destinationCodes {
             let primary = codes.joined(separator: NSLocalizedString("REF_DELIMITER", bundle: .mapboxCoreNavigation, value: " / ", comment: "Delimiter between route numbers in a road concurrency"))
-            let secondary = destinations.joined(separator: NSLocalizedString("DESTINATION_DELIMITER", bundle: .mapboxCoreNavigation, value: " / ", comment: "Delimiter between multiple destinations"))
+            let secondary = step.destinations?.joined(separator: NSLocalizedString("DESTINATION_DELIMITER", bundle: .mapboxCoreNavigation, value: " / ", comment: "Delimiter between multiple destinations"))
             return (primary, secondary)
-        } else if let destinations = step?.destinations, let names = step?.names {
-            let primary = names.first
+        } else if let step = step, step.isNumberedMotorway, let codes = step.codes {
+            let primary = codes.joined(separator: NSLocalizedString("REF_DELIMITER", bundle: .mapboxCoreNavigation, value: " / ", comment: "Delimiter between route numbers in a road concurrency"))
+            let secondary = step.destinations?.joined(separator: NSLocalizedString("DESTINATION_DELIMITER", bundle: .mapboxCoreNavigation, value: " / ", comment: "Delimiter between multiple destinations"))
+            return (primary, secondary)
+        } else if let destinations = step?.destinations ?? step?.names {
+            let primary = destinations.first
             let secondary = destinations.suffix(from: 1).joined(separator: NSLocalizedString("DESTINATION_DELIMITER", bundle: .mapboxCoreNavigation, value: " / ", comment: "Delimiter between multiple destinations"))
             return (primary, secondary)
-        } else if let step = step, let names = step.names {
-            let primary = names.first
-            let secondary = names.suffix(from: 1).joined(separator: NSLocalizedString("DESTINATION_DELIMITER", bundle: .mapboxCoreNavigation, value: " / ", comment: "Delimiter between multiple destinations"))
-            return (primary, secondary)
-        } else if let currentLeg = leg, let destinationName = currentLeg.destination.name {
-            return (destinationName, nil)
         } else {
             return (step?.instructions, nil)
         }
