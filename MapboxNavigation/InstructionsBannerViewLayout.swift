89ae36541c451b0f8b35129f17ec114c18d874c7
Fredrik Karlsson <bjorn.fredrik.karlsson@gmail.com>
2017-11-01 17:21:52 +0100

    38x38 maneuver icon

--- MapboxNavigation/InstructionsBannerViewLayout.swift
+++ MapboxNavigation/InstructionsBannerViewLayout.swift
@@ -61,8 +61,8 @@ extension InstructionsBannerView {
         distanceLabel.lastBaselineAnchor.constraint(equalTo: bottomAnchor, constant: -16).isActive = true
         
         // Turn arrow view
-        maneuverView.heightAnchor.constraint(equalToConstant: 40).isActive = true
-        maneuverView.widthAnchor.constraint(equalToConstant: 40).isActive = true
+        maneuverView.heightAnchor.constraint(equalToConstant: 38).isActive = true
+        maneuverView.widthAnchor.constraint(equalToConstant: 38).isActive = true
         maneuverView.topAnchor.constraint(equalTo: topAnchor, constant: 16).isActive = true
         maneuverView.bottomAnchor.constraint(greaterThanOrEqualTo: distanceLabel.topAnchor).isActive = true
         maneuverView.leftAnchor.constraint(equalTo: leftAnchor, constant: 16).isActive = true
