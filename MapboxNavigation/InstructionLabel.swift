842b3e795f9def9438e85e80246a13c82c5fc08f
Fredrik Karlsson <bjorn.fredrik.karlsson@gmail.com>
2017-11-09 17:39:09 +0100

    Cache shield based on height

--- MapboxNavigation/InstructionLabel.swift
+++ MapboxNavigation/InstructionLabel.swift
@@ -29,7 +29,8 @@ open class InstructionLabel: StylableLabel {
             
             if let roadCode = component.roadCode, let network = component.network, let number = component.number {
                 // Check if shield image is cached, otherwise display road code in text
-                if let cachedImage = component.cachedShield {
+                let shieldKey = UIImage.shieldKey(network, number: number, height: shieldHeight)
+                if let cachedImage = UIImage.cachedShield(shieldKey) {
                     string.append(attributedString(with: cachedImage))
                     if let direction = component.direction {
                         string.append(NSAttributedString(string: " "+direction, attributes: attributes))
@@ -37,10 +38,9 @@ open class InstructionLabel: StylableLabel {
                 } else {
                     // Download shield and display road code in the meantime
                     string.append(NSAttributedString(string: joinChar+roadCode, attributes: attributes))
-                    let height = shieldHeight * UIScreen.main.scale
-                    UIImage.shieldImage(network, number: number, height: height, completion: { [unowned self] (image) in
+                    UIImage.shieldImage(network, number: number, height: shieldHeight, completion: { [unowned self] (image) in
                         // Reconstruct instructions if we did get a shield image
-                        guard image != nil, component.cachedShield != nil else { return }
+                        guard image != nil, UIImage.cachedShield(shieldKey) != nil else { return }
                         self.constructInstructions()
                     })
                 }
@@ -62,13 +62,6 @@ open class InstructionLabel: StylableLabel {
         attachment.font = font
         attachment.image = shieldImage
         return NSAttributedString(attachment: attachment)
-    }
-}
-
-extension Instruction.Component {
-    var cachedShield: UIImage? {
-        guard roadCode == roadCode, let shieldKey = shieldKey else { return nil }
-        return UIImage.shieldImageCache.object(forKey: shieldKey as NSString)
     }
 }
 
--- MapboxNavigation/UIImage.swift
+++ MapboxNavigation/UIImage.swift
@@ -6,46 +6,53 @@ extension UIImage {
     static let shieldURLCache = NSCache<NSString, NSURL>()
     static let shieldImageCache = NSCache<NSString, UIImage>()
     
-    static func shieldImage(_ network: String, number: String, height: CGFloat, completion: @escaping (UIImage?) -> Void) {
+    static func shieldKey(_ network: String, number: String, height: CGFloat) -> String {
+        return "\(network)-\(number)-\(Int(height))"
+    }
+    
+    static func cachedShield(_ shieldKey: String) -> UIImage? {
+        return UIImage.shieldImageCache.object(forKey: shieldKey as NSString)
+    }
+    
+    static func shieldImage(_ network: String, number: String, height: CGFloat, scale: CGFloat = UIScreen.main.scale, completion: @escaping (UIImage?) -> Void) {
         
-        let key = "\(network)\(number)" as NSString
+        let shieldKey = self.shieldKey(network, number: number, height: height)
         
-        if let cachedImage = UIImage.shieldImageCache.object(forKey: key) {
+        if let cachedImage = UIImage.cachedShield(shieldKey) {
             completion(cachedImage)
             return
         }
         
-        if let cachedURL = UIImage.shieldURLCache.object(forKey: key) {
+        if let cachedURL = UIImage.shieldURLCache.object(forKey: shieldKey as NSString) {
             
             SDWebImageDownloader.shared().downloadImage(with: (cachedURL as URL), options: [], progress: nil, completed: { (image, data, error, successful) in
                 guard let imageData = data else { return }
-                guard let downscaledImage = UIImage(data: imageData, scale: UIScreen.main.scale) else {
+                guard let downscaledImage = UIImage(data: imageData, scale: scale) else {
                     completion(nil)
                     return
                 }
                 
-                UIImage.shieldImageCache.setObject(downscaledImage, forKey: key)
+                UIImage.shieldImageCache.setObject(downscaledImage, forKey: shieldKey as NSString)
                 completion(downscaledImage)
                 return
             })
         } else {
             
-            guard let shieldURL = URL.shieldURL(network: network, number: number, height: height) else {
+            guard let shieldURL = URL.shieldURL(network: network, number: number, height: height * scale) else {
                 completion(nil)
                 return
             }
             
             URL.shieldImageURL(shieldURL: shieldURL, completion: { (imageURL) in
-                UIImage.shieldURLCache.setObject(shieldURL as NSURL, forKey: key)
+                UIImage.shieldURLCache.setObject(shieldURL as NSURL, forKey: shieldKey as NSString)
                 
                 SDWebImageDownloader.shared().downloadImage(with: imageURL, options: [], progress: nil, completed: { (image, data, error, successful) in
                     guard let imageData = data else { return }
-                    guard let downscaledImage = UIImage(data: imageData, scale: UIScreen.main.scale) else {
+                    guard let downscaledImage = UIImage(data: imageData, scale: scale) else {
                         completion(nil)
                         return
                     }
-                    
-                    UIImage.shieldImageCache.setObject(downscaledImage, forKey: key)
+                    UIImage.shieldImageCache.setObject(downscaledImage, forKey: shieldKey as NSString)
                     completion(downscaledImage)
                 })
             })
