5d0dbdb08037a1c37b9e3680ec5ee140ae40db31
Bobby Sudekum <bobby@mapbox.com>
2017-11-27 16:56:51 -0800

    Fix leaky timer

--- MapboxNavigation/RouteMapViewController.swift
+++ MapboxNavigation/RouteMapViewController.swift
@@ -144,20 +144,21 @@ class RouteMapViewController: UIViewController {
     
     override func viewWillDisappear(_ animated: Bool) {
         super.viewWillDisappear(animated)
-        updateETATimer?.invalidate()
-        updateETATimer = nil
+        removeTimer()
     }
-    
+
     func resumeNotifications() {
         NotificationCenter.default.addObserver(self, selector: #selector(willReroute(notification:)), name: RouteControllerWillReroute, object: nil)
         NotificationCenter.default.addObserver(self, selector: #selector(didReroute(notification:)), name: RouteControllerDidReroute, object: nil)
         NotificationCenter.default.addObserver(self, selector: #selector(applicationWillEnterForeground(notification:)), name: .UIApplicationWillEnterForeground, object: nil)
+        NotificationCenter.default.addObserver(self, selector: #selector(removeTimer), name: .UIApplicationWillResignActive, object: nil)
     }
     
     func suspendNotifications() {
         NotificationCenter.default.removeObserver(self, name: RouteControllerWillReroute, object: nil)
         NotificationCenter.default.removeObserver(self, name: RouteControllerDidReroute, object: nil)
         NotificationCenter.default.removeObserver(self, name: .UIApplicationWillEnterForeground, object: nil)
+        NotificationCenter.default.removeObserver(self, name: .UIApplicationWillResignActive, object: nil)
     }
 
     @IBAction func recenter(_ sender: AnyObject) {
@@ -171,6 +172,11 @@ class RouteMapViewController: UIViewController {
                          stepIndex: routeController.routeProgress.currentLegProgress.stepIndex + 1)
         
         removePreviewInstructions()
+    }
+    
+    @objc func removeTimer() {
+        updateETATimer?.invalidate()
+        updateETATimer = nil
     }
     
     func removePreviewInstructions() {
@@ -293,6 +299,7 @@ class RouteMapViewController: UIViewController {
     
     @objc func applicationWillEnterForeground(notification: NSNotification) {
         mapView.updateCourseTracking(location: routeController.location, animated: false)
+        resetETATimer()
     }
     
     @objc func willReroute(notification: NSNotification) {
