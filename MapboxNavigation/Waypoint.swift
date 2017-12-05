a4caa86948af1050f85ddcf13b191e3082e9fdc3
Jerrad Thramer <jerrad.thramer@mapbox.com>
2017-12-05 12:46:02 -0700

    I really need to remove that template one of these days...

--- MapboxNavigation/Waypoint.swift
+++ MapboxNavigation/Waypoint.swift
@@ -1,11 +1,3 @@
-//
-//  Waypoint.swift
-//  MapboxNavigation
-//
-//  Created by Jerrad Thramer on 12/5/17.
-//  Copyright © 2017 Mapbox. All rights reserved.
-//
-
 import Foundation
 import MapboxDirections
 
