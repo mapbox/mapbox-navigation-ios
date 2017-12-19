61e1defe893bdb2e4fd439939f6aaa75023612e6
Bobby Sudekum <bobby@mapbox.com>
2017-12-19 07:37:19 -0800

    Fix when we show the then banner (#940)

--- MapboxNavigation/RouteMapViewController.swift
+++ MapboxNavigation/RouteMapViewController.swift
@@ -455,7 +455,7 @@ class RouteMapViewController: UIViewController {
         
         // If the followon step is short and the user is near the end of the current step, show the nextBanner.
         guard nextStep.expectedTravelTime <= RouteControllerHighAlertInterval * RouteControllerLinkedInstructionBufferMultiplier,
-            routeProgress.currentLegProgress.durationRemaining <= RouteControllerHighAlertInterval * RouteControllerLinkedInstructionBufferMultiplier else {
+            routeProgress.currentLegProgress.currentStepProgress.durationRemaining <= RouteControllerHighAlertInterval * RouteControllerLinkedInstructionBufferMultiplier else {
                 hideNextBanner()
                 return
         }
