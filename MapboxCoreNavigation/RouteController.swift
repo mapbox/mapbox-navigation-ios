585d897a5369a6ea31586861805fea13e5ae4faf
Bobby Sudekum <bobby@mapbox.com>
2017-12-19 17:09:01 -0800

    Assert if app is missing location permissions (#947)

--- MapboxCoreNavigation/RouteController.swift
+++ MapboxCoreNavigation/RouteController.swift
@@ -202,6 +202,11 @@ open class RouteController: NSObject {
         DispatchQueue.main.async {
             self.startEvents(route: route)
         }
+        
+        guard let _ = Bundle.main.bundleIdentifier else { return }
+        if Bundle.main.locationAlwaysUsageDescription == nil && Bundle.main.locationWhenInUseUsageDescription == nil && Bundle.main.locationAlwaysAndWhenInUseUsageDescription == nil {
+            preconditionFailure("This application’s Info.plist file must include a NSLocationWhenInUseUsageDescription. See https://developer.apple.com/documentation/corelocation for more information.")
+        }
     }
 
     deinit {
