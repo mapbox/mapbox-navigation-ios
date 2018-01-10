508db9eef5305b5e57945126c2683d6054ae16a3
Bobby Sudekum <bobby@mapbox.com>
2018-01-10 15:19:29 -0800

    Add nav examples to guides

---
+++ scripts/update-guides.sh
@@ -0,0 +1,19 @@
+set -eu
+
+if [ -d "navigation-ios-examples" ]; then rm -Rf navigation-ios-examples; fi
+git clone https://github.com/mapbox/navigation-ios-examples
+
+cp navigation-ios-examples/Navigation\ Examples/Examples/*.swift docs/guides
+
+for file in docs/guides/*.swift; do
+    # Add markdown formatting
+    url="https://github.com/mapbox/navigation-ios-examples/blob/master/Navigation%20Examples/Examples/$(basename "$file" .swift).swift"
+    urlNote="\n_Source available [here]($url)_\n"
+
+    echo -e "$urlNote\n\`\`\`swift\n$(cat $file)\n\`\`\`" > $file
+
+    # Add .md extension
+    mv "$file" "docs/guides/$(basename "$file" .swift).md"
+done
+
+rm -rf navigation-ios-examples
