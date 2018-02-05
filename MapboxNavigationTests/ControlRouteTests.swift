514e8d0c43976067b1e992f4d8998779240688c9
Fredrik Karlsson <bjorn.fredrik.karlsson@gmail.com>
2018-02-05 17:15:22 +0100

    Remove unmatched test

--- MapboxNavigationTests/ControlRouteTests.swift
+++ MapboxNavigationTests/ControlRouteTests.swift
@@ -31,23 +31,6 @@ class ControlRouteTests: FBSnapshotTestCase {
         FBSnapshotVerifyView(view)
     }
     
-    func testSignalJumps() {
-        let bundle = Bundle(for: ControlRouteTests.self)
-        let filePath = bundle.path(forResource: "38512A3B-032C-452D-95EA-06C845B64780", ofType: "json")!
-        let locations = Array<CLLocation>.locations(from: filePath)!
-        
-        let waypoints: [Waypoint] = [Waypoint(coordinate: locations.first!.coordinate),
-                                     Waypoint(coordinate: locations.last!.coordinate)]
-        
-        let route = Fixture.route(from: "38512A3B-032C-452D-95EA-06C845B64780.route", waypoints: waypoints)
-        route.accessToken = bogusToken
-        
-        let view = PlotRoute(frame: CGRect(origin: .zero, size: CGSize(width: 2000, height: 2000)))
-        view.route = route
-        view.rawLocations = locations
-        FBSnapshotVerifyView(view)
-    }
-    
     func testRerouteSignalJumps() {
         let bundle = Bundle(for: ControlRouteTests.self)
         let filePath = bundle.path(forResource: "7E2DC0D0-50E9-406F-857C-77FDE0A14B67", ofType: "json")!
