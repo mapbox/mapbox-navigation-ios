7c486b0de1508e3d281db339de6ca3944d5572f7
Bobby Sudekum <bobby@mapbox.com>
2018-02-09 08:40:06 -0800

    remove

--- MapboxSpeech.framework/Headers/MapboxSpeech.h
+++
@@ -1,7 +0,0 @@
-#import <UIKit/UIKit.h>
-
-//! Project version number for MapboxSpeech.
-FOUNDATION_EXPORT double MapboxSpeechVersionNumber;
-
-//! Project version string for MapboxSpeech.
-FOUNDATION_EXPORT const unsigned char MapboxSpeechVersionString[];
