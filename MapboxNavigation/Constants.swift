0dfec5bdab35a039cee1eb213a219a2fd410b618
Bobby Sudekum <bobby@mapbox.com>
2018-02-13 11:10:50 -0800

    Add constants file for MapboxNavigation

---
+++ MapboxNavigation/Constants.swift
@@ -0,0 +1,6 @@
+import Foundation
+
+/**
+ The minium distance remaining on a route before overhead zooming is stopped.
+ */
+public var NavigationMapViewMinimumDistanceForOverheadZooming: CLLocationDistance = 200
