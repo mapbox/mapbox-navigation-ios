7caadfde8dd3be7f5e686c707f03262307efed88
Fredrik Karlsson <bjorn.fredrik.karlsson@gmail.com>
2018-02-14 18:21:44 +0100

    Add script to format Podfile

---
+++ MapboxCoreNavigationTests/CocoaPodsTest/PodInstall/process_podfile.sh
@@ -0,0 +1,6 @@
+# Formats the Podfile according to the current PR, branch, and fork.
+USER_GH_SUFFIX=`git config --get remote.origin.url | cut -d ":" -f 2`
+GH_URL="https://github.com/$USER_GH_SUFFIX"
+
+sed -i '' "s;BRANCH_NAME;$BITRISE_GIT_BRANCH;g" Podfile
+sed -i '' "s;GIT_REPO_URL;"$GH_URL";g" Podfile
