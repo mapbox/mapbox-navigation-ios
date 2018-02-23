33b1a697eade39547f9b3a053bf29dedc6157d13
Bobby Sudekum <bobby@mapbox.com>
2018-02-23 12:38:14 -0800

    Add advanceBy test

--- MapboxCoreNavigationTests/RouteControllerTests.swift
+++ MapboxCoreNavigationTests/RouteControllerTests.swift
@@ -30,6 +30,22 @@ class RouteControllerTests: XCTestCase {
         XCTAssertFalse(navigation.userIsOnRoute(locationOffRoute), "User should be off route")
     }
     
+    func testAdvancingToFutureStepAndNotRerouting() {
+        let navigation = setup.routeController
+        let firstLocation = setup.firstLocation
+        navigation.locationManager(navigation.locationManager, didUpdateLocations: [firstLocation])
+        XCTAssertTrue(navigation.userIsOnRoute(firstLocation), "User should be on route")
+        XCTAssertEqual(navigation.routeProgress.currentLegProgress.stepIndex, 0, "User is on first step")
+        
+        let futureCoordinate = navigation.routeProgress.currentLegProgress.leg.steps[2].coordinates![10]
+        let futureLocation = CLLocation(latitude: futureCoordinate.latitude, longitude: futureCoordinate.longitude)
+        
+        navigation.locationManager(navigation.locationManager, didUpdateLocations: [futureLocation])
+        XCTAssertTrue(navigation.userIsOnRoute(futureLocation), "User should be on route")
+        
+        XCTAssertEqual(navigation.routeProgress.currentLegProgress.stepIndex, 2, "User should be on route and we should increment all the way to the 4th step")
+    }
+    
     func testSnappedLocation() {
         let navigation = setup.routeController
         let firstLocation = setup.firstLocation
