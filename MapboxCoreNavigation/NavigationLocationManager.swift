192afc215769115ab3eeedffe9c272f1a7d88e0b
Bobby Sudekum <bobby@mapbox.com>
2018-03-01 15:15:22 -0800

    Re-enable kCLLocationAccuracyBestForNavigation while phone is plugged in

--- MapboxCoreNavigation/NavigationLocationManager.swift
+++ MapboxCoreNavigation/NavigationLocationManager.swift
@@ -17,6 +17,17 @@ open class NavigationLocationManager: CLLocationManager {
     
     var lastKnownLocation: CLLocation?
     
+    /**
+     Indicates whether the device is plugged in or not.
+     */
+    public private(set) var isPluggedIn: Bool = false
+    
+    /**
+     Indicates whether the location manager’s desired accuracy should update
+     when the battery state changes.
+     */
+    public var automaticallyUpdatesDesiredAccuracy = true
+    
     override public init() {
         super.init()
         
@@ -35,6 +46,30 @@ open class NavigationLocationManager: CLLocationManager {
             }
         }
         
-        desiredAccuracy = kCLLocationAccuracyBestForNavigation
+        if #available(iOS 11.0, *) {
+            showsBackgroundLocationIndicator = true
+        }
+        
+        desiredAccuracy = kCLLocationAccuracyBest
+        
+        #if os(iOS)
+            UIDevice.current.addObserver(self, forKeyPath: "batteryState", options: [.initial, .new], context: nil)
+        #endif
+    }
+    
+    open override func observeValue(forKeyPath keyPath: String?, of object: Any?, change: [NSKeyValueChangeKey : Any]?, context: UnsafeMutableRawPointer?) {
+        if keyPath == "batteryState" {
+            let batteryState = UIDevice.current.batteryState
+            isPluggedIn = batteryState == .charging || batteryState == .full
+            
+            guard automaticallyUpdatesDesiredAccuracy else { return }
+            desiredAccuracy = isPluggedIn ? kCLLocationAccuracyBestForNavigation : kCLLocationAccuracyBest
+        }
+    }
+    
+    deinit {
+        #if os(iOS)
+            UIDevice.current.removeObserver(self, forKeyPath: "batteryState")
+        #endif
     }
 }
