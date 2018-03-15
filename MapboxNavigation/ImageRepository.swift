a7a75f5813507d7921a2ea55359953cc523b209c
Andrew Kitchen <andrew.kitchen@mapbox.com>
2018-03-15 15:59:55 -0700

    Introduce BimodalCache base protocol and new protocol for Data
    rename completion type
    [#1219]

--- MapboxNavigation/ImageRepository.swift
+++ MapboxNavigation/ImageRepository.swift
@@ -1,14 +1,5 @@
 import Foundation
 
-typealias NoArgBlock = () -> Void
-
-protocol BimodalImageCache {
-    func store(_ image: UIImage, forKey key: String, toDisk: Bool, completion completionBlock: NoArgBlock?)
-    func imageFromCache(forKey: String?) -> UIImage?
-    func clearMemory()
-    func clearDisk(completion: NoArgBlock?)
-}
-
 class ImageRepository {
 
     public var sessionConfiguration: URLSessionConfiguration = URLSessionConfiguration.default {
@@ -29,7 +20,7 @@ class ImageRepository {
         imageCache = cache
     }
 
-    func resetImageCache(_ completion: NoArgBlock?) {
+    func resetImageCache(_ completion: CompletionHandler?) {
         imageCache.clearMemory()
         imageCache.clearDisk(completion: completion)
     }
