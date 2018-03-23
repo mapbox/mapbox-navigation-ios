f30abc07b06ae231e6fc45b094d4b8a47d74d194
Bobby Sudekum <bobby@mapbox.com>
2018-03-23 08:23:07 -0700

    rm

--- Examples/Swift/ViewController.swift
+++ Examples/Swift/ViewController.swift
@@ -216,10 +216,8 @@ class ViewController: UIViewController, MGLMapViewDelegate, CLLocationManagerDel
             guard let routes = potentialRoutes else { return }
             return success(routes)
         }
-        
-        let directions = Directions(accessToken: "pk.eyJ1IjoiYnN1ZGVrdW0iLCJhIjoiY2loNDVmYWRwMDBocmlibHh5Z3J6bDAwMiJ9.KGUP5pu3aLWKsvFPPZ2EgA", host: "api-directions-maxspeed-internal.tilestream.net")
 
-        _ = directions.calculate(options, completionHandler: handler)
+        _ = Directions.shared.calculate(options, completionHandler: handler)
     }
 
     // MARK: Basic Navigation
