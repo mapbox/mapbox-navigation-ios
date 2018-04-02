612a7e027d7defbbc9a3750d60b270c57fbbbbb5
Bobby Sudekum <bobby@mapbox.com>
2018-04-02 10:36:39 -0700

    Don't use new func

--- MapboxNavigation/RouteMapViewController.swift
+++ MapboxNavigation/RouteMapViewController.swift
@@ -687,7 +687,7 @@ extension RouteMapViewController: NavigationViewDelegate {
         
         let closestCoordinate = location.coordinate
         let roadLabelLayerIdentifier = "roadLabelLayer"
-        var streetsSources = style.sources.compatibleFlatMap {
+        var streetsSources = style.sources.compactMap {
             $0 as? MGLVectorSource
             }.filter {
                 $0.isMapboxStreets
