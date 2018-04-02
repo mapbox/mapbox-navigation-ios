73b1a23e452c838ef7a14c5b015f2b0576f028f0
Bobby Sudekum <bobby@mapbox.com>
2018-04-02 10:36:39 -0700

    Don't use new func

--- MapboxCoreNavigation/sequence.swift
+++ MapboxCoreNavigation/sequence.swift
@@ -1,11 +1,9 @@
 import Foundation
 
 extension Sequence where Element: Hashable {
-    func compatibleFlatMap<ElementOfResult>(_ transform: (Element) throws -> ElementOfResult?) rethrows -> [ElementOfResult] {
-        #if swift(>=4.1)
-            return try compactMap(transform)
-        #else
-            return try flatMap(transform)
-        #endif
+    #if !swift(>=4.1)
+    func compactMap<ElementOfResult>(_ transform: (Element) throws -> ElementOfResult?) rethrows -> [ElementOfResult] {
+        return try flatMap(transform)
     }
+    #endif
 }
