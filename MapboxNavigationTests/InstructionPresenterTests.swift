423a669cd4da9a6c48c54bd48bc03bf43fc0f830
Fredrik Karlsson <bjorn.fredrik.karlsson@gmail.com>
2018-05-03 00:49:58 +0200

    Performance test abbrevations

--- MapboxNavigationTests/InstructionPresenterTests.swift
+++ MapboxNavigationTests/InstructionPresenterTests.swift
@@ -23,4 +23,21 @@ class InstructionPresenterTests: XCTestCase {
 
         XCTAssert(attachment is ExitAttachment, "Attachment for exit shield should be of type ExitAttachment")
     }
+    
+    func testAbbreviationPerformance() {
+        let route = Fixture.route(from: "route-with-banner-instructions", waypoints: [Waypoint(coordinate: CLLocationCoordinate2D(latitude: 37.795042, longitude: -122.413165)), Waypoint(coordinate: CLLocationCoordinate2D(latitude: 37.7727, longitude: -122.433378))])
+        
+        let allSteps = route.legs.flatMap { $0.steps }
+        let allInstructions = allSteps.flatMap { $0.instructionsDisplayedAlongStep?.first?.primaryInstruction }.filter { $0 != nil }
+        
+        let label = InstructionLabel(frame: CGRect(origin: .zero, size:CGSize(width: 10, height: 50)))
+        label.availableBounds = { return CGRect(origin: .zero, size: CGSize.iPhoneX) }
+        
+        self.measure {
+            for instruction in allInstructions {
+                let presenter = InstructionPresenter(instruction, dataSource: label, downloadCompletion: nil)
+                label.attributedText = presenter.attributedText()
+            }
+        }
+    }
 }
