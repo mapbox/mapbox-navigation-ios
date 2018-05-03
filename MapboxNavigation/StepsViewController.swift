d1ea455fb9318844d8d313d5d9e0914607a50894
Fredrik Karlsson <bjorn.fredrik.karlsson@gmail.com>
2018-05-03 18:15:51 +0200

    rename guard

--- MapboxNavigation/StepsViewController.swift
+++ MapboxNavigation/StepsViewController.swift
@@ -47,9 +47,9 @@ open class StepsViewController: UIViewController {
     func rebuildDataSourceIfNecessary() -> Bool {
         
         let legIndex = routeProgress.legIndex
+        let didProcessCurrentStep = previousLegIndex != legIndex && previousStepIndex != routeProgress.currentLegProgress.stepIndex
         
-        guard previousLegIndex != legIndex,
-            previousStepIndex != routeProgress.currentLegProgress.stepIndex else { return false }
+        guard !didProcessCurrentStep else { return false }
         
         sections.removeAll()
         
