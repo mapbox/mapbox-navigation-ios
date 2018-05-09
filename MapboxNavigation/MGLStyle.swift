7b690d45784b4ec06d67ce077d7528485cedec8a
Fredrik Karlsson <bjorn.fredrik.karlsson@gmail.com>
2018-05-09 18:44:55 +0200

    Document new MGLStyle methods

--- MapboxNavigation/MGLStyle.swift
+++ MapboxNavigation/MGLStyle.swift
@@ -5,17 +5,20 @@ extension MGLStyle {
     // Returns the URL to the current version of the Mapbox Navigation Guidance Day style.
     @objc
     public class var navigationGuidanceDayStyleURL: URL { get { return URL(string: "mapbox://styles/mapbox/navigation-guidance-day-v2")! } }
+    
     // Returns the URL to the current version of the Mapbox Navigation Guidance Night style.
     @objc
     public class var navigationGuidanceNightStyleURL: URL { get { return URL(string: "mapbox://styles/mapbox/navigation-guidance-night-v2")! } }
     
     @objc
-    public func navigationGuidanceDayStyle(version: Int) -> URL {
+    // Returns the URL to the given version of the navigation guidance style. Available version are 1, 2, and 3.
+    public class func navigationGuidanceDayStyleURL(version: Int) -> URL {
         return URL(string: "mapbox://styles/mapbox/navigation-guidance-day-v\(version)")!
     }
 
     @objc
-    public func navigationGuidanceNightStyle(version: Int) -> URL {
+    // Returns the URL to the given version of the navigation guidance style. Available version are 2, and 3.
+    public class func navigationGuidanceNightStyleURL(version: Int) -> URL {
         return URL(string: "mapbox://styles/mapbox/navigation-guidance-night-v\(version)")!
     }
 }
