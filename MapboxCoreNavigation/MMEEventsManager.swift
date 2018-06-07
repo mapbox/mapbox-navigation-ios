70c365bbe0bbc4634050b8fdb2e75be19f6e4217
Bobby Sudekum <bobby@mapbox.com>
2018-06-07 09:54:11 -0700

    Describe type for return values from map/reduce

--- MapboxCoreNavigation/MMEEventsManager.swift
+++ MapboxCoreNavigation/MMEEventsManager.swift
@@ -335,7 +335,7 @@ extension UIDevice {
             var systemInfo = utsname()
             uname(&systemInfo)
             let machineMirror = Mirror(reflecting: systemInfo.machine)
-            let identifier = machineMirror.children.reduce("") { identifier, element in
+            let identifier: String = machineMirror.children.reduce("") { (identifier: String, element: Mirror.Child) in
                 guard let value = element.value as? Int8, value != 0 else { return identifier }
                 return identifier + String(UnicodeScalar(UInt8(value)))
             }
--- MapboxCoreNavigation/RouteController.swift
+++ MapboxCoreNavigation/RouteController.swift
@@ -520,7 +520,7 @@ extension RouteController {
             _ = enqueueFoundFasterRouteEvent()
         }
 
-        if let lastReroute = outstandingFeedbackEvents.map({$0 as? RerouteEvent }).last {
+        if let lastReroute: RerouteEvent? = outstandingFeedbackEvents.map({$0 as? RerouteEvent }).last {
             lastReroute?.update(newRoute: routeProgress.route)
         }
 
@@ -992,7 +992,7 @@ extension RouteController: CLLocationManagerDelegate {
     func updateIntersectionDistances() {
         if let coordinates = routeProgress.currentLegProgress.currentStep.coordinates, let intersections = routeProgress.currentLegProgress.currentStep.intersections {
             let polyline = Polyline(coordinates)
-            let distances = intersections.map { polyline.distance(from: coordinates.first, to: $0.location) }
+            let distances: [CLLocationDistance] = intersections.map { polyline.distance(from: coordinates.first, to: $0.location) }
             routeProgress.currentLegProgress.currentStepProgress.intersectionDistances = distances
         }
     }
