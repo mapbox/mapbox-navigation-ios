116d58732a55a1823a0c6277349b480ebee1bee3
Bobby Sudekum <bobby@mapbox.com>
2018-06-08 15:57:45 -0700

    Initalize date formatter once

--- MapboxCoreNavigation/Date.swift
+++ MapboxCoreNavigation/Date.swift
@@ -1,17 +1,15 @@
 import Foundation
 
+let ISO8601Formatter: DateFormatter = {
+    let formatter = DateFormatter()
+    formatter.locale = Locale(identifier: "en_US_POSIX")
+    formatter.dateFormat = "yyyy-MM-dd'T'HH:mm:ss.SSSZ"
+    formatter.timeZone = TimeZone(secondsFromGMT: 0)
+    return formatter
+}()
+
 extension Date {
     var ISO8601: String {
-        return DateFormatter.ISO8601.string(from: self)
-    }
-}
-
-extension DateFormatter {
-    class var ISO8601: DateFormatter {
-        let formatter = DateFormatter()
-        formatter.locale = Locale(identifier: "en_US_POSIX")
-        formatter.dateFormat = "yyyy-MM-dd'T'HH:mm:ss.SSSZ"
-        formatter.timeZone = TimeZone(secondsFromGMT: 0)
-        return formatter
+        return ISO8601Formatter.string(from: self)
     }
 }
