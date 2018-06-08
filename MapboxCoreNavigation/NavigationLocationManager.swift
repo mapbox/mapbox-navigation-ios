2854be4f06fb998de062b2e80be9ad4f89d8d0cc
Bobby Sudekum <bobby@mapbox.com>
2018-06-08 10:04:10 -0700

    Fix desired accuracy

--- MapboxCoreNavigation/NavigationLocationManager.swift
+++ MapboxCoreNavigation/NavigationLocationManager.swift
@@ -9,37 +9,21 @@ import UIKit
 #endif
 
 /**
- `NavigationLocationManager` is the base location manager which handles
- permissions and background modes.
+ `NavigationLocationManager` is the base location manager which handles permissions and background modes.
+ 
+ If it's necessary to change the default behavior in `NavigationLocationManager`, you can subclass this class.
  */
 @objc(MBNavigationLocationManager)
 open class NavigationLocationManager: CLLocationManager {
     
     var lastKnownLocation: CLLocation?
     
-    /**
-     Indicates whether the location manager’s desired accuracy should update
-     when the battery state changes.
-     */
-    @available(*, deprecated, message: "Manually override desiredAccuracy instead.")
-    public var automaticallyUpdatesDesiredAccuracy = true
-    
-    var accuracyOverride: CLLocationAccuracy?
-    
-    override open var desiredAccuracy: CLLocationAccuracy {
-        get {
-            if let override = accuracyOverride { return override }
-            return  UIDevice.current.isPluggedIn ? kCLLocationAccuracyBestForNavigation : kCLLocationAccuracyBest
-        }
-        set {
-            accuracyOverride = newValue
-        }
-    }
-    
     override public init() {
         super.init()
         
         requestWhenInUseAuthorization()
+        
+        desiredAccuracy = UIDevice.current.isPluggedIn ? kCLLocationAccuracyBestForNavigation : kCLLocationAccuracyBest
         
         if #available(iOS 9.0, *) {
             if Bundle.main.backgroundModes.contains("location") {
