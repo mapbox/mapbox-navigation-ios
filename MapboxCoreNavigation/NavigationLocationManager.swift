3f0de0d8fa42df7efb6c43e849d879252144d534
Bobby Sudekum <bobby@mapbox.com>
2018-06-11 13:32:12 -0700

    always kCLLocationAccuracyBest for now

--- MapboxCoreNavigation/NavigationLocationManager.swift
+++ MapboxCoreNavigation/NavigationLocationManager.swift
@@ -10,8 +10,6 @@ import UIKit
 
 /**
  `NavigationLocationManager` is the base location manager which handles permissions and background modes.
- 
- If it's necessary to change the default behavior in `NavigationLocationManager`, you can subclass this class.
  */
 @objc(MBNavigationLocationManager)
 open class NavigationLocationManager: CLLocationManager {
@@ -23,7 +21,7 @@ open class NavigationLocationManager: CLLocationManager {
         
         requestWhenInUseAuthorization()
         
-        desiredAccuracy = UIDevice.current.isPluggedIn ? kCLLocationAccuracyBestForNavigation : kCLLocationAccuracyBest
+        desiredAccuracy = kCLLocationAccuracyBest
         
         if #available(iOS 9.0, *) {
             if Bundle.main.backgroundModes.contains("location") {
