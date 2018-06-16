012f1345b4bd65d3b150e63e39ce5b4b512efbe6
Bobby Sudekum <bobby@mapbox.com>
2018-06-15 17:15:45 -0700

    update

--- MapboxNavigation/CarPlayNavigationViewController.swift
+++ MapboxNavigation/CarPlayNavigationViewController.swift
@@ -2,77 +2,66 @@ import Foundation
 import MapboxDirections
 import MapboxCoreNavigation
 
-class CarPlayNavigationViewController: UIViewController, RouteControllerDelegate, StyleManagerDelegate {
-    func locationFor(styleManager: StyleManager) -> CLLocation {
-        return CLLocation(latitude: 0, longitude: 0)
-    }
+public class CarPlayNavigationViewController: UIViewController, MGLMapViewDelegate {
     
     var route: Route
     
-    var routeController: RouteController
+    var routeController: RouteController!
     
-    var directions: Directions
+    var directions: Directions!
     
     var styleManager: StyleManager!
     
-    var mapView: NavigationMapView!
+    var mapView: NavigationMapView?
     
-    public init(for route: Route,
-                directions: Directions,
-                styles: [Style] = [DayStyle(), NightStyle()],
-                locationManager: NavigationLocationManager = NavigationLocationManager()) {
-        
-        self.routeController = RouteController(along: route, directions: directions, locationManager: locationManager)
-        self.routeController.usesDefaultUserInterface = true
+    let voiceController = MapboxVoiceController()
+    
+    var mapHasLoaded = false {
+        didSet {
+//            resumeNotifications()
+        }
+    }
+    
+    public init(for route: Route) {
         self.route = route
-        self.directions = directions
-        
         super.init(nibName: nil, bundle: nil)
-        self.styleManager = StyleManager(self)
-        self.styleManager.styles = styles
-        self.routeController.delegate = self
         
-        routeController.resume()
-        resumeNotifications()
-        
-        NavigationSettings.shared.distanceUnit = route.routeOptions.locale.usesMetric ? .kilometer : .mile
-    }
-    
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        
-        let mapView = MGLMapView(frame: view.bounds)
-        mapView.autoresizingMask = [.flexibleWidth, .flexibleHeight]
-        
-        mapView.styleURL = MGLStyle.outdoorsStyleURL
-        
-        // Mauna Kea, Hawaii
-        let center = CLLocationCoordinate2D(latitude: 19.820689, longitude: -155.468038)
-        
-        // Optionally set a starting point.
-        mapView.setCenter(center, zoomLevel: 7, direction: 0, animated: false)
-        
-        view.addSubview(mapView)
-    }
-    
-    override func viewWillDisappear(_ animated: Bool) {
-        super.viewWillDisappear(animated)
-        suspendNotifications()
-        routeController.suspendLocationUpdates()
+//        routeController.resume()
     }
     
     required init?(coder aDecoder: NSCoder) {
         fatalError("init(coder:) has not been implemented")
     }
     
+    override public func viewDidLoad() {
+        super.viewDidLoad()
+        
+        mapView = NavigationMapView(frame: view.bounds)
+        mapView?.autoresizingMask = [.flexibleWidth, .flexibleHeight]
+        mapView?.delegate = self
+        
+        self.routeController = RouteController(along: route, directions: Directions.shared, locationManager: NavigationLocationManager())
+        
+        view.addSubview(mapView!)
+        
+        // Add listeners for progress updates
+        resumeNotifications()
+        
+        // Start navigation
+        routeController.resume()
+        
+        // Center map on user
+        mapView?.recenterMap()
+    }
+    
     func resumeNotifications() {
-        NotificationCenter.default.addObserver(self, selector: #selector(progressDidChange(_ :)), name: .routeControllerProgressDidChange, object: nil)
+        NotificationCenter.default.addObserver(self, selector: #selector(progressDidChange(_:)), name: .routeControllerProgressDidChange, object: nil)
         NotificationCenter.default.addObserver(self, selector: #selector(rerouted(_:)), name: .routeControllerDidReroute, object: nil)
     }
     
-    func suspendNotifications() {
-        NotificationCenter.default.removeObserver(self, name: .routeControllerProgressDidChange, object: nil)
-        NotificationCenter.default.removeObserver(self, name: .routeControllerWillReroute, object: nil)
+    
+    public func mapView(_ mapView: MGLMapView, didFinishLoading style: MGLStyle) {
+        self.mapView?.showRoutes([routeController.routeProgress.route])
     }
     
     @objc func progressDidChange(_ notification: NSNotification) {
@@ -81,13 +70,18 @@ class CarPlayNavigationViewController: UIViewController, RouteControllerDelegate
         
         // Add maneuver arrow
         if routeProgress.currentLegProgress.followOnStep != nil {
-            mapView.addArrow(route: routeProgress.route, legIndex: routeProgress.legIndex, stepIndex: routeProgress.currentLegProgress.stepIndex + 1)
+            mapView?.addArrow(route: routeProgress.route, legIndex: routeProgress.legIndex, stepIndex: routeProgress.currentLegProgress.stepIndex + 1)
         } else {
-            mapView.removeArrow()
+            mapView?.removeArrow()
         }
         
         // Update the user puck
-        mapView.updateCourseTracking(location: location, animated: true)
+        mapView?.updateCourseTracking(location: location, animated: true)
     }
+    
+    @objc func rerouted(_ notification: NSNotification) {
+        self.mapView?.showRoutes([routeController.routeProgress.route])
+    }
+    
 }
 
