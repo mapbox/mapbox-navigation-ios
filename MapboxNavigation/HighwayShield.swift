8751b639830520b15e8b3bf53b785b1d080ad3ac
Vincent Sam <vincent.sam@mapbox.com>
2018-07-24 14:08:49 -0400

    [GH-1440] - WIP Created a Highway shield struct to contain all the details for textColor and fillColor for current road feature.

---
+++ MapboxNavigation/HighwayShield.swift
@@ -0,0 +1,137 @@
+struct HighwayShield {
+    
+    enum textColor {
+        case black, white
+    }
+
+    enum fillColor {
+        case red, yellow, blue, green, white
+    }
+    
+    enum borderColor {
+        case black, white
+    }
+    
+    enum shield: String {
+        case `default`
+        case atMotorway = "at-motorway"
+        case atExpressway = "at-expressway"
+        case atStateB = "at-state-b"
+        case bgMotorway = "bg-motorway"
+        case bgNational = "bg-national"
+        case brFederal = "br-federal"
+        case brState = "br-state"
+        case chMotorway = "ch-motorway"
+        case chMain = "ch-main"
+        case czMotorway = "cz-motorway"
+        case czRoad = "cz-road"
+        case deMotorway = "de-motorway"
+        case deFederal = "de-federal"
+        case dkPrimary = "dk-primary"
+        case dkSecondary = "dk-secondary"
+        case fiMain = "fi-main"
+        case fiTrunk = "fi-trunk"
+        case fiRegional = "fi-regional"
+        case grMotorway = "gr-motorway"
+        case grNational = "gr-national"
+        case usHighway = "us-highway"
+        
+        func textColor() -> UIColor? {
+            switch self {
+            case .default:
+                return .black
+            case .atMotorway:
+                return .white
+            case .atExpressway:
+                return .white
+            case .atStateB:
+                return .white
+            case .bgMotorway:
+                return .white
+            case .bgNational:
+                return .white
+            case .brFederal:
+                return .black
+            case .brState:
+                return .black
+            case .chMotorway:
+                return .white
+            case .chMain:
+                return .white
+            case .czMotorway:
+                return .white
+            case .czRoad:
+                return .white
+            case .deMotorway:
+                return .white
+            case .deFederal:
+                return .black
+            case .dkPrimary:
+                return .black
+            case .dkSecondary:
+                return .black
+            case .fiMain:
+                return .white
+            case .fiTrunk:
+                return .black
+            case .fiRegional:
+                return .black
+            case .grMotorway:
+                return .white
+            case .grNational:
+                return .white
+            case .usHighway:
+                return .black
+            }
+        }
+        
+        func fillColor() -> UIColor {
+            switch self {
+            case .default:
+                return .white
+            case .atMotorway:
+                return .blue
+            case .atExpressway:
+                return .blue
+            case .atStateB:
+                return .blue
+            case .bgMotorway:
+                return .green
+            case .bgNational:
+                return .blue
+            case .brFederal:
+                return .white
+            case .brState:
+                return .white
+            case .chMotorway:
+                return .red
+            case .chMain:
+                return .blue
+            case .czMotorway:
+                return .blue
+            case .czRoad:
+                return .blue
+            case .deMotorway:
+                return .blue
+            case .deFederal:
+                return .yellow
+            case .dkPrimary:
+                return .yellow
+            case .dkSecondary:
+                return .white
+            case .fiMain:
+                return .red
+            case .fiTrunk:
+                return .yellow
+            case .fiRegional:
+                return .white
+            case .grMotorway:
+                return .green
+            case .grNational:
+                return .blue
+            case .usHighway:
+                return .white
+            }
+        }
+    }
+}
