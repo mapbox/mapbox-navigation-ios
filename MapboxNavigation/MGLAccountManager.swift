2ee884cf99a4c94d5e0d6f366e2d769c1f7cdcb3
M.Stephen <truestyle2005@163.com>
2018-07-24 15:59:16 +0800

    Change the default URL of .cn map

--- MapboxNavigation/MGLAccountManager.swift
+++ MapboxNavigation/MGLAccountManager.swift
@@ -1,11 +1,13 @@
 import Foundation
 import Mapbox
 
+
 extension MGLAccountManager{
     
     static let mapboxChinaBaseAPIURL = "https://api.mapbox.cn"
     static let mapboxChinaBaseURLHost = "api.mapbox.cn"
-    static let mapboxChinaStyleURL = "mapbox://styles/mapbox/streets-zh-v1"
+    static let mapboxChinaDayStyleURL = "mapbox://styles/mapbox/streets-zh-v1"
+    static let mapboxChinaNightStyleURL = "mapbox://styles/mapbox/dark-zh-v1"
     
     // Value of whether the map is China map or not
     public class var hasChinaBaseURL : Bool{
