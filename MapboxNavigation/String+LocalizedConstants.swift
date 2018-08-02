79785814d52e29df8df07e4b9c358a0a0eb0c5f6
Vincent Sam <vincent.sam@mapbox.com>
2018-08-01 22:32:26 -0400

    [GH-1569] - Changed the string extension from struct to enum.

--- MapboxNavigation/String+LocalizedConstants.swift
+++ MapboxNavigation/String+LocalizedConstants.swift
@@ -1,5 +1,5 @@
 extension String {
-    struct Localized {
+    enum Localized {
         static func simulationStatus(speed: Int) -> String {
             let format = NSLocalizedString("USER_IN_SIMULATION_MODE", bundle: .mapboxNavigation, value: "Simulating Navigation at %d×", comment: "The text of a banner that appears during turn-by-turn navigation when route simulation is enabled.")
             return String.localizedStringWithFormat(format, speed)
