eee8f5627b4b25d13fb049ba01015c54e275182a
Andrew Kitchen <andrew.kitchen@mapbox.com>
2018-08-06 16:28:31 -0700

    Some lightweight documentation on using the Swift AppDelegate
    We currently need to maintain a CarPlay and non-CarPlay example solely
    for the provisioning option. Other than for provisioning, these apps
    should contain identical code. If we wish to remove the CarPlay example
    it will require developers to select & deselect a provisioning profile
    which includes the CarPlay entitlement.

--- Examples/Swift/AppDelegate.swift
+++ Examples/Swift/AppDelegate.swift
@@ -2,6 +2,16 @@ import UIKit
 import MapboxNavigation
 import CarPlay
 
+/**
+ This example application delegate implementation is used in both our "Example-Swift" and our "Example-CarPlay" example apps.
+
+ In order to run the "Example-CarPlay" example app with CarPlay functionality enabled, one must first obtain a CarPlay entitlement from Apple.
+
+ Once the entitlement has been obtained and loaded into your ADC account:
+  - Create a provisioning profile which includes that entitlement
+  - Download and select that provisioning profile for the "Example-CarPlay" example app 
+  - Be sure to select an iOS simulator or device running iOS 12 or greater
+ **/
 @UIApplicationMain
 class AppDelegate: UIResponder, UIApplicationDelegate, CPApplicationDelegate {
 
