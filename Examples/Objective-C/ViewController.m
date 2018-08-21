cedabcb566202eeb00983790fbee597739a5a521
Jerrad Thramer <jerrad.thramer@mapbox.com>
2018-08-20 19:48:54 -0600

    Gah, forgot about the OBJ-C interface!

--- Examples/Objective-C/ViewController.m
+++ Examples/Objective-C/ViewController.m
@@ -125,7 +125,7 @@
         
         controller.directions = [MBDirections sharedDirections];
         controller.route = self.route;
-        controller.navigationService = [[MBNavigationService alloc] initWithRoute:self.route directions:controller.directions locationSource:locationManager eventsManager:nil];
+        controller.navigationService = [[MBNavigationService alloc] initWithRoute:self.route directions:controller.directions locationSource:locationManager eventsManager:nil simulated: NO];
         
 
     }
