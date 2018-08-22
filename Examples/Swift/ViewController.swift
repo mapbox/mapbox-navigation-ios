7bcc40dad871a4dd72cbc9eb2a133a7b56006636
Andrew Kitchen <andrew.kitchen@mapbox.com>
2018-08-22 14:26:20 -0700

    Restoring the example view controller's ability to dismiss itself

--- Examples/Swift/ViewController.swift
+++ Examples/Swift/ViewController.swift
@@ -383,7 +383,7 @@ extension ViewController: NavigationViewControllerDelegate {
     // Called when the user hits the exit button.
     // If implemented, you are responsible for also dismissing the UI.
     func navigationViewControllerDidDismiss(_ navigationViewController: NavigationViewController, byCanceling canceled: Bool) {
-        
+        navigationViewController.dismiss(animated: true, completion: nil)
     }
 }
 
