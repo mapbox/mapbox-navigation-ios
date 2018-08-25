bbc3ef8dc9360c65534ff0dfeffcad24c9ff2d3c
Vincent Sam <vincent.sam@mapbox.com>
2018-08-25 03:02:11 -0400

    Referenced the car play map panning increment constant in determining how to center the camera when the map view is panned.

--- MapboxNavigation/CarPlayManager.swift
+++ MapboxNavigation/CarPlayManager.swift
@@ -60,53 +60,6 @@ public class CarPlayManager: NSObject, CPInterfaceControllerDelegate, CPSearchTe
         shared = CarPlayManager()
     }
 
-    enum CPFavoritesList {
-
-        enum POI: RawRepresentable {
-            typealias RawValue = String
-            case mapboxSF, timesSquare
-
-            var subTitle: String {
-                switch self {
-                case .mapboxSF:
-                    return "Office Location"
-                case .timesSquare:
-                    return "Downtown Attractions"
-                }
-            }
-
-            var location: CLLocation {
-                switch self {
-                case .mapboxSF:
-                    return CLLocation(latitude: 37.7820776, longitude: -122.4155262)
-                case .timesSquare:
-                    return CLLocation(latitude: 40.758899, longitude: -73.9873197)
-                }
-            }
-            
-            var rawValue: String {
-                switch self {
-                case .mapboxSF:
-                    return "Mapbox SF"
-                case .timesSquare:
-                    return "Times Square"
-                }
-            }
-            
-            init?(rawValue: String) {
-                let value = rawValue.lowercased()
-                switch value {
-                case "mapbox sf":
-                    self = .mapboxSF
-                case "times square":
-                    self = .timesSquare
-                default:
-                    return nil
-                }
-            }
-        }
-    }
-
     // MARK: CPApplicationDelegate
 
     public func application(_ application: UIApplication, didConnectCarInterfaceController interfaceController: CPInterfaceController, to window: CPWindow) {
@@ -119,6 +72,8 @@ public class CarPlayManager: NSObject, CPInterfaceControllerDelegate, CPSearchTe
         self.carWindow = window
         
         let tap = UITapGestureRecognizer(target: self, action: #selector(handleScreenTap))
+        tap.numberOfTapsRequired = 1
+        tap.numberOfTouchesRequired = 1
         carWindow?.addGestureRecognizer(tap)
         
         let traitCollection = viewController.traitCollection
@@ -392,10 +347,8 @@ extension CarPlayManager: CPMapTemplateDelegate {
             facing = 270
         }
         
-        /// Distance in points that a single press of the panning button pans the map by.
-        let cpPanningIncrement: CLLocationDistance = 50
-        
-        let newCenter = camera.centerCoordinate.coordinate(at: cpPanningIncrement, facing: facing)
+        let newCenter = camera.centerCoordinate.coordinate(at: CarPlayMapViewPanningIncrement,
+                                                       facing: facing)
         camera.centerCoordinate = newCenter
         mapView.setCamera(camera, animated: true)
     }
