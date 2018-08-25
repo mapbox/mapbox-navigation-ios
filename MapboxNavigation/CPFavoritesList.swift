2a5fbb581b5fecd975b32411ccf9426aa54f7945
Vincent Sam <vincent.sam@mapbox.com>
2018-08-25 03:04:16 -0400

    Extracted the CPFavoritesList from the car play manager and consolidated it in a new file.

--- MapboxNavigation/CPFavoritesList.swift
+++ MapboxNavigation/CPFavoritesList.swift
@@ -1,9 +1,46 @@
-//
-//  CPFavoritesList.swift
-//  MapboxNavigation
-//
-//  Created by Vincent Sam on 8/25/18.
-//  Copyright © 2018 Mapbox. All rights reserved.
-//
-
-import Foundation
+public enum CPFavoritesList {
+    
+    enum POI: RawRepresentable {
+        typealias RawValue = String
+        case mapboxSF, timesSquare
+        
+        var subTitle: String {
+            switch self {
+            case .mapboxSF:
+                return "Office Location"
+            case .timesSquare:
+                return "Downtown Attractions"
+            }
+        }
+        
+        var location: CLLocation {
+            switch self {
+            case .mapboxSF:
+                return CLLocation(latitude: 37.7820776, longitude: -122.4155262)
+            case .timesSquare:
+                return CLLocation(latitude: 40.758899, longitude: -73.9873197)
+            }
+        }
+        
+        var rawValue: String {
+            switch self {
+            case .mapboxSF:
+                return "Mapbox SF"
+            case .timesSquare:
+                return "Times Square"
+            }
+        }
+        
+        init?(rawValue: String) {
+            let value = rawValue.lowercased()
+            switch value {
+            case "mapbox sf":
+                self = .mapboxSF
+            case "times square":
+                self = .timesSquare
+            default:
+                return nil
+            }
+        }
+    }
+}
