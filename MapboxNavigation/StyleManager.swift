2f404825504ea7325e6ae76fe22c0dbb08271749
Jean-Baptiste Quesney <jean.baptiste.quesney.ext@mappy.com>
2018-08-27 14:27:16 +0200

    Try to re-schedule sunset/sunrise event for automatic style switching when assigning the styles property of StyleManager.

--- MapboxNavigation/StyleManager.swift
+++ MapboxNavigation/StyleManager.swift
@@ -56,6 +56,7 @@ open class StyleManager: NSObject {
     @objc public var styles = [Style]() {
         didSet {
             applyStyle()
+            resetTimeOfDayTimer()
         }
     }
     
