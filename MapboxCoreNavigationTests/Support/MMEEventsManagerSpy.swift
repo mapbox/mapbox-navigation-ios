1ff1484f88f342b9078f7046b1147115828c8b93
Andrew Kitchen <andrew.kitchen@mapbox.com>
2018-09-10 12:51:10 -0700

    Introduce a TestNavigationEventsManager, with a stubbed/spy MMEEventsManager

--- MapboxCoreNavigationTests/Support/MMEEventsManagerSpy.swift
+++ MapboxCoreNavigationTests/Support/MMEEventsManagerSpy.swift
@@ -57,3 +57,12 @@ class MMEEventsManagerSpy: MMEEventsManager {
         }.count
     }
 }
+
+import MapboxCoreNavigation
+
+class TestNavigationEventsManager: EventsManager {
+    init() {
+        super.init(accessToken: "not a real token")
+        self.manager = MMEEventsManagerSpy()
+    }
+}
