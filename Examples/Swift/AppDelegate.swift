072905fdda82868e99d21ded4b457533047b5e49
Fredrik Karlsson <bjorn.fredrik.karlsson@gmail.com>
2018-09-12 22:14:52 +0200

    Move CarPlay doc to relevant app delegate

--- Examples/Swift/AppDelegate.swift
+++ Examples/Swift/AppDelegate.swift
@@ -1,16 +1,7 @@
 import UIKit
 import MapboxNavigation
 
-/**
- This example application delegate implementation is used in both our "Example-Swift" and our "Example-CarPlay" example apps.
 
- In order to run the "Example-CarPlay" example app with CarPlay functionality enabled, one must first obtain a CarPlay entitlement from Apple.
-
- Once the entitlement has been obtained and loaded into your ADC account:
-  - Create a provisioning profile which includes the entitlement
-  - Download and select the provisioning profile for the "Example-CarPlay" example app
-  - Be sure to select an iOS simulator or device running iOS 12 or greater
- **/
 @UIApplicationMain
 class AppDelegate: UIResponder, UIApplicationDelegate {
 
