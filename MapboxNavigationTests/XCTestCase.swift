e8d78fcd98f9c52afb7ae5c7d282d8fc3aa483fe
Andrew Kitchen <andrew.kitchen@mapbox.com>
2018-09-12 20:23:48 -0700

    Refactor to reuse runUntil() to troubleshoot flaky test on Xcode ~10

--- MapboxNavigationTests/XCTestCase.swift
+++ MapboxNavigationTests/XCTestCase.swift
@@ -9,4 +9,20 @@ extension XCTestCase {
 
         static let pollingInterval: TimeInterval = 0.05
     }
+
+    func runUntil(_ condition: () -> Bool, testCase: String = #function) {
+        runUntil(condition: condition, testCase: testCase, pollingInterval: NavigationTests.pollingInterval, until: NavigationTests.timeout)
+    }
+
+    func runUntil(condition: () -> Bool, testCase: String, pollingInterval: TimeInterval, until timeout: DispatchTime) {
+        guard (timeout >= DispatchTime.now()) else {
+            XCTFail("Timeout occurred in \(testCase)")
+            return
+        }
+
+        if condition() == false {
+            RunLoop.current.run(until: Date(timeIntervalSinceNow: pollingInterval))
+            runUntil(condition: condition, testCase: testCase, pollingInterval: pollingInterval, until: timeout)
+        }
+    }
 }
