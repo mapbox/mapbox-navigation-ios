5de613237d68c35e5578094501e1a941bacf522f
Nick <nick.italiano@mapbox.com>
2018-09-29 08:14:06 -0700

    Adds routeProgress.remainingLegs and cleansup routeProgress.remainingSteps

--- MapboxCoreNavigation/RouteProgress.swift
+++ MapboxCoreNavigation/RouteProgress.swift
@@ -37,32 +37,17 @@ open class RouteProgress: NSObject {
     }
 
     /**
+     Returns the remaining legs left on the current route
+     */
+    @objc public var remainingLegs: [RouteLeg] {
+        return Array(route.legs.suffix(from: legIndex + 1))
+    }
+
+    /**
      Returns the remaining steps left on the current route
      */
     @objc public var remainingSteps: [RouteStep] {
-        var steps = [RouteStep]()
-        
-        // get all remaining steps on currentLeg
-        let curStepIndex = currentLegProgress.stepIndex
-        var curLegSteps = currentLeg.steps.suffix(from: curStepIndex)
-        _ = curLegSteps.popLast()
-        
-        for step in curLegSteps {
-            steps.append(step)
-        }
-        
-        // get all remaining steps after currentLeg
-        let remainingLegs = route.legs.suffix(from: legIndex + 1)
-        for leg in remainingLegs {
-            var curLegSteps = leg.steps
-            _ = curLegSteps.popLast()
-            
-            for step in curLegSteps {
-                steps.append(step)
-            }
-        }
-        
-        return steps
+        return currentLegProgress.remainingSteps + remainingLegs.flatMap { $0.steps }
     }
     
     /**
