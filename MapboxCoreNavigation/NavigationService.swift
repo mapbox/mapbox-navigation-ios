8cef1bacba7c028364f23adcdc98367f83d763ae
Fredrik Karlsson <bjorn.fredrik.karlsson@gmail.com>
2018-10-19 11:46:39 +0200

    Set initial location

--- MapboxCoreNavigation/NavigationService.swift
+++ MapboxCoreNavigation/NavigationService.swift
@@ -315,6 +315,14 @@ public class MapboxNavigationService: NSObject, NavigationService, DefaultInterf
     }
     
     public func start() {
+        // Jump to the first coordinate on the route if the location source does
+        // not yet have a fixed location.
+        if router.location == nil,
+            let coordinate = route.coordinates?.first {
+            let location = CLLocation(latitude: coordinate.latitude, longitude: coordinate.longitude)
+            router.locationManager?(nativeLocationSource, didUpdateLocations: [location])
+        }
+        
         nativeLocationSource.startUpdatingHeading()
         nativeLocationSource.startUpdatingLocation()
         
