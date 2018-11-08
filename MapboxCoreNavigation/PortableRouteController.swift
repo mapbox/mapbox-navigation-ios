fffec9d699e29902bddbb4fcb346a88147dbf970
Dave Prukop <dave.prukop@mapbox.com>
2018-11-08 09:52:58 -0800

    Added method to control minimum speed threshold for predicted locations

--- MapboxCoreNavigation/PortableRouteController.swift
+++ MapboxCoreNavigation/PortableRouteController.swift
@@ -89,6 +89,10 @@ open class PortableRouteController: RouteController {
         return navigator.getHistory()
     }
     
+    public func setMinimumSpeedForLocationProjection(metersPerSecond: Double = 7.0) {
+        navigator.configureNavigator(forName: "min_prediction_speed", value: String(metersPerSecond))
+    }
+    
     public func projectedLocation(date: Date, projectedSeconds: TimeInterval) -> (coordinate: CLLocationCoordinate2D, course: CLLocationDirection) {
         let projectedDate = date.addingTimeInterval(projectedSeconds) // + projectedMilliseconds
         let status = navigator.getStatusForTimestamp(projectedDate)
