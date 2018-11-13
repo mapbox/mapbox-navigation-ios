bbf2a92c53159a3af5d971b2c77c437fad905a24
Fredrik Karlsson <bjorn.fredrik.karlsson@gmail.com>
2018-11-13 11:23:15 +0100

    Remove unused UI tests

--- BenchUITests/BenchUITests.swift
+++ BenchUITests/BenchUITests.swift
@@ -9,39 +9,6 @@ class BenchUITests: XCTestCase {
         continueAfterFailure = false
         XCUIApplication().launch()
     }
-
-    func testControlRoute1() {
-        let app = XCUIApplication()
-
-        measureMetrics([.wallClockTime], automaticallyStartMeasuring: false) {
-            startMeasuring()
-            app.tables.staticTexts["DCA to Arboretum"].tap()
-
-            let endNavigation = app.staticTexts["End Navigation"]
-            let exists = NSPredicate(format: "endNavigation == 1")
-            expectation(for: exists, evaluatedWith: endNavigation, handler: nil)
-            waitForExpectations(timeout: 20*60, handler: nil)
-
-            stopMeasuring()
-        }
-    }
-    
-    func testControlRoute2() {
-        let app = XCUIApplication()
-        
-        measureMetrics([.wallClockTime], automaticallyStartMeasuring: false) {
-            startMeasuring()
-            
-            app.tables.staticTexts["Pipe Fitters Union to Four Seasons Boston"].tap()
-            
-            let endNavigation = app.staticTexts["End Navigation"]
-            let exists = NSPredicate(format: "endNavigation == 1")
-            _ = self.expectation(for: exists, evaluatedWith: endNavigation, handler: nil)
-            self.waitForExpectations(timeout: 11*60, handler: nil)
-            
-            stopMeasuring()
-        }
-    }
     
     func testTemporaryRoute() {
         let app = XCUIApplication()
