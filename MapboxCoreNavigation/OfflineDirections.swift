d9e81473f0b11b68db1ad225fc70a7997cec87ac
Minh Nguyễn <mxn@1ec5.org>
2018-12-04 22:05:40 -0800

    Continued renaming OfflineDirections to NavigationDirections
    Also fixed some copypasta in documentation comments.

--- MapboxCoreNavigation/OfflineDirections.swift
+++ MapboxCoreNavigation/OfflineDirections.swift
@@ -2,7 +2,10 @@ import Foundation
 import MapboxDirections
 import MapboxNavigationNative
 
-public typealias OfflineDirectionsCompletionHandler = (_ numberOfTiles: UInt64) -> Void
+/**
+ A closure to call when the `NavigationDirections` router has been configured completely.
+ */
+public typealias NavigationDirectionsCompletionHandler = (_ numberOfTiles: UInt64) -> Void
 
 enum OfflineRoutingError: Error, LocalizedError {
     case unexpectedRouteResult(String)
@@ -25,22 +28,31 @@ enum OfflineRoutingError: Error, LocalizedError {
     }
 }
 
-struct OfflineDirectionsConstants {
+struct NavigationDirectionsConstants {
     static let offlineSerialQueueLabel = Bundle.mapboxCoreNavigation.bundleIdentifier!.appending(".offline")
     static let unpackSerialQueueLabel = Bundle.mapboxCoreNavigation.bundleIdentifier!.appending(".offline.unpack")
-    static let offlineSerialQueue = DispatchQueue(label: OfflineDirectionsConstants.offlineSerialQueueLabel)
-    static let unpackSerialQueue = DispatchQueue(label: OfflineDirectionsConstants.unpackSerialQueueLabel)
+    static let offlineSerialQueue = DispatchQueue(label: NavigationDirectionsConstants.offlineSerialQueueLabel)
+    static let unpackSerialQueue = DispatchQueue(label: NavigationDirectionsConstants.unpackSerialQueueLabel)
 }
 
 /**
- A `NavigationDirections` object provides you with optimal directions between different locations, or waypoints. The directions object passes your request to the [Mapbox Directions API](https://www.mapbox.com/api-documentation/?language=Swift#directions) and returns the requested information to a closure (block) that you provide. A directions object can handle multiple simultaneous requests. A `RouteOptions` object specifies criteria for the results, such as intermediate waypoints, a mode of transportation, or the level of detail to be returned. In addition to `Directions`, `NavigationDiretions` provides support for offline routing.
+ A `NavigationDirections` object provides you with optimal directions between different locations, or waypoints. The directions object passes your request to a built-in routing engine and returns the requested information to a closure (block) that you provide. A directions object can handle multiple simultaneous requests. A `RouteOptions` object specifies criteria for the results, such as intermediate waypoints, a mode of transportation, or the level of detail to be returned. In addition to `Directions`, `NavigationDirections` provides support for offline routing.
  
  Each result produced by the directions object is stored in a `Route` object. Depending on the `RouteOptions` object you provide, each route may include detailed information suitable for turn-by-turn directions, or it may include only high-level information such as the distance, estimated travel time, and name of each leg of the trip. The waypoints that form the request may be conflated with nearby locations, as appropriate; the resulting waypoints are provided to the closure.
  */
 @objc(MBNavigationDirections)
 public class NavigationDirections: Directions {
-    
+    /**
+     A closure to call when an unpacking operation has made some progress.
+     
+     - parameter totalBytes: The total size of tile pack in bytes.
+     - parameter remainingBytes: The remaining number of bytes left to download.
+     */
     public typealias UnpackProgressHandler = (_ totalBytes: UInt64, _ remainingBytes: UInt64) -> ()
+    
+    /**
+     A closure to call once an unpacking operation has completed.
+     */
     public typealias UnpackCompletionHandler = (_ result: UInt64, _ error: Error?) -> ()
     
     public override init(accessToken: String? = nil, host: String? = nil) {
@@ -48,16 +60,15 @@ public class NavigationDirections: Directions {
     }
     
     /**
-     Configures the router with the given set of tiles
+     Configures the router with the given set of tiles.
      
      - parameter tilesURL: The location where the tiles has been sideloaded to.
      - parameter translationsURL: The location where the translations has been downloaded to.
-     - parameter accessToken: A Mapbox [access token](https://www.mapbox.com/help/define-access-token/). If an access token is not specified when initializing the directions object, it should be specified in the `MGLMapboxAccessToken` key in the main application bundle’s Info.plist.
-     - parameter host: An optional hostname to the server API. The [Mapbox Directions API](https://www.mapbox.com/api-documentation/?language=Swift#directions) endpoint is used by default.
+     - parameter completionHandler: A block that is called when the router is completely configured.
      */
-    public func configureRouter(tilesURL: URL, translationsURL: URL? = nil, completionHandler: @escaping OfflineDirectionsCompletionHandler) {
+    public func configureRouter(tilesURL: URL, translationsURL: URL? = nil, completionHandler: @escaping NavigationDirectionsCompletionHandler) {
         
-        OfflineDirectionsConstants.offlineSerialQueue.sync {
+        NavigationDirectionsConstants.offlineSerialQueue.sync {
             // Translations files bundled within navigation native
             // will be used when passing an empty string to `translationsPath`.
             let tileCount = self.navigator.configureRouter(forTilesPath: tilesURL.path, translationsPath: translationsURL?.path ?? "")
@@ -79,14 +90,14 @@ public class NavigationDirections: Directions {
      */
     public class func unpackTilePack(at filePathURL: URL, outputDirectoryURL: URL, progressHandler: UnpackProgressHandler?, completionHandler: UnpackCompletionHandler?) {
         
-        OfflineDirectionsConstants.offlineSerialQueue.sync {
+        NavigationDirectionsConstants.offlineSerialQueue.sync {
             
             let totalPackedBytes = filePathURL.fileSize!
             
             // Report 0% progress
             progressHandler?(totalPackedBytes, totalPackedBytes)
             
-            var timer: DispatchTimer? = DispatchTimer(countdown: .seconds(500), accuracy: .seconds(500), executingOn: OfflineDirectionsConstants.unpackSerialQueue) {
+            var timer: DispatchTimer? = DispatchTimer(countdown: .seconds(500), accuracy: .seconds(500), executingOn: NavigationDirectionsConstants.unpackSerialQueue) {
                 if let remainingBytes = filePathURL.fileSize {
                     progressHandler?(totalPackedBytes, remainingBytes)
                 }
@@ -131,7 +142,7 @@ public class NavigationDirections: Directions {
         
         let url = self.url(forCalculating: options)
         
-        OfflineDirectionsConstants.offlineSerialQueue.async { [weak self] in
+        NavigationDirectionsConstants.offlineSerialQueue.async { [weak self] in
             
             guard let result = self?.navigator.getRouteForDirectionsUri(url.absoluteString) else {
                 let message = NSLocalizedString("OFFLINE_NO_RESULT", bundle: .mapboxCoreNavigation, value: "Unable to calculate the requested route while offline.", comment: "Error description when an offline route request returns no result")
@@ -171,7 +182,7 @@ public class NavigationDirections: Directions {
     var _navigator: MBNavigator!
     var navigator: MBNavigator {
         
-        assert(currentQueueName() == OfflineDirectionsConstants.offlineSerialQueueLabel,
+        assert(currentQueueName() == NavigationDirectionsConstants.offlineSerialQueueLabel,
                "The offline navigator must be accessed from the dedicated serial queue")
         
         if _navigator == nil {
