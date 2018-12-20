c815b391d5be0317eedab753a5eda72071bbf651
Fredrik Karlsson <bjorn.fredrik.karlsson@gmail.com>
2018-12-20 15:01:10 +0100

    Update RouteProgressTests for the updated test fixture

--- MapboxCoreNavigationTests/RouteProgressTests.swift
+++ MapboxCoreNavigationTests/RouteProgressTests.swift
@@ -7,27 +7,27 @@ class RouteProgressTests: XCTestCase {
     func testRouteProgress() {
         let routeProgress = RouteProgress(route: route)
         XCTAssertEqual(routeProgress.fractionTraveled, 0)
-        XCTAssertEqual(routeProgress.distanceRemaining, 4316.9)
+        XCTAssertEqual(routeProgress.distanceRemaining, 4054.2)
         XCTAssertEqual(routeProgress.distanceTraveled, 0)
-        XCTAssertEqual(round(routeProgress.durationRemaining), 764)
+        XCTAssertEqual(round(routeProgress.durationRemaining), 858)
     }
     
     func testRouteLegProgress() {
         let routeProgress = RouteProgress(route: route)
-        XCTAssertEqual(routeProgress.currentLeg.description, "Sacramento Street, Gough Street")
+        XCTAssertEqual(routeProgress.currentLeg.description, "Hyde Street, Page Street")
         XCTAssertEqual(routeProgress.currentLegProgress.distanceTraveled, 0)
-        XCTAssertEqual(round(routeProgress.currentLegProgress.durationRemaining), 764)
+        XCTAssertEqual(round(routeProgress.currentLegProgress.durationRemaining), 858)
         XCTAssertEqual(routeProgress.currentLegProgress.fractionTraveled, 0)
         XCTAssertEqual(routeProgress.currentLegProgress.stepIndex, 0)
-        XCTAssertEqual(routeProgress.currentLegProgress.followOnStep?.description, "Turn left onto Gough Street")
-        XCTAssertEqual(routeProgress.currentLegProgress.upcomingStep?.description, "Turn right onto Sacramento Street")
+        XCTAssertEqual(routeProgress.currentLegProgress.followOnStep?.description, "Turn left onto Hyde Street")
+        XCTAssertEqual(routeProgress.currentLegProgress.upcomingStep?.description, "Turn right onto California Street")
     }
     
     func testRouteStepProgress() {
         let routeProgress = RouteProgress(route: route)
-        XCTAssertEqual(routeProgress.currentLegProgress.currentStepProgress.distanceRemaining, 279.8)
+        XCTAssertEqual(routeProgress.currentLegProgress.currentStepProgress.distanceRemaining, 384.1)
         XCTAssertEqual(routeProgress.currentLegProgress.currentStepProgress.distanceTraveled, 0)
-        XCTAssertEqual(routeProgress.currentLegProgress.currentStepProgress.durationRemaining, 79.2)
+        XCTAssertEqual(routeProgress.currentLegProgress.currentStepProgress.durationRemaining, 86.6, accuracy: 0.001)
         XCTAssertEqual(routeProgress.currentLegProgress.currentStepProgress.fractionTraveled, 0)
         XCTAssertEqual(routeProgress.currentLegProgress.currentStepProgress.userDistanceToManeuverLocation, Double.infinity)
         XCTAssertEqual(routeProgress.currentLegProgress.currentStepProgress.step.description, "Head south on Taylor Street")
@@ -37,11 +37,11 @@ class RouteProgressTests: XCTestCase {
         let routeProgress = RouteProgress(route: route)
         routeProgress.currentLegProgress.stepIndex = 1
         XCTAssertEqual(routeProgress.currentLegProgress.currentStepProgress.spokenInstructionIndex, 0)
-        XCTAssertEqual(routeProgress.currentLegProgress.currentStepProgress.distanceRemaining, 1171)
+        XCTAssertEqual(routeProgress.currentLegProgress.currentStepProgress.distanceRemaining, 439.1)
         XCTAssertEqual(routeProgress.currentLegProgress.currentStepProgress.distanceTraveled, 0)
-        XCTAssertEqual(round(routeProgress.currentLegProgress.currentStepProgress.durationRemaining), 194)
+        XCTAssertEqual(round(routeProgress.currentLegProgress.currentStepProgress.durationRemaining), 73)
         XCTAssertEqual(routeProgress.currentLegProgress.currentStepProgress.fractionTraveled, 0)
         XCTAssertEqual(routeProgress.currentLegProgress.currentStepProgress.userDistanceToManeuverLocation, Double.infinity)
-        XCTAssertEqual(routeProgress.currentLegProgress.currentStepProgress.step.description, "Turn right onto Sacramento Street")
+        XCTAssertEqual(routeProgress.currentLegProgress.currentStepProgress.step.description, "Turn right onto California Street")
     }
 }
