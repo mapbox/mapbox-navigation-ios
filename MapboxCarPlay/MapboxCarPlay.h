0cfe689f59142adca6df1cb6452cd8aeb93c2710
Vincent Sam <vincent.sam@mapbox.com>
2019-01-23 10:56:22 -0500

    Initial development efforts for extracting files into the CarPlay Framework.

---
+++ MapboxCarPlay/MapboxCarPlay.h
@@ -0,0 +1,19 @@
+//
+//  MapboxCarPlay.h
+//  MapboxCarPlay
+//
+//  Created by Vincent Sam on 1/23/19.
+//  Copyright © 2019 Mapbox. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for MapboxCarPlay.
+FOUNDATION_EXPORT double MapboxCarPlayVersionNumber;
+
+//! Project version string for MapboxCarPlay.
+FOUNDATION_EXPORT const unsigned char MapboxCarPlayVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <MapboxCarPlay/PublicHeader.h>
+
+
