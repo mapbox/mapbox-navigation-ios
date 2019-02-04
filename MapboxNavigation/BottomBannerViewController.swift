6232eeec3cde996af8f14d81af2a855ce6268f63
Jerrad Thramer <Jerrad.Thramer@mapbox.com>
2019-02-04 15:15:20 -0700

    Adding docs for the BBVC Delegate, and NavigationCustomizable.

--- MapboxNavigation/BottomBannerViewController.swift
+++ MapboxNavigation/BottomBannerViewController.swift
@@ -2,7 +2,15 @@ import UIKit
 import MapboxCoreNavigation
 import MapboxDirections
 
+/**
+ `BottomBannerViewControllerDelegate` provides a method for reacting to the user tapping on the "cancel" button in the `BottomBannerViewController`.
+ */
 public protocol BottomBannerViewControllerDelegate: class {
+    
+    /**
+     A method that is invoked when the user taps on the cancel button.
+     - parameter sender: The button that originated the tap event.
+     */
     func didTapCancel(_ sender: Any)
 }
 
