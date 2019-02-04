8e76dd59c21b3e62970a1c859a02addccd98285e
Vincent Sam <vincent.sam@mapbox.com>
2019-02-04 17:28:14 -0500

    Removed redundant comment

--- MapboxCarPlay/MapboxCarPlay.h
+++ MapboxCarPlay/MapboxCarPlay.h
@@ -6,6 +6,4 @@ FOUNDATION_EXPORT double MapboxCarPlayVersionNumber;
 //! Project version string for MapboxCarPlay.
 FOUNDATION_EXPORT const unsigned char MapboxCarPlayVersionString[];
 
-// In this header, you should import all the public headers of your framework using statements like #import <MapboxCarPlay/PublicHeader.h>
-
 
