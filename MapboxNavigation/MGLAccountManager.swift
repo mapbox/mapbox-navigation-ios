05121088b43a13c5d69fbf413900cd28690154ef
Fredrik Karlsson <bjorn.fredrik.karlsson@gmail.com>
2019-03-08 17:07:23 +0100

    Remove unused constant

--- MapboxNavigation/MGLAccountManager.swift
+++ MapboxNavigation/MGLAccountManager.swift
@@ -7,9 +7,6 @@ extension MGLAccountManager{
     // Mapbox China base API URL
     static let mapboxChinaBaseAPIURL = "https://api.mapbox.cn"
     
-    //Mapbox China base URL host.
-    static let mapboxChinaBaseURLHost = "api.mapbox.cn"
-    
     /**
      Returns true if the map's endpoint is China.
      */
