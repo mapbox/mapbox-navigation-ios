5c8d40c9438a9fb169d55ec35ffe383ad67e9ef3
Jerrad Thramer <887225+JThramer@users.noreply.github.com>
2019-03-11 17:45:55 -0600

    Update CarPlayManagerDelegate.swift
    Let's remove the term "alternate".

--- MapboxNavigation/CarPlayManagerDelegate.swift
+++ MapboxNavigation/CarPlayManagerDelegate.swift
@@ -57,7 +57,7 @@ public protocol CarPlayManagerDelegate {
     optional func carPlayManager(_ carPlayManager: CarPlayManager, mapButtonsCompatibleWith traitCollection: UITraitCollection, in template: CPTemplate, for activity: CarPlayActivity) -> [CPMapButton]?
     
     /**
-     Asks the delegate to provide an alternate navigation service. If no delegate is set, a default built-in MapboxNavigationService will be created and used.
+     Asks the delegate to provide a navigation service. If no delegate is set, a default built-in MapboxNavigationService will be created and used.
      
      - parameter carPlayManager: The CarPlay manager instance.
      - parameter route: The route for which the returned route controller will manage location updates.
