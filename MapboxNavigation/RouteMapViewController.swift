9ffa3dba514e6582a45f0dd4c23d7cf26ffb611d
Fredrik Karlsson <bjorn.fredrik.karlsson@gmail.com>
2019-03-20 19:17:23 +0100

    Fix sticky course view and consistency between edge padding and content insets

--- MapboxNavigation/RouteMapViewController.swift
+++ MapboxNavigation/RouteMapViewController.swift
@@ -73,7 +73,7 @@ extension RouteMapViewController: NavigationComponent {
         
         if isInOverviewMode {
             if let coordinates = route.coordinates, let userLocation = router.location?.coordinate {
-                mapView.setOverheadCameraView(from: userLocation, along: coordinates, for: overheadInsets)
+                mapView.setOverheadCameraView(from: userLocation, along: coordinates, contentInsets: contentInset(forOverviewing: true))
             }
         } else {
             mapView.tracksUserCourse = true
@@ -102,7 +102,6 @@ extension RouteMapViewController: NavigationComponent {
     func navigationService(_ service: NavigationService, didFailToRerouteWith error: Error) {
         statusView.hide()
     }
-    
 }
 
 
@@ -236,7 +235,7 @@ class RouteMapViewController: UIViewController {
         super.viewDidLoad()
         
         let mapView = self.mapView
-        mapView.contentInset = contentInsets
+        mapView.contentInset = contentInset(forOverviewing: false)
         view.layoutIfNeeded()
 
         mapView.tracksUserCourse = true
@@ -351,8 +350,9 @@ class RouteMapViewController: UIViewController {
 
     @objc func toggleOverview(_ sender: Any) {
         mapView.enableFrameByFrameCourseViewTracking(for: 3)
-        if let coordinates = router.route.coordinates, let userLocation = router.location?.coordinate {
-            mapView.setOverheadCameraView(from: userLocation, along: coordinates, for: overheadInsets)
+        if let coordinates = router.route.coordinates,
+            let userLocation = router.location?.coordinate {
+            mapView.setOverheadCameraView(from: userLocation, along: coordinates, contentInsets: contentInset(forOverviewing: true))
         }
         isInOverviewMode = true
     }
@@ -381,7 +381,7 @@ class RouteMapViewController: UIViewController {
 
     override func viewDidLayoutSubviews() {
         super.viewDidLayoutSubviews()
-        mapView.setContentInset(contentInsets, animated: true)
+        mapView.setContentInset(contentInset(forOverviewing: isInOverviewMode), animated: true)
         mapView.setNeedsUpdateConstraints()
     }
 
@@ -469,11 +469,28 @@ class RouteMapViewController: UIViewController {
     @objc func resetFrameRate(_ sender: UIGestureRecognizer) {
         mapView.preferredFramesPerSecond = NavigationMapView.FrameIntervalOptions.defaultFramesPerSecond
     }
-
-    var contentInsets: UIEdgeInsets {
-        let top = instructionsBannerContentView.bounds.height
-        let bottom = bottomBannerContainerView.bounds.height
-        return UIEdgeInsets(top: top, left: 0, bottom: bottom, right: 0)
+    
+    func contentInset(forOverviewing overviewing: Bool) -> UIEdgeInsets {
+        let instructionBannerHeight = instructionsBannerContentView.bounds.height
+        let bottomBannerHeight = bottomBannerContainerView.bounds.height
+        
+        var insets = UIEdgeInsets(top: instructionBannerHeight,
+                                  left: 0,
+                                  bottom: bottomBannerHeight,
+                                  right: 0)
+        
+        if overviewing {
+            let courseView = mapView.userCourseView != nil ? mapView.userCourseView!.bounds.midX : 0
+            let routeLineWidths = MBRouteLineWidthByZoomLevel.compactMap { $0.value.constantValue as? Int }
+            let maxRouteLineWidth = CGFloat(routeLineWidths.max() ?? 0)
+            
+            insets.top += maxRouteLineWidth + courseView
+            insets.left += maxRouteLineWidth + courseView
+            insets.bottom += maxRouteLineWidth + courseView
+            insets.right += maxRouteLineWidth + courseView
+        }
+        
+        return insets
     }
 
     // MARK: End Of Route
