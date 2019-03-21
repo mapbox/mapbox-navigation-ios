124f021dc1a030b14c2bca171b550bc93e11afe5
Fredrik Karlsson <bjorn.fredrik.karlsson@gmail.com>
2019-03-21 17:17:18 +0100

    Temporary CarPlay overview tweaks

--- MapboxNavigation/CPMapTemplate.swift
+++ MapboxNavigation/CPMapTemplate.swift
@@ -32,5 +32,21 @@ extension CLLocationDirection {
         self = heading.wrap(min: 0, max: 360)
     }
 }
+
+extension UIViewController {
+    
+    func carPlayContentInsets(forOverviewing overviewing: Bool) -> UIEdgeInsets {
+        var contentInsets = view.safeArea
+        
+        if overviewing {
+            //let routeLineWidths = MBRouteLineWidthByZoomLevel.compactMap { $0.value.constantValue as? Int }
+            //contentInsets += UIEdgeInsets(floatLiteral: Double(routeLineWidths.max() ?? 0))
+            contentInsets += NavigationMapView.courseViewMinimumInsets
+        }
+        
+        return contentInsets
+    }
+}
+
 #endif
 
