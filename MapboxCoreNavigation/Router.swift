c14252546072213a7c7699fca5e60a5983148d72
Minh Nguyễn <mxn@1ec5.org>
2019-04-04 10:20:31 -0700

    Delete conflicting files to fix jazzy docset generation (#2076)
    * Deleted conflicting Date.swift
    
    There were two Date.swift files in the merged, CocoaPods-generated target that jazzy uses to calculate USRs.
    
    * Resolved more file name collisions

--- MapboxCoreNavigation/Router.swift
+++ MapboxCoreNavigation/Router.swift
@@ -186,3 +186,13 @@ extension InternalRouter where Self: Router {
     }
 }
 
+extension Array where Element: MapboxDirections.Route {
+    func mostSimilar(to route: Route) -> Route? {
+        let target = route.description
+        return self.min { (left, right) -> Bool in
+            let leftDistance = left.description.minimumEditDistance(to: target)
+            let rightDistance = right.description.minimumEditDistance(to: target)
+            return leftDistance < rightDistance
+        }
+    }
+}
