d194c9c82188fb73432bb1af0e57e975af6bf2f0
Fredrik Karlsson <bjorn.fredrik.karlsson@gmail.com>
2019-04-24 13:48:29 +0200

    Show compass in the CarPlay example app

--- Example/AppDelegate+CarPlay.swift
+++ Example/AppDelegate+CarPlay.swift
@@ -58,6 +58,7 @@ extension AppDelegate: CarPlayManagerDelegate {
     // MARK: CarPlayManagerDelegate
     func carPlayManager(_ carPlayManager: CarPlayManager, didBeginNavigationWith service: NavigationService) {
         currentAppRootViewController?.beginNavigationWithCarplay(navigationService: service)
+        carPlayManager.currentNavigator?.compassView.isHidden = false
     }
     
     func carPlayManagerDidEndNavigation(_ carPlayManager: CarPlayManager) {
