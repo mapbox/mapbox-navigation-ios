5ffec1b5335c43f9d1d11e7f3a0b45388c42bd3f
Vincent Sam <vincent.sam@mapbox.com>
2019-04-30 19:01:39 -0400

    Added snapshot tests before upcoming cards refactor.

---
+++ MapboxNavigationTests/InstructionsCardSnapshotTests.swift
@@ -0,0 +1,65 @@
+import Foundation
+import XCTest
+import SnappyShrimp
+import MapboxDirections
+@testable import TestHelper
+@testable import MapboxNavigation
+@testable import MapboxCoreNavigation
+
+class InstructionsCard: SnapshotTest {
+    
+    override func setUp() {
+        super.setUp()
+        recordMode = false
+    }
+    
+    lazy var initialRoute: Route = {
+        return Fixture.route(from: jsonFileName)
+    }()
+    
+    @available(iOS 11.0, *)
+    func testInstructionsCardCollection() {
+        let host = UIViewController(nibName: nil, bundle: nil)
+        let container = UIView.forAutoLayout()
+        let subject = InstructionsCardCollection(nibName: nil, bundle: nil)
+        
+        host.view.addSubview(container)
+        constrain(container, to: host.view)
+        
+        embed(parent: host, child: subject, in: container) { (parent, guidanceCard) -> [NSLayoutConstraint] in
+            guidanceCard.view.translatesAutoresizingMaskIntoConstraints = false
+            return guidanceCard.view.constraintsForPinning(to: container)
+        }
+        
+        let fakeRoute = Fixture.route(from: "route-with-banner-instructions")
+        
+        let service = MapboxNavigationService(route: initialRoute, directions: DirectionsSpy(accessToken: "knut"), simulating: .never)
+        let routeProgress = RouteProgress(route: fakeRoute)
+        let intersectionLocation = routeProgress.route.legs.first!.steps.first!.intersections!.first!.location
+        let fakeLocation = CLLocation(latitude: intersectionLocation.latitude, longitude: intersectionLocation.longitude)
+        subject.steps = routeProgress.steps
+        subject.routeProgress = routeProgress
+        subject.navigationService(service, didUpdate: routeProgress, with: fakeLocation, rawLocation: fakeLocation)
+        
+        verify(host, for: Device.iPhoneX.portrait)
+    }
+    
+    func constrain(_ child: UIView, to parent: UIView) {
+        let constraints = [
+            child.leadingAnchor.constraint(equalTo: parent.leadingAnchor),
+            child.trailingAnchor.constraint(equalTo: parent.trailingAnchor),
+            child.topAnchor.constraint(equalTo: parent.topAnchor, constant: 30.0)
+        ]
+        NSLayoutConstraint.activate(constraints)
+    }
+    
+    func embed(parent:UIViewController, child: UIViewController, in container: UIView, constrainedBy constraints: ((UIViewController, UIViewController) -> [NSLayoutConstraint])?) {
+        child.willMove(toParent: parent)
+        parent.addChild(child)
+        container.addSubview(child.view)
+        if let childConstraints: [NSLayoutConstraint] = constraints?(parent, child) {
+            parent.view.addConstraints(childConstraints)
+        }
+        child.didMove(toParent: parent)
+    }
+}
