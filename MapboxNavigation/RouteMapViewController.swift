5c2ca3e7d23dc6fcc92cab3b59c86fe9de589dcd
Jerrad Thramer <Jerrad.Thramer@mapbox.com>
2019-05-13 14:14:26 -0600

    Removing UIEdgeInsets.zero reference in favor of 0.0

--- MapboxNavigation/RouteMapViewController.swift
+++ MapboxNavigation/RouteMapViewController.swift
@@ -477,7 +477,7 @@ class RouteMapViewController: UIViewController {
         let instructionBannerHeight = topBannerContainerView.bounds.height//instructionsBannerContentView.bounds.height
         let bottomBannerHeight = bottomBannerContainerView.bounds.height
         
-        var insets = UIEdgeInsets(top: instructionBannerHeight, left: .zero, bottom: bottomBannerHeight, right: .zero)
+        var insets = UIEdgeInsets(top: instructionBannerHeight, left: 0.0, bottom: bottomBannerHeight, right: 0.0)
         
         if overviewing {
             insets += NavigationMapView.courseViewMinimumInsets
