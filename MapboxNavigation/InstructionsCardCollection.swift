0f469e6279eddc470f1f17bed4eeaa950bd369cb
Vincent Sam <vincent.sam@mapbox.com>
2019-06-28 11:58:01 -0400

    Modified delegate method signatures.

--- MapboxNavigation/InstructionsCardCollection.swift
+++ MapboxNavigation/InstructionsCardCollection.swift
@@ -9,18 +9,18 @@ import MapboxCoreNavigation
      - parameter instructionsCardCollection: The instructions card collection instance.
      - parameter step: The step for the maneuver instruction in preview.
      */
-    @objc(instructionsCardCollection:previewFor:)
+    @objc(instructionsCardCollection:previewForStep:)
     func instructionsCardCollection(_ instructionsCardCollection: InstructionsCardCollection, previewFor step: RouteStep)
     
     /**
      Offers the delegate the opportunity to customize the size of a prototype collection view cell per the associated trait collection.
      
      - parameter instructionsCardCollection: The instructions card collection instance.
-     - parameter cardSizeForTraitcollection: The trait collection associated to the current container view controller.
+     - parameter traitCollection: The traitCollection associated to the current container view controller.
      - returns: The preferred size of the cards for each cell in the instructions card collection.
      */
-    @objc(instructionsCardCollection:cardSizeForTraitcollection:)
-    optional func instructionsCardCollection(_ instructionsCardCollection: InstructionsCardCollection, cardSizeForTraitcollection: UITraitCollection) -> CGSize
+    @objc(instructionsCardCollection:cardSizeForTraitCollection:)
+    optional func instructionsCardCollection(_ instructionsCardCollection: InstructionsCardCollection, cardSizeFor traitCollection: UITraitCollection) -> CGSize
 }
 
 open class InstructionsCardCollection: UIViewController {
@@ -81,7 +81,7 @@ open class InstructionsCardCollection: UIViewController {
         super.viewDidLoad()
         
         /* TODO: Identify the traitCollections to define the width of the cards */
-        if let customSize = cardCollectionDelegate?.instructionsCardCollection?(self, cardSizeForTraitcollection: traitCollection) {
+        if let customSize = cardCollectionDelegate?.instructionsCardCollection?(self, cardSizeFor: traitCollection) {
             cardSize = customSize
         } else {
             cardSize = CGSize(width: Int(floor(view.frame.size.width * 0.82)), height: 200)
