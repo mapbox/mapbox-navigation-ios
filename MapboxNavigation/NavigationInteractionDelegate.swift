159664e002298a6925d06110ca046f5d4608fd08
Jerrad Thramer <Jerrad.Thramer@mapbox.com>
2019-09-25 12:46:06 -0600

    Removing Orphaned class.

--- MapboxNavigation/NavigationInteractionDelegate.swift
+++
@@ -1,9 +0,0 @@
-
-public protocol NavigationInteractionDelegate: class {
-    
-    optional func navigationViewController(_ controller: NavigationViewController, didRecenterAt location: CLLocation)
-    optional func navigationViewControllerDidConnectCarPlay(_ controller: NavigationViewController)
-    optional func navigationViewControllerDidDisconnectCarPlay(_ controller: NavigationViewController)
-    optional func showStatus(title: String, withSpinner: Bool, for time: TimeInterval, animated: Bool, interactive: Bool)
-
-}
