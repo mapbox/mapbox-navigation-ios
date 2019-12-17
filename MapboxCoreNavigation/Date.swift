f634a372e7f01a693f5ed40bf8e95f5b8d877539
Minh Nguyễn <mxn@1ec5.org>
2019-12-17 15:35:16 -0800

    Use built-in ISO date formatter

--- MapboxCoreNavigation/Date.swift
+++ MapboxCoreNavigation/Date.swift
@@ -5,10 +5,9 @@ extension Date {
         return Date.ISO8601Formatter.string(from: self)
     }
 
-    static let ISO8601Formatter: DateFormatter = {
-        let formatter = DateFormatter()
-        formatter.locale = Locale(identifier: "en_US_POSIX")
-        formatter.dateFormat = "yyyy-MM-dd'T'HH:mm:ss.SSSZ"
+    static let ISO8601Formatter: ISO8601DateFormatter = {
+        let formatter = ISO8601DateFormatter()
+        formatter.formatOptions = .withInternetDateTime
         formatter.timeZone = TimeZone(secondsFromGMT: 0)
         return formatter
     }()
