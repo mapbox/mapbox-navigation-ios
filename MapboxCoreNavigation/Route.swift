9d2bed785073d3579a97568602fedc864cd6a591
Avi Cieplinski <avi@mapbox.com>
2020-01-06 15:36:54 -0800

    Respond to feedback on generating json string from a Route.

--- MapboxCoreNavigation/Route.swift
+++ MapboxCoreNavigation/Route.swift
@@ -2,7 +2,7 @@ import MapboxDirections
 
 extension Route {
     
-    public var json: String? {
+    public func jsonRepresentation() -> String? {
         let encoder = JSONEncoder()
         encoder.userInfo[.options] = routeOptions
         guard let encoded = try? encoder.encode(self) else {
