6455762d0b68cc8591a104837a97450a08508132
Minh Nguyễn <mxn@1ec5.org>
2020-01-30 19:59:20 -0800

    Test ISO 8601 conversion

---
+++ MapboxCoreNavigationTests/DateTests.swift
@@ -0,0 +1,10 @@
+import XCTest
+@testable import MapboxCoreNavigation
+
+class DateTests: XCTestCase {
+    func testISO8601() {
+        // https://github.com/mapbox/mapbox-navigation-ios/issues/2327
+        let epoch = Date(timeIntervalSinceReferenceDate: 0)
+        XCTAssertEqual(epoch.ISO8601, "2001-01-01T00:00:00.000+0000", "ISO 8601 format should include milliseconds and full time zone for literal consistency with original event implementation")
+    }
+}
