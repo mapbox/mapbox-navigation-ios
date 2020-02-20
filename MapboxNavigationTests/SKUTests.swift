50acb87a4f65d52eaf476b7927c585669b834ca0
Minh Nguyễn <mxn@1ec5.org>
2020-02-20 15:03:40 -0800

    Updated defaults in tests
    Removed the alternative test because there’s no way to artificially test for API-based billing after testing for MAU-based billing.

--- MapboxNavigationTests/SKUTests.swift
+++ MapboxNavigationTests/SKUTests.swift
@@ -5,28 +5,10 @@ import MapboxDirections
 import MapboxSpeech
 import MapboxNavigation
 
-
 class SKUTests: XCTestCase {
 
-    // Billing per trip is the default which corresponds to `MBXAccountsSKUID.navigationUser`
+    // Billing per monthly active user (MAU), the default, corresponds to `MBXAccountsSKUID.navigationUser`.
     func testDefaultSKU() {
-        let mapsSkuToken = MGLMapView.skuToken
-        let directionsSkuToken = Directions.skuToken
-        let speechSkuToken = SpeechSynthesizer.skuToken
-        
-        XCTAssertNotNil(mapsSkuToken)
-        XCTAssertNotNil(speechSkuToken)
-        XCTAssertNotNil(directionsSkuToken)
-        
-        XCTAssertEqual(mapsSkuToken?.skuId, SkuID.mapsUser.rawValue)
-        XCTAssertEqual(speechSkuToken?.skuId, SkuID.navigationSession.rawValue)
-        XCTAssertEqual(directionsSkuToken?.skuId, SkuID.navigationSession.rawValue)
-    }
-    
-    // Billing per Monthly Active User (MAU) corresponds to `MBXAccountsSKUID.navigationUser`
-    func testMAUSKU() {
-        MBXAccounts.activateSKUID(.navigationUser)
-        
         let mapsSkuToken = MGLMapView.skuToken
         let directionsSkuToken = Directions.skuToken
         let speechSkuToken = SpeechSynthesizer.skuToken
@@ -38,8 +20,5 @@ class SKUTests: XCTestCase {
         XCTAssertEqual(mapsSkuToken?.skuId, SkuID.navigationUser.rawValue)
         XCTAssertEqual(speechSkuToken?.skuId, SkuID.navigationUser.rawValue)
         XCTAssertEqual(directionsSkuToken?.skuId, SkuID.navigationUser.rawValue)
-        
-        // Re-activate default
-        MBXAccounts.activateSKUID(.navigationSession)
     }
 }
