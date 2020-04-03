f951452d2aea2b290fce737397aa0ccb0260b385
Jerrad Thramer <Jerrad.Thramer@mapbox.com>
2020-04-03 11:32:32 -0600

    PR suggestion: removing redundancies in options initializers

--- MapboxCoreNavigation/NavigationRouteOptions.swift
+++ MapboxCoreNavigation/NavigationRouteOptions.swift
@@ -7,7 +7,7 @@ import MapboxDirections
  `NavigationRouteOptions` is a subclass of `RouteOptions` that has been optimized for navigation. Pass an instance of this class into the `Directions.calculate(_:completionHandler:)` method.
  - note: `NavigationRouteOptions` is designed to be used with the `Directions` and `NavigationDirections` classes for specifying routing criteria. To customize the user experience in a `NavigationViewController`, use the `NavigationOptions` class.
  */
-open class NavigationRouteOptions: RouteOptions {
+open class NavigationRouteOptions: RouteOptions, OptimizedForNavigation {
     /**
      Initializes a navigation route options object for routes between the given waypoints and an optional profile identifier optimized for navigation.
 
@@ -19,19 +19,14 @@ open class NavigationRouteOptions: RouteOptions {
             return $0
         }, profileIdentifier: profileIdentifier)
         includesAlternativeRoutes = true
-        shapeFormat = .polyline6
-        includesSteps = true
-        routeShapeResolution = .full
         if profileIdentifier == .walking {
             attributeOptions = [.congestionLevel, .expectedTravelTime]
         } else {
             attributeOptions = [.congestionLevel, .expectedTravelTime, .maximumSpeedLimit]
         }
-        includesSpokenInstructions = true
-        locale = Locale.nationalizedCurrent
-        distanceMeasurementSystem = Locale.current.usesMetricSystem ? .metric : .imperial
-        includesVisualInstructions = true
         includesExitRoundaboutManeuver = true
+
+        optimizeForNavigation()
     }
 
     /**
@@ -41,21 +36,7 @@ open class NavigationRouteOptions: RouteOptions {
      */
     public convenience init(navigationMatchOptions options: NavigationMatchOptions) {
         self.init(waypoints: options.waypoints, profileIdentifier: options.profileIdentifier)
-        self.includesSteps = true
-        routeShapeResolution = .full
-        shapeFormat = .polyline6
-        attributeOptions = [.congestionLevel, .expectedTravelTime]
-        if profileIdentifier == .walking {
-            attributeOptions = [.congestionLevel, .expectedTravelTime]
-        } else {
-            attributeOptions = [.congestionLevel, .expectedTravelTime, .maximumSpeedLimit]
-        }
-        includesSpokenInstructions = true
-        locale = Locale.nationalizedCurrent
-        distanceMeasurementSystem = Locale.current.usesMetricSystem ? .metric : .imperial
-        includesVisualInstructions = true
-        includesExitRoundaboutManeuver = true
-        }
+    }
     /**
      Initializes a navigation route options object for routes between the given locations and an optional profile identifier optimized for navigation.
 
@@ -86,7 +67,7 @@ open class NavigationRouteOptions: RouteOptions {
  
  Note: it is very important you specify the `waypoints` for the route. Usually the only two values for this `IndexSet` will be 0 and the length of the coordinates. Otherwise, all coordinates passed through will be considered waypoints.
  */
-open class NavigationMatchOptions: MatchOptions {
+open class NavigationMatchOptions: MatchOptions, OptimizedForNavigation {
     /**
      Initializes a navigation route options object for routes between the given waypoints and an optional profile identifier optimized for navigation.
      
@@ -97,17 +78,12 @@ open class NavigationMatchOptions: MatchOptions {
             $0.coordinateAccuracy = -1
             return $0
         }, profileIdentifier: profileIdentifier)
-        includesSteps = true
-        routeShapeResolution = .full
-        shapeFormat = .polyline6
         attributeOptions = [.congestionLevel, .expectedTravelTime]
         if profileIdentifier == .automobile || profileIdentifier == .automobileAvoidingTraffic {
             attributeOptions.insert(.maximumSpeedLimit)
         }
-        includesSpokenInstructions = true
-        locale = Locale.nationalizedCurrent
-        distanceMeasurementSystem = Locale.current.usesMetricSystem ? .metric : .imperial
-        includesVisualInstructions = true
+
+        optimizeForNavigation()
     }
     
     
@@ -131,5 +107,30 @@ open class NavigationMatchOptions: MatchOptions {
     
     required public init(from decoder: Decoder) throws {
         try super.init(from: decoder)
+    }
+}
+
+protocol OptimizedForNavigation: class {
+    var includesSteps: Bool { get set }
+    var routeShapeResolution: RouteShapeResolution { get set }
+    var shapeFormat: RouteShapeFormat { get set }
+    var attributeOptions: AttributeOptions { get set }
+    var locale: Locale { get set }
+    var distanceMeasurementSystem: MeasurementSystem { get set }
+    var includesSpokenInstructions: Bool { get set }
+    var includesVisualInstructions: Bool { get set }
+    
+    func optimizeForNavigation()
+}
+
+extension OptimizedForNavigation {
+    func optimizeForNavigation() {
+        shapeFormat = .polyline6
+        includesSteps = true
+        routeShapeResolution = .full
+        includesSpokenInstructions = true
+        locale = Locale.nationalizedCurrent
+        distanceMeasurementSystem = Locale.current.usesMetricSystem ? .metric : .imperial
+        includesVisualInstructions = true
     }
 }
