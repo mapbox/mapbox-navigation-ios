e7667de0fa251f8bbeb8646e94f82670294ec546
udumft <viktor.kononov@mapbox.com>
2020-06-11 16:48:29 +0300

    vk-turf-update: removed CoordinateBounds usage. Fixed RouteLeg.shape calculation

--- MapboxCoreNavigation/RouteLeg.swift
+++ MapboxCoreNavigation/RouteLeg.swift
@@ -4,7 +4,7 @@ import Turf
 extension RouteLeg {
     public var shape: LineString {
         return steps.dropFirst().reduce(into: steps.first?.shape ?? LineString([])) { (result, step) in
-            result = LineString(result.coordinates + (step.shape?.coordinates ?? []).dropFirst())
+            result.coordinates += (step.shape?.coordinates ?? []).dropFirst()
         }
     }
 }
