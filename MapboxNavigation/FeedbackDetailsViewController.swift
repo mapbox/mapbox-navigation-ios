08a6add2705116ce6ae7f412f023af14ef1f3c0e
Nishant Karajgikar <nishant.karajgikar@mapbox.com>
2020-06-25 11:11:32 -0400

    Extract TopLevelFeedbackViewController from FeedbackViewController and embed in navigationController

---
+++ MapboxNavigation/FeedbackDetailsViewController.swift
@@ -0,0 +1,93 @@
+import UIKit
+import MapboxCoreNavigation
+
+class FeedbackDetailsViewController: UITableViewController {
+
+    var navigationItemTitle: String = ""
+    var feedbackSubTypes: [FeedbackSubType] = [] {
+        didSet {
+            self.tableView.reloadData()
+        }
+    }
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        self.tableView.register(FeedbackSubTypeCell.self, forCellReuseIdentifier: "feedbackSubTypeCell")
+
+        // Uncomment the following line to display an Edit button in the navigation bar for this view controller.
+        // self.navigationItem.rightBarButtonItem = self.editButtonItem
+    }
+
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        self.navigationItem.title = navigationItemTitle
+        self.tableView.reloadData()
+    }
+
+    // MARK: - Table view data source
+
+    override func numberOfSections(in tableView: UITableView) -> Int {
+        return 1
+    }
+
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return feedbackSubTypes.count
+    }
+
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        guard let cell = tableView.dequeueReusableCell(withIdentifier: "feedbackSubTypeCell", for: indexPath) as? FeedbackSubTypeCell else {
+            return UITableViewCell()
+        }
+
+        cell.configure(with: feedbackSubTypes[indexPath.row].titleForCell())
+        return cell
+    }
+
+
+    /*
+    // Override to support conditional editing of the table view.
+    override func tableView(_ tableView: UITableView, canEditRowAt indexPath: IndexPath) -> Bool {
+        // Return false if you do not want the specified item to be editable.
+        return true
+    }
+    */
+
+    /*
+    // Override to support editing the table view.
+    override func tableView(_ tableView: UITableView, commit editingStyle: UITableViewCell.EditingStyle, forRowAt indexPath: IndexPath) {
+        if editingStyle == .delete {
+            // Delete the row from the data source
+            tableView.deleteRows(at: [indexPath], with: .fade)
+        } else if editingStyle == .insert {
+            // Create a new instance of the appropriate class, insert it into the array, and add a new row to the table view
+        }    
+    }
+    */
+
+    /*
+    // Override to support rearranging the table view.
+    override func tableView(_ tableView: UITableView, moveRowAt fromIndexPath: IndexPath, to: IndexPath) {
+
+    }
+    */
+
+    /*
+    // Override to support conditional rearranging of the table view.
+    override func tableView(_ tableView: UITableView, canMoveRowAt indexPath: IndexPath) -> Bool {
+        // Return false if you do not want the item to be re-orderable.
+        return true
+    }
+    */
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
--- MapboxNavigation/FeedbackViewController.swift
+++ MapboxNavigation/FeedbackViewController.swift
@@ -71,48 +71,11 @@ public class FeedbackViewController: UIViewController, DismissDraggable, UIGestu
     static let verticalCellPadding: CGFloat = 20.0
     
     let interactor = Interactor()
-    /**
-     The feedback items that are visible and selectable by the user.
-     */
-    public var sections: [FeedbackItem] =  [FeedbackType.incorrectVisual(subtype: nil),
-                                            FeedbackType.confusingAudio(subtype: nil),
-                                            FeedbackType.illegalRoute(subtype: nil),
-                                            FeedbackType.roadClosure(subtype: nil),
-                                            FeedbackType.routeQuality(subtype: nil)].map { $0.generateFeedbackItem() }
-    
+
     public weak var delegate: FeedbackViewControllerDelegate?
-    
-    lazy var collectionView: UICollectionView = {
-        let view: UICollectionView = UICollectionView(frame: .zero, collectionViewLayout: flowLayout)
-        view.translatesAutoresizingMaskIntoConstraints = false
-        view.backgroundColor = .clear
-        view.delegate = self
-        view.dataSource = self
-        view.register(FeedbackCollectionViewCell.self, forCellWithReuseIdentifier: FeedbackCollectionViewCell.defaultIdentifier)
-        return view
-    }()
-    
-    lazy var reportIssueLabel: UILabel = {
-        let label: UILabel = .forAutoLayout()
-        label.textAlignment = .center
-        label.text = FeedbackViewController.sceneTitle
-        return label
-    }()
-    
-    lazy var flowLayout: UICollectionViewFlowLayout = {
-        let layout = UICollectionViewFlowLayout()
-        layout.minimumInteritemSpacing = 0.0
-        layout.minimumLineSpacing = 0.0
-        return layout
-    }()
-    
+
     var draggableHeight: CGFloat {
-        let numberOfRows = collectionView.numberOfRows(using: self)
-        let padding = (flowLayout.sectionInset.top + flowLayout.sectionInset.bottom) * CGFloat(numberOfRows)
-        let indexPath = IndexPath(row: 0, section: 0)
-        let collectionViewHeight = collectionView(collectionView, layout: collectionView.collectionViewLayout, sizeForItemAt: indexPath).height * CGFloat(numberOfRows) + padding + view.safeArea.bottom
-        let fullHeight = reportIssueLabel.bounds.height+collectionViewHeight
-        return fullHeight
+        return topLevelFeedbackVC.draggableHeight
     }
     
     /**
@@ -147,11 +110,24 @@ public class FeedbackViewController: UIViewController, DismissDraggable, UIGestu
         self.modalPresentationStyle = .custom
         self.transitioningDelegate = self
     }
-    
+
+    var navController: UINavigationController!
+    var topLevelFeedbackVC: TopLevelFeedbackViewController!
+    var feedbackDetailsVC: FeedbackDetailsViewController!
+
     override public func viewDidLoad() {
         super.viewDidLoad()
-        setupViews()
-        setupConstraints()
+        topLevelFeedbackVC = TopLevelFeedbackViewController(nibName: nil, bundle: nil)
+        feedbackDetailsVC = FeedbackDetailsViewController(style: .plain)
+        navController = UINavigationController(rootViewController: topLevelFeedbackVC)
+        topLevelFeedbackVC.send = { (item: FeedbackItem) -> Void in
+            self.feedbackDetailsVC.navigationItemTitle = item.title
+            self.feedbackDetailsVC.feedbackSubTypes = item.feedbackType.subtypes
+            self.navController.pushViewController(self.feedbackDetailsVC, animated: true)
+        }
+
+        self.view.addSubview(navController.view)
+
         view.layoutIfNeeded()
         transitioningDelegate = self
         view.backgroundColor = .white
@@ -199,27 +175,6 @@ public class FeedbackViewController: UIViewController, DismissDraggable, UIGestu
         dismissFeedback()
     }
     
-    private func setupViews() {
-        let children = [reportIssueLabel, collectionView]
-        view.addSubviews(children)
-    }
-    
-    private func setupConstraints() {
-        let labelTop = reportIssueLabel.topAnchor.constraint(equalTo: view.topAnchor)
-        let labelHeight = reportIssueLabel.heightAnchor.constraint(equalToConstant: 30.0)
-        let labelLeading = reportIssueLabel.leadingAnchor.constraint(equalTo: view.leadingAnchor)
-        let labelTrailing = reportIssueLabel.trailingAnchor.constraint(equalTo: view.trailingAnchor)
-        let collectionLabelSpacing = collectionView.topAnchor.constraint(equalTo: reportIssueLabel.bottomAnchor)
-        let collectionLeading = collectionView.leadingAnchor.constraint(equalTo: view.leadingAnchor)
-        let collectionTrailing = collectionView.trailingAnchor.constraint(equalTo: view.trailingAnchor)
-        let collectionBarSpacing = collectionView.bottomAnchor.constraint(equalTo: view.safeBottomAnchor)
-        
-        let constraints = [labelTop, labelHeight, labelLeading, labelTrailing,
-                           collectionLabelSpacing, collectionLeading, collectionTrailing, collectionBarSpacing]
-        
-        NSLayoutConstraint.activate(constraints)
-    }
-    
     func send(_ item: FeedbackItem) {
         if let uuid = self.uuid {
             delegate?.feedbackViewController(self, didSend: item, uuid: uuid)
@@ -242,52 +197,6 @@ public class FeedbackViewController: UIViewController, DismissDraggable, UIGestu
             eventsManager?.cancelFeedback(uuid: uuid)
         }
         dismiss(animated: true, completion: nil)
-    }
-}
-
-extension FeedbackViewController: UICollectionViewDataSource {
-    public func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
-        let cell = collectionView.dequeueReusableCell(withReuseIdentifier: FeedbackCollectionViewCell.defaultIdentifier, for: indexPath) as! FeedbackCollectionViewCell
-        let item = sections[indexPath.row]
-        
-        cell.titleLabel.text = item.title
-        cell.imageView.tintColor = .clear
-        cell.imageView.image = item.image
-        
-        return cell
-    }
-    
-    public func numberOfSections(in collectionView: UICollectionView) -> Int {
-        return 1
-    }
-    
-    public func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
-        return sections.count
-    }
-}
-
-extension FeedbackViewController: UICollectionViewDelegate {
-    public func collectionView(_ collectionView: UICollectionView, didSelectItemAt indexPath: IndexPath) {
-        let item = sections[indexPath.row]
-        send(item)
-    }
-}
-
-extension FeedbackViewController: UICollectionViewDelegateFlowLayout {
-    public func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, sizeForItemAt indexPath: IndexPath) -> CGSize {
-        let availableWidth = collectionView.bounds.width
-        // 3 columns and 2 rows in portrait mode.
-        // 6 columns and 1 row in landscape mode.
-        let width = traitCollection.verticalSizeClass == .compact
-            ? floor(availableWidth / CGFloat(sections.count))
-            : floor(availableWidth / CGFloat(sections.count / 2))
-        let item = sections[indexPath.row]
-        let titleHeight = item.title.height(constrainedTo: width, font: FeedbackCollectionViewCell.Constants.titleFont)
-        let cellHeight: CGFloat = FeedbackCollectionViewCell.Constants.imageSize.height
-            + FeedbackCollectionViewCell.Constants.padding
-            + titleHeight
-            + FeedbackViewController.verticalCellPadding
-        return CGSize(width: width, height: cellHeight )
     }
 }
 
