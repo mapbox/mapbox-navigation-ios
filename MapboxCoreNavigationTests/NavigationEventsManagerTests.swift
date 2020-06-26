aad180441afa370d127295ed358b47c0da33eb0e
Maxim Makhun <maxim.makhun@mapbox.com>
2020-06-25 17:49:57 -0700

    Change testDepartRerouteArrive to fail instead of crashing.

--- MapboxCoreNavigationTests/NavigationEventsManagerTests.swift
+++ MapboxCoreNavigationTests/NavigationEventsManagerTests.swift
@@ -21,8 +21,8 @@ class NavigationEventsManagerTests: XCTestCase {
         let firstRoute = Fixture.route(from: "DCA-Arboretum", options: firstRouteOptions)
         
         let secondRouteOptions =  NavigationRouteOptions(coordinates: [
-                   CLLocationCoordinate2D(latitude: 42.361634, longitude: -71.12852),
-                   CLLocationCoordinate2D(latitude: 42.352396, longitude: -71.068719),
+            CLLocationCoordinate2D(latitude: 42.361634, longitude: -71.12852),
+            CLLocationCoordinate2D(latitude: 42.352396, longitude: -71.068719),
         ])
         let secondRoute = Fixture.route(from: "PipeFittersUnion-FourSeasonsBoston", options: secondRouteOptions)
         
@@ -53,9 +53,9 @@ class NavigationEventsManagerTests: XCTestCase {
         
         XCTAssertEqual(events.count, 3, "There should be one depart, one reroute, and one arrive event.")
         
-        let departEvent = events.filter { $0.event == MMEEventTypeNavigationDepart }.first!
-        let rerouteEvent = events.filter { $0.event == MMEEventTypeNavigationReroute }.first!
-        let arriveEvent = events.filter { $0.event == MMEEventTypeNavigationArrive }.first!
+        guard let departEvent = events.filter({ $0.event == MMEEventTypeNavigationDepart }).first else { XCTFail(); return }
+        guard let rerouteEvent = events.filter({ $0.event == MMEEventTypeNavigationReroute }).first else { XCTFail(); return }
+        guard let arriveEvent = events.filter({ $0.event == MMEEventTypeNavigationArrive }).first else { XCTFail(); return }
         
         let durationBetweenDepartAndArrive = arriveEvent.arrivalTimestamp!.timeIntervalSince(departEvent.startTimestamp!)
         let durationBetweenDepartAndReroute = rerouteEvent.created.timeIntervalSince(departEvent.startTimestamp!)
