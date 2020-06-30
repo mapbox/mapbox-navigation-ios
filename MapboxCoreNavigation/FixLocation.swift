0d9b5b5ffe44bc7ac0721f1684074a7e3120efca
Maxim Makhun <maxim.makhun@mapbox.com>
2020-06-30 12:24:21 -0700

    Remove availability check for speedAccuracy.

--- MapboxCoreNavigation/FixLocation.swift
+++ MapboxCoreNavigation/FixLocation.swift
@@ -4,11 +4,6 @@ import MapboxNavigationNative
 
 extension FixLocation {
     convenience init(_ location: CLLocation) {
-        var speedAccuracy: NSNumber? = nil
-        if #available(iOS 10.0, *) {
-            speedAccuracy = location.speedAccuracy >= 0 ? location.speedAccuracy as NSNumber : nil
-        }
-        
         var bearingAccuracy: NSNumber? = nil
         if #available(iOS 13.4, *) {
             bearingAccuracy = location.courseAccuracy >= 0 ? location.courseAccuracy as NSNumber : nil
@@ -22,7 +17,7 @@ extension FixLocation {
                   accuracyHorizontal: location.horizontalAccuracy >= 0 ? location.horizontalAccuracy as NSNumber : nil,
                   provider: nil,
                   bearingAccuracy: bearingAccuracy,
-                  speedAccuracy: speedAccuracy,
+                  speedAccuracy: location.speedAccuracy >= 0 ? location.speedAccuracy as NSNumber : nil,
                   verticalAccuracy: location.verticalAccuracy >= 0 ? location.verticalAccuracy as NSNumber : nil)
     }
 }
