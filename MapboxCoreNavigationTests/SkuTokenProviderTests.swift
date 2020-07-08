17d7c429504bcb7cb085082f5e45227474e0395d
Dersim Davaod <dersim.davaod@mapbox.com>
2020-07-08 09:44:33 +0300

    Update SkuTokenProviderTests: add testGetTokenMethodReturnsNonEmptySkuToken.

--- MapboxCoreNavigationTests/SkuTokenProviderTests.swift
+++ MapboxCoreNavigationTests/SkuTokenProviderTests.swift
@@ -1,5 +1,15 @@
 @testable import MapboxCoreNavigation
+import XCTest
 
 class SkuTokenProviderTests: XCTestCase {
-    
+    func testGetTokenMethodReturnsNonEmptySkuToken() {
+        // Given
+        let tokenProvider = SkuTokenProvider()
+        
+        // When
+        let skuToken = tokenProvider.getToken()
+        
+        // Then
+        XCTAssertFalse(skuToken.isEmpty)
+    }
 }
