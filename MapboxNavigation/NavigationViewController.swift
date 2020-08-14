108adcaee4b515f3e6fcf430e9ecd62012fe3ebe
Maxim Makhun <maxim.makhun@mapbox.com>
2020-08-14 12:57:24 -0700

    Remove WaypointStyle from NavigationViewController.

--- MapboxNavigation/NavigationViewController.swift
+++ MapboxNavigation/NavigationViewController.swift
@@ -148,26 +148,6 @@ open class NavigationViewController: UIViewController, NavigationStatusPresenter
     public var shouldManageApplicationIdleTimer = true
     
     /**
-     Enum denoting the types of the destination waypoint highlighting on arrival.
-     */
-    public enum WaypointStyle: Int {
-        /**
-         Do not highlight destination waypoint on arrival. Destination annotation is always shown by default.
-         */
-        case annotation
-        
-        /**
-         Highlight destination building on arrival in 2D. In case if destination building wasn't found only annotation will be shown.
-         */
-        case building
-        
-        /**
-         Highlight destination building on arrival in 3D. In case if destination building wasn't found only annotation will be shown.
-         */
-        case extrudedBuilding
-    }
-    
-    /**
      Allows to control highlighting of the destination building on arrival. By default destination buildings will not be highlighted.
      */
     public var waypointStyle: WaypointStyle = .annotation
