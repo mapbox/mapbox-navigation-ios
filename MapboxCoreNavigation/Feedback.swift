18b89363fd8d36210372354383b034f1e65b6ddd
Avi Cieplinski <avi-c@users.noreply.github.com>
2020-09-14 16:36:23 -0700

    Merge pull request #2621 from mapbox/ac-PositioningFeedback
    Add new positioning category for nav feedback
    
    Cherry-picked from def7d409e5acdee3d13d10209a079eebe07700a3

--- MapboxCoreNavigation/Feedback.swift
+++ MapboxCoreNavigation/Feedback.swift
@@ -23,21 +23,83 @@ public enum FeedbackType: CustomStringConvertible {
 
     /// Indicates a road closure was observed.
     case roadClosure(subtype: RoadClosureSubtype?)
-    
+
+    /// Indicates a problem with positioning the user
+    case positioning(subtype: PositioningSubtype?)
+
+    /// Description of the category for this type of feedback
     public var description: String {
         switch self {
         case .general:
             return "general"
         case .incorrectVisual(_):
-            return "incorrect_visual"
+            return "incorrect_visual_guidance"
         case .confusingAudio(_):
-            return "confusing_audio"
+            return "incorrect_audio_guidance"
         case .routeQuality(_):
-            return "route_quality"
+            return "routing_error"
         case .illegalRoute(_):
-            return "illegal_route"
+            return "not_allowed"
         case .roadClosure(_):
-            return "road_closure"
+            return "road_closed"
+        case .positioning(_):
+            return "positioning_issue"
+        }
+    }
+
+    /// Optional detailed description of the subtype of this feedback
+    public var subtypeDescription: String? {
+        switch self {
+        case .incorrectVisual(subtype: .turnIconIncorrect):
+            return "turn_icon_incorrect"
+        case .incorrectVisual(subtype: .streetNameIncorrect):
+            return "street_name_incorrect"
+        case .incorrectVisual(subtype: .instructionUnnecessary):
+            return "instruction_unnecessary"
+        case .incorrectVisual(subtype: .instructionMissing):
+            return "instruction_missing"
+        case .incorrectVisual(subtype: .maneuverIncorrect):
+            return "maneuver_incorrect"
+        case .incorrectVisual(subtype: .exitInfoIncorrect):
+            return "exit_info_incorrect"
+        case .incorrectVisual(subtype: .laneGuidanceIncorrect):
+            return "lane_guidance_incorrect"
+        case .incorrectVisual(subtype: .roadKnownByDifferentName):
+            return "road_known_by_different_name"
+        case .confusingAudio(subtype: .guidanceTooEarly):
+            return "guidance_too_early"
+        case .confusingAudio(subtype: .guidanceTooLate):
+            return "guidance_too_late"
+        case .confusingAudio(subtype: .pronunciationIncorrect):
+            return "pronunciation_incorrect"
+        case .confusingAudio(subtype: .roadNameRepeated):
+            return "road_name_repeated"
+        case .routeQuality(subtype: .routeNonDrivable):
+            return "route_not_driveable"
+        case .routeQuality(subtype: .routeNotPreferred):
+            return "route_not_preferred"
+        case .routeQuality(subtype: .alternativeRouteNotExpected):
+            return "alternative_route_not_expected"
+        case .routeQuality(subtype: .routeIncludedMissingRoads):
+            return "route_included_missing_roads"
+        case .routeQuality(subtype: .routeHadRoadsTooNarrowToPass):
+            return "route_had_roads_too_narrow_to_pass"
+        case .illegalRoute(subtype: .routedDownAOneWay):
+            return "routed_down_a_one_way"
+        case .illegalRoute(subtype: .turnWasNotAllowed):
+            return "turn_was_not_allowed"
+        case .illegalRoute(subtype: .carsNotAllowedOnStreet):
+            return "cars_not_allowed_on_street"
+        case .illegalRoute(subtype: .turnAtIntersectionUnprotected):
+            return "turn_at_intersection_was_unprotected"
+        case .roadClosure(subtype: .streetPermanentlyBlockedOff):
+            return "street_permanently_blocked_off"
+        case .roadClosure(subtype: .roadMissingFromMap):
+            return "road_is_missing_from_map"
+        case .positioning(subtype: .userPosition):
+            return "positioning_issue"
+        default:
+            return nil
         }
     }
 }
@@ -88,6 +150,10 @@ public enum RoadClosureSubtype: String {
     case streetPermanentlyBlockedOff
     case roadMissingFromMap
     case other
+}
+
+public enum PositioningSubtype: String {
+    case userPosition
 }
 
 /// Enum denoting the origin source of the corresponding feedback item
