4986d43c5864be14592898118dd9bef034a2f967
Maxim Makhun <maxim.makhun@mapbox.com>
2020-09-29 12:19:12 -0700

    Update to Navigation Native 23.0.0.

--- MapboxCoreNavigation/Navigator.swift
+++ MapboxCoreNavigation/Navigator.swift
@@ -3,7 +3,6 @@ import MapboxNavigationNative
 
 extension Navigator {
     func status(at timestamp: Date) -> NavigationStatus {
-        // Because `FixLocation(_:)` passes in 0 as `monotonicTimestampNanoseconds`, we need to always call `getStatusForTimestamp(_:)` instead of `getStatusForMonotonicTimestampNanoseconds(_:)`.
-        return getStatusForTimestamp(timestamp)
+        return getStatusForMonotonicTimestampNanoseconds(Int64(timestamp.nanosecondsSince1970))
     }
 }
