40fcd619b62052fd7ebacedc70dec1e01274bf95
Maxim Makhun <maxim.makhun@mapbox.com>
2020-10-13 17:45:52 -0700

    Remove unpacking functionality for Navigation tiles.

--- OfflineService/OfflineServiceDataSource.swift
+++ OfflineService/OfflineServiceDataSource.swift
@@ -7,7 +7,6 @@ open class OfflineServiceDataSource: OfflineServiceObserver {
     public weak var delegate: OfflineServiceDataSourceDelegate?
     
     private var offlineDataItems = [OfflineDataItem]()
-    private let tilesUnpackingLock = NSLock()
     
     // MARK: - OfflineServiceObserver methods
     
@@ -39,47 +38,6 @@ open class OfflineServiceDataSource: OfflineServiceObserver {
                                               domain: domain)
         
         self.delegate?.offlineServiceDataSource(self, didUpdate: [offlineDataItem])
-        
-        switch domain {
-        case .maps:
-            // Maps SDK will automatically pick up downloaded offline pack
-            break
-        case .navigation:
-            tilesUnpackingLock.lock()
-            
-            do {
-                guard let outputDirectoryURL = Bundle.mapboxCoreNavigation.suggestedTileURL?.appendingPathComponent("unpacked") else { return }
-                guard let packName = URL(string: pack.path)?.lastPathComponent else { return }
-                guard let packData = FileManager.default.contents(atPath: pack.path) else { return }
-                
-                try FileManager.default.createDirectory(at: outputDirectoryURL, withIntermediateDirectories: true, attributes: nil)
-                let temporaryPackURL = outputDirectoryURL.appendingPathComponent(packName).appendingPathExtension("tar")
-                try packData.write(to: temporaryPackURL)
-                
-                NavigationDirections.unpackTilePack(at: temporaryPackURL, outputDirectoryURL: outputDirectoryURL, progressHandler: { (totalBytes, unpackedBytes) in
-                    NSLog("Unpacked \(unpackedBytes) of \(totalBytes) bytes")
-                }) { (numberOfTiles, error) in
-                    do {
-                        if FileManager.default.fileExists(atPath: temporaryPackURL.path) {
-                            try FileManager.default.removeItem(at: temporaryPackURL)
-                        }
-                    } catch {
-                        self.tilesUnpackingLock.unlock()
-                        
-                        NSLog("Failed to remove temporary pack archive. Error: \(error)")
-                        self.delegate?.offlineServiceDataSource(self, didFail: OfflineServiceError.genericError(message: error.localizedDescription))
-                    }
-                    
-                    self.tilesUnpackingLock.unlock()
-                    NSLog("Finished unpacking \(numberOfTiles) tiles")
-                }
-            } catch {
-                tilesUnpackingLock.unlock()
-                
-                NSLog("Error occured while unpacking navigation tiles: \(error)")
-                delegate?.offlineServiceDataSource(self, didFail: OfflineServiceError.genericError(message: error.localizedDescription))
-            }
-        }
     }
     
     public func onExpired(for domain: OfflineDataDomain, metadata: OfflineDataRegionMetadata, pack: OfflineDataPack) {
@@ -125,8 +83,6 @@ open class OfflineServiceDataSource: OfflineServiceObserver {
     public func startObservingAvailableRegions() {
         OfflineServiceManager.register(self)
         
-        removeUnpackedTilesDirectory()
-
         OfflineServiceManager.instance.listAvailableRegions { [weak self] (expected) in
             guard let self = self else { return }
             
@@ -148,13 +104,6 @@ open class OfflineServiceDataSource: OfflineServiceObserver {
                 self.delegate?.offlineServiceDataSource(self, didUpdate: offlineDataItems)
             }
         }
-    }
-    
-    // MARK: - Private methods
-    
-    private func removeUnpackedTilesDirectory() {
-        guard let unpackedTilesDirectoryURL = Bundle.mapboxCoreNavigation.suggestedTileURL?.appendingPathComponent("unpacked") else { return }
-        try? FileManager.default.removeItem(at: unpackedTilesDirectoryURL)
     }
 }
 
