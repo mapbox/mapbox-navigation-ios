7e57c32af74bdeaed36eeb5f9b58abae08583472
danswick <dan.swick@gmail.com>
2020-10-30 20:42:16 -0700

    update nav docs script

--- scripts/update-navigation.sh
+++ scripts/update-navigation.sh
@@ -12,7 +12,7 @@ OUTPUT="/tmp/`uuidgen`"
 RELEASE_BRANCH=${1:-master}
 
 step "Updating mapbox-navigation-ios…"
-git fetch --depth=1 --prune
+git fetch
 git fetch --tags
 git checkout $RELEASE_BRANCH
 VERSION=$( git describe --tags --match=v*.*.* --abbrev=0 | sed 's/^v//' )
@@ -28,13 +28,13 @@ OUTPUT=${OUTPUT} scripts/document.sh
 
 step "Moving new docs folder to ./$VERSION"
 rm -rf "./$VERSION"
-mkdir -p "./navigation/api/$VERSION"
+mkdir -p "./$VERSION"
 mv -v $OUTPUT/* "./navigation/api/$VERSION"
 
 step "Switching branch to publisher-production"
 git checkout origin/publisher-production
 step "Committing API docs for $VERSION"
 git add "./$VERSION"
-git commit -m "[navigation] Add Mapbox Directions for Swift API docs for $VERSION [ci skip]" --no-verify
+git commit -m "[navigation] Add Mapbox Navigation SDK for iOS docs for v$VERSION [ci skip]" --no-verify
 
 step "Finished updating documentation"
