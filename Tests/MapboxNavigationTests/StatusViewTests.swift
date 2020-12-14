d9af246df83766b1f6ca731fa16bcb87c76dc6e0
Jill Cardamon <jillian.cardamon@mapbox.com>
2020-12-14 18:58:20 -0500

    test cases pass as expected

--- Tests/MapboxNavigationTests/StatusViewTests.swift
+++ Tests/MapboxNavigationTests/StatusViewTests.swift
@@ -2,63 +2,87 @@ import XCTest
 @testable import MapboxNavigation
 @testable import MapboxCoreNavigation
 
-class StatusViewTests {
+class StatusViewTests: XCTestCase {
     lazy var statusView: StatusView = {
         let view: StatusView = .forAutoLayout()
         view.isHidden = true
         return view
     }()
-    
-    
-    let title = NSLocalizedString("TEST_STATUS_1", bundle: .mapboxNavigation, value: "test status 1", comment: "the first status banner used for testing")
-    let firstStatus = StatusView.Status(id: title, duration: 0.5, priority: StatusView.Priority(rawValue: 0))
-    
-    let title = NSLocalizedString("TEST_STATUS_2", bundle: .mapboxNavigation, value: "test status 2", comment: "the second status banner used for testing")
-    let secondStatus = StatusView.Status(id: title, duration: 0.1, priority: StatusView.Priority(rawValue: 1))
-    
-    let title = NSLocalizedString("TEST_STATUS_3", bundle: .mapboxNavigation, value: "test status 3", comment: "the third status banner used for testing")
-    let thirdStatus = StatusView.Status(id: title, duration: .infinity, priority: StatusView.Priority(rawValue: 2))
-    
+
     func testWithSignificantDelay() {
-        addNewStatus(status: firstStatus)
+        addNewStatus(status: firstStatus())
         let seconds = 4.0
-        Dispatch.Queue.main.asyncAfter(deadline: .now() + seconds) {
-            XCTAssertEqual(statusView.statuses.count, 0)
+        DispatchQueue.main.asyncAfter(deadline: .now() + seconds) {
+            XCTAssertEqual(self.statusView.statuses.count, 0)
         }
     }
     
     func testWithDurationDelay() {
-        addNewStatus(status: firstStatus)
-        let seconds = firstStatus.duration
-        Dispatch.Queue.main.asyncAfter(deadline: .now() + seconds) {
-            XCTAssertEqual(statusView.statuses.count, 0)
+        addNewStatus(status: firstStatus())
+        let seconds = firstStatus().duration
+        DispatchQueue.main.asyncAfter(deadline: .now() + seconds) {
+            XCTAssertEqual(self.statusView.statuses.count, 0)
         }
     }
     
     func testFirstAndSecond() {
-        addNewStatus(status: firstStatus)
-        addNewStatus(status: secondStatus)
+        addNewStatus(status: firstStatus())
+        XCTAssertEqual(statusView.statuses.count, 1)
+        addNewStatus(status: secondStatus())
         XCTAssertEqual(statusView.statuses.count, 2)
-        Dispatch.Queue.main.asyncAfter(deadline: .now() + firstStatus.duration + secondStatus.duration) {
-            XCTAssertEqual(statusView.statuses.count, 0)
+        DispatchQueue.main.asyncAfter(deadline: .now() + firstStatus().duration + secondStatus().duration) {
+            XCTAssertEqual(self.statusView.statuses.count, 0)
         }
     }
     
     func testFirstAndSecondWithDelay() {
-        addNewStatus(status: firstStatus)
-        Dispatch.Queue.main.asyncAfter(deadline: .now() + firstStatus.duration) {
-            addNewStatus(status: secondStatus)
-            XCTAssertEqual(statusView.statuses.count, 1)
+        addNewStatus(status: firstStatus())
+        DispatchQueue.main.asyncAfter(deadline: .now() + firstStatus().duration) {
+            self.addNewStatus(status: self.secondStatus())
+            XCTAssertEqual(self.statusView.statuses.count, 1)
         }
     }
     
     func testFirstAndThird() {
-        addNewStatus(status: firstStatus)
-        addNewStatus(status: thirdStatus)
-        XCTAssertEqual(statusView.status.count, 2)
-        DispatchQueue.main.asyncAfter(deadline: .now() + firstStatus.durationze) {
-            XCTAssertEqual(statusView.statuses.count, 1)
+        print("!!! statuses before reset: \(statusView.statuses)")
+        clearStatuses()
+        addNewStatus(status: firstStatus())
+        addNewStatus(status: thirdStatus())
+        XCTAssertEqual(self.statusView.statuses.count, 2)
+        DispatchQueue.main.asyncAfter(deadline: .now() + firstStatus().duration) {
+            XCTAssertEqual(self.statusView.statuses.count, 1)
         }
     }
-    
 }
+
+extension StatusViewTests {
+    
+    // define statuses
+    func firstStatus() -> StatusView.Status {
+        let title1 = NSLocalizedString("FIRST_TEST_STATUS", bundle: .mapboxNavigation, value: "first test status", comment: "the first status banner used for testing")
+        return StatusView.Status(id: title1, duration: 50, priority: StatusView.Priority(rawValue: 0))
+    }
+    
+    func secondStatus() -> StatusView.Status {
+        let title2 = NSLocalizedString("SECOND_TEST_STATUS", bundle: .mapboxNavigation, value: "second test status", comment: "the second status banner used for testing")
+        return StatusView.Status(id: title2, duration: 100, priority: StatusView.Priority(rawValue: 1))
+    }
+    
+    func thirdStatus() -> StatusView.Status {
+        let title3 = NSLocalizedString("THIRD_TEST_STATUS", bundle: .mapboxNavigation, value: "third test status", comment: "the third status banner used for testing")
+        return StatusView.Status(id: title3, duration: .infinity, priority: StatusView.Priority(rawValue: 2))
+    }
+
+    func addNewStatus(status: StatusView.Status) {
+        statusView.addNewStatus(status: status)
+    }
+    
+    func hideStatus(status: StatusView.Status) {
+        statusView.hideStatus(usingStatus: status)
+    }
+    
+    func clearStatuses() {
+        statusView.statuses.removeAll()
+    }
+}
+
