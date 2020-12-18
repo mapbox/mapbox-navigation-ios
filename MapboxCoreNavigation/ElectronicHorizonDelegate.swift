7c64185396112adfeeff1fb688239ea6be0cfd27
Avi Cieplinski <avi@mapbox.com>
2020-12-18 09:15:23 -0800

    Rebase to main and updates for NavNative 28.1

--- MapboxCoreNavigation/ElectronicHorizonDelegate.swift
+++ MapboxCoreNavigation/ElectronicHorizonDelegate.swift
@@ -3,5 +3,7 @@ import MapboxNavigationNative
 
 public protocol ElectronicHorizonDelegate: class {
     func electronicHorizonDidUpdate(_ electronicHorizon: ElectronicHorizon, type: ElectronicHorizonResultType)
-    func didUpdatePosition(_ position: GraphPosition)
+    func didUpdatePosition(_ position: GraphPosition, distances: [String : RoadObjectDistanceInfo])
+    func roadObjectEnter(roadObjectId: String, fromStart: Bool)
+    func roadObjectExit(roadObjectId: String, fromEnd: Bool)
 }
