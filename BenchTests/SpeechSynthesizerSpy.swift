99e1236b2fd04df1202c86710494e9b2b58bf375
Minh Nguyễn <mxn@1ec5.org>
2021-02-19 14:33:58 -0800

    Moved SpeechSynthesizerSpy from TestHelper to BenchTests

--- BenchTests/SpeechSynthesizerSpy.swift
+++ BenchTests/SpeechSynthesizerSpy.swift
@@ -5,34 +5,6 @@ import MapboxDirections
 import MapboxCoreNavigation
 @testable import MapboxNavigation
 
-/**
- This class can be used as a substitute for SpeechSynthesizer under test, in order to verify whether expected calls were made.
- */
-public class SpeechAPISpy: SpeechSynthesizer {
-    public struct AudioDataCall {
-        public static let sound = NSDataAsset(name: "reroute-sound", bundle: .mapboxNavigation)!
-        
-        public let options: MapboxSpeech.SpeechOptions
-        public let completion: SpeechSynthesizer.CompletionHandler
-        
-        func fulfill() {
-            completion(AudioDataCall.sound.data, nil)
-        }
-    }
-
-    public var audioDataCalls: [AudioDataCall] = []
-
-    override public func audioData(with options: MapboxSpeech.SpeechOptions, completionHandler: @escaping MapboxSpeech.SpeechSynthesizer.CompletionHandler) -> URLSessionDataTask {
-        let call = AudioDataCall(options: options, completion: completionHandler)
-        audioDataCalls.append(call)
-        return DummyURLSessionDataTask()
-    }
-
-    public func reset() {
-        audioDataCalls.removeAll()
-    }
-}
-
 public class SpeechSynthesizerSpy: SpeechSynthesizing {
     lazy var notifier: NotificationCenter = .default
     fileprivate typealias Note = Notification.Name.MapboxVoiceTests
