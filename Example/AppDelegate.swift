ca9b9882781aa4d24596294b0238dc09e7d29e3d
Avi Cieplinski <avi@mapbox.com>
2021-02-25 16:28:38 -0800

    Create a UIWindow for the test target if running on iOS 13 and above.

--- Example/AppDelegate.swift
+++ Example/AppDelegate.swift
@@ -28,6 +28,9 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
         if isRunningTests() {
+            if window == nil {
+                window = UIWindow(frame: UIScreen.main.bounds)
+            }
             window!.rootViewController = UIViewController()
         }
         
