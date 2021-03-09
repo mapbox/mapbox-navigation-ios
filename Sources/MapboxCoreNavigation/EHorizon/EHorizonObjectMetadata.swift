4134be7ae59716936db8b24e13dcc218576945d9
Minh Nguyễn <mxn@1ec5.org>
2021-03-08 18:47:20 -0800

    Inline object type when associated with metadata
    Replaced mutually exclusive properties with associated values. Deleted provider enumeration because it is sufficiently represented by the presence of an associated value.

--- Sources/MapboxCoreNavigation/EHorizon/EHorizonObjectMetadata.swift
+++ Sources/MapboxCoreNavigation/EHorizon/EHorizonObjectMetadata.swift
@@ -2,39 +2,88 @@ import Foundation
 import MapboxDirections
 import MapboxNavigationNative
 
-public struct EHorizonObjectMetadata {
+public enum EHorizonObjectMetadata {
+    /**
+     Incident.
+     
+     Objects provided by Mapbox have additional information about the incident.
+     */
+    case incident(Incident?)
+    
+    /**
+     Tunnel entrance.
+     
+     Objects provided by Mapbox have additional information about the tunnel.
+     */
+    case tunnelEntrance(Tunnel?)
+    
+    /**
+     Tunnel exit.
+     
+     Objects provided by Mapbox have additional information about the tunnel.
+     */
+    case tunnelExit(Tunnel?)
+    
+    /**
+     Border crossing.
+     
+     Objects provided by Mapbox have additional information about the border crossing.
+     */
+    case borderCrossing(BorderCrossing?)
+    
+    /**
+     Toll collection point.
+     
+     Objects provided by Mapbox have additional information about the toll collection point.
+     */
+    case tollCollection(TollCollection?)
 
-    /** type of road object */
-    public let type: EHorizonObjectType
+    /**
+     Service area, also known as a rest stop.
+     
+     Objects provided by Mapbox have additional information about the service area.
+     */
+    case serviceArea(RestStop?)
+    
+    /// Restricted area entrance
+    case restrictedAreaEntrance
 
-    /** provider of road object */
-    public let provider: EHorizonObjectProvider
+    /// Restricted area exit
+    case restrictedAreaExit
+    
+    /// Bridge entrance
+    case bridgeEntrance
 
-    /** will be filled only if `type` is `Incident` and `provider` is `Mapbox` */
-    public let incident: Incident?
-
-    /** will be filled only if `type` is `TunnelEntrance` and `provider` is `Mapbox` */
-    public let tunnel: Tunnel?
-
-    /** will be filled only if `type` is `BorderCrossing` and `provider` is `Mapbox` */
-    public let borderCrossing: BorderCrossing?
-
-    /** will be filled only if `type` is `TollCollectionPoint` and `provider` is `Mapbox` */
-    public let tollCollection: TollCollection?
-
-    /** will be filled only if `type` is `ServiceArea` and `provider` is `Mapbox` */
-    public let serviceArea: RestStop?
+    /// Bridge exit
+    case bridgeExit
+    
+    /// Object was added by the application using the `RoadObjectsStore.addCustomRoadObject(for:location:)` method.
+    case userDefined
 
     init(_ native: RoadObjectMetadata) {
-        self.type = EHorizonObjectType(native.type)
-        self.provider = EHorizonObjectProvider(native.provider)
-        self.incident = native.incident != nil ? Incident(native.incident!) : nil
-        self.tunnel = native.tunnelInfo != nil ? Tunnel(native.tunnelInfo!) : nil
-        self.borderCrossing = native.borderCrossingInfo != nil
-            ? BorderCrossing(native.borderCrossingInfo!) : nil
-        self.tollCollection = native.tollCollectionInfo != nil
-            ? TollCollection(native.tollCollectionInfo!) : nil
-        self.serviceArea = native.serviceAreaInfo != nil
-            ? RestStop.init(native.serviceAreaInfo!) : nil
+        switch native.type {
+        case .incident:
+            self = .incident(native.incident != nil ? Incident(native.incident!) : nil)
+        case .tunnelEntrance:
+            self = .tunnelEntrance(native.tunnelInfo != nil ? Tunnel(native.tunnelInfo!) : nil)
+        case .tunnelExit:
+            self = .tunnelExit(native.tunnelInfo != nil ? Tunnel(native.tunnelInfo!) : nil)
+        case .borderCrossing:
+            self = .borderCrossing(native.borderCrossingInfo != nil ? BorderCrossing(native.borderCrossingInfo!) : nil)
+        case .tollCollectionPoint:
+            self = .tollCollection(native.tollCollectionInfo != nil ? TollCollection(native.tollCollectionInfo!) : nil)
+        case .serviceArea:
+            self = .serviceArea(native.serviceAreaInfo != nil ? RestStop.init(native.serviceAreaInfo!) : nil)
+        case .restrictedAreaEntrance:
+            self = .restrictedAreaEntrance
+        case .restrictedAreaExit:
+            self = .restrictedAreaExit
+        case .bridgeEntrance:
+            self = .bridgeEntrance
+        case .bridgeExit:
+            self = .bridgeExit
+        case .custom:
+            self = .userDefined
+        }
     }
 }
