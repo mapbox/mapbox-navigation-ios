4847b803b4c1e4ff7c803026e78d2e679434829a
Minh Nguyễn <mxn@1ec5.org>
2021-03-10 17:15:21 -0800

    Converted RoadName to associated-value enumeration
    The name and code are mutually exclusive; an edge identified by both a name and a code would have two RoadNames in its names array.

--- Sources/MapboxCoreNavigation/EHorizon/RoadName.swift
+++ Sources/MapboxCoreNavigation/EHorizon/RoadName.swift
@@ -1,34 +1,29 @@
 import Foundation
 import MapboxNavigationNative
 
-/** Road name information */
-public struct RoadName {
-
+/**
+ A human-readable name or route reference code that identifies a road.
+ */
+public enum RoadName {
     /**
-     The name of the road.
+     A road name.
      
      If you display a name to the user, you may need to abbreviate common words like “East” or “Boulevard” to ensure that it fits in the allotted space.
-     
-     This property is set for large roundabouts that have their own names but not for smaller, unnamed roundabouts.
      */
-    public let name: String?
+    case name(_ name: String)
     
     /**
-     Any route reference code assigned to the road.
+     A route reference code assigned to a road.
      
      A route reference code commonly consists of an alphabetic network code, a space or hyphen, and a route number. You should not assume that the network code is globally unique: for example, a network code of “NH” may indicate a “National Highway” or “New Hampshire”. Moreover, a route number may not even uniquely identify a route within a given network.
-     
-     If a highway ramp is part of a numbered route, its reference code is contained in this property. Otherwise, this property does not contain the route reference code of the adjoining road that appears on guide signage.
      */
-    public let code: String?
+    case code(_ code: String)
 
     init(_ native: MapboxNavigationNative.RoadName) {
         if native.isShielded {
-            name = native.name
-            code = nil
+            self = .code(native.name)
         } else {
-            name = nil
-            code = native.name
+            self = .name(native.name)
         }
     }
 }
