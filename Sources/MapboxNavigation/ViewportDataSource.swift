7d1af835d73a2d7323b2fe3eb57d8e859fc37754
Maxim Makhun <maxim.makhun@mapbox.com>
2021-03-29 18:36:05 -0700

    Update documentation.

--- Sources/MapboxNavigation/ViewportDataSource.swift
+++ Sources/MapboxNavigation/ViewportDataSource.swift
@@ -1,19 +1,56 @@
 import MapboxMaps
 
+/**
+ Protocol, which is used to fill and store `CameraOptions` which will be used by
+ `NavigationCamera` for execution of transitions and continuous updates.
+ 
+ By default Navigation SDK for iOS provides default implementation of `ViewportDataSource`
+ in `NavigationViewportDataSource`.
+ */
 public protocol ViewportDataSource: class {
     
+    /**
+     Delegate, which is used to notify `NavigationCamera` regarding upcoming `CameraOptions`
+     related changes.
+     */
     var delegate: ViewportDataSourceDelegate? { get set }
     
+    /**
+     `CameraOptions`, which are used on iOS when transitioning to `NavigationCameraState.following` or
+     for continuous updates when already in `NavigationCameraState.following` state.
+     */
     var followingMobileCamera: CameraOptions { get }
     
+    /**
+     `CameraOptions`, which are used on CarPlay when transitioning to `NavigationCameraState.following` or
+     for continuous updates when already in `NavigationCameraState.following` state.
+     */
     var followingHeadUnitCamera: CameraOptions { get }
     
+    /**
+     `CameraOptions`, which are used on iOS when transitioning to `NavigationCameraState.overview` or
+     for continuous updates when already in `NavigationCameraState.overview` state.
+     */
     var overviewMobileCamera: CameraOptions { get }
     
+    /**
+     `CameraOptions`, which are used on CarPlay when transitioning to `NavigationCameraState.overview` or
+     for continuous updates when already in `NavigationCameraState.overview` state.
+     */
     var overviewHeadUnitCamera: CameraOptions { get }
 }
 
+/**
+ Delegate, which is used to notify `NavigationCamera` regarding upcoming `CameraOptions`
+ related changes.
+ */
 public protocol ViewportDataSourceDelegate {
     
+    /**
+     Method, which must be called upon each location change to notify `NavigationCamera` that there is new data to consume.
+     
+     - parameter dataSource: Object, which conforms to `ViewportDataSource` protocol.
+     - parameter cameraOptions: Dictionary, which contains `CameraOptions` objects for both `iOS` and `CarPlay`.
+     */
     func viewportDataSource(_ dataSource: ViewportDataSource, didUpdate cameraOptions: [String: CameraOptions])
 }
