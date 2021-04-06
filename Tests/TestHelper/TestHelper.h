ae77f456f0c7ca5ba8b96b8b14c42950cd93f6f8
Shan Ma <shanma@alumni.usc.edu>
2021-04-06 13:53:25 -0700

    replace the MMEEventsManager+Spy from TestHelper to CTestHelper

--- Tests/TestHelper/TestHelper.h
+++ Tests/TestHelper/TestHelper.h
@@ -5,7 +5,3 @@ FOUNDATION_EXPORT double TestHelperVersionNumber;
 
 //! Project version string for TestHelper.
 FOUNDATION_EXPORT const unsigned char TestHelperVersionString[];
-
-// In this header, you should import all the public headers of your framework using statements like #import <TestHelper/PublicHeader.h>
-
-#import <TestHelper/MMEEventsManager+Spy.h>
