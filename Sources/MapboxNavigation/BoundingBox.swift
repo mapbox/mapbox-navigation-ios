3fff0f19ddb674e2f50a1af3a0818618c69e2bfc
Maxim Makhun <maxim.makhun@mapbox.com>
2021-04-09 12:29:41 -0700

    Remove unused `CLLocationDegrees` extension.

--- Sources/MapboxNavigation/BoundingBox.swift
+++ Sources/MapboxNavigation/BoundingBox.swift
@@ -7,7 +7,7 @@ extension BoundingBox {
      Returns zoom level inside of specific `CGSize`, in which `BoundingBox` was fit to.
      */
     func zoomLevel(fitTo size: CGSize) -> Double {
-        let latitudeFraction = (self.northEast.latitude.radius - self.southWest.latitude.radius) / .pi
+        let latitudeFraction = (self.northEast.latitude.toRadians() - self.southWest.latitude.toRadians()) / .pi
         let longitudeDiff = self.northEast.longitude - self.southWest.longitude
         let longitudeFraction = ((longitudeDiff < 0) ? (longitudeDiff + 360) : longitudeDiff) / 360
         let latitudeZoom = log(Double(size.height) / 512.0 / latitudeFraction) / M_LN2
