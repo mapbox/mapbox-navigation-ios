5e47d97f48b9db44e62505bb85f66c50f9a8f078
Alexander Pristavko <alexander.pristavko@mapbox.com>
2021-04-20 19:23:45 +0300

    Add associated values to RoadObjectType

--- Sources/MapboxCoreNavigation/EHorizon/RoadObjectType.swift
+++ Sources/MapboxCoreNavigation/EHorizon/RoadObjectType.swift
@@ -1,32 +1,33 @@
 import Foundation
+import MapboxDirections
 import MapboxNavigationNative
 
 /** Type of the road object */
 public enum RoadObjectType {
 
     /** Road object represents some road incident */
-    case incident
+    case incident(Incident?)
 
     /** Road object represents some toll collection point */
-    case tollCollection
+    case tollCollection(TollCollection?)
 
     /** Road object represents some border crossing */
-    case borderCrossing
+    case borderCrossing(BorderCrossing?)
 
     /** Road object represents some tunnel entrance */
-    case tunnelEntrance
+    case tunnelEntrance(Tunnel?)
 
     /** Road object represents some tunnel exit */
-    case tunnelExit
+    case tunnelExit(Tunnel?)
+
+    /** Road object represents some service area */
+    case serviceArea(RestStop?)
 
     /** Road object represents some restricted area entrance */
     case restrictedAreaEntrance
 
     /** Road object represents some restricted area exit */
     case restrictedAreaExit
-
-    /** Road object represents some service area */
-    case serviceArea
 
     /** Road object represents some bridge entrance */
     case bridgeEntrance
@@ -40,21 +41,21 @@ public enum RoadObjectType {
     init(_ native: MapboxNavigationNative.RoadObjectType) {
         switch native {
         case .incident:
-            self = .incident
+            self = .incident(nil)
         case .tollCollectionPoint:
-            self = .tollCollection
+            self = .tollCollection(nil)
         case .borderCrossing:
-            self = .borderCrossing
+            self = .borderCrossing(nil)
         case .tunnelEntrance:
-            self = .tunnelEntrance
+            self = .tunnelEntrance(nil)
         case .tunnelExit:
-            self = .tunnelExit
+            self = .tunnelExit(nil)
+        case .serviceArea:
+            self = .serviceArea(nil)
         case .restrictedAreaEntrance:
             self = .restrictedAreaEntrance
         case .restrictedAreaExit:
             self = .restrictedAreaExit
-        case .serviceArea:
-            self = .serviceArea
         case .bridgeEntrance:
             self = .bridgeEntrance
         case .bridgeExit:
