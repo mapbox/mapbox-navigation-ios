99b16129b7e67ec6c072b495828466da810b4050
Azarov Alex <alexander.azarov@mapbox.com>
2021-07-08 12:40:46 +0300

    Add ability to pass custom config through `UserDefaults` (#3151)
    * Add ability to pass custom config through UserDefaults.
    
    * Adress PR feedback
    
    * Fix CI build

---
+++ Tests/MapboxCoreNavigationTests/NativeHandlersFactoryTests.swift
@@ -0,0 +1,83 @@
+import XCTest
+import MapboxNavigationNative
+import MapboxDirections
+import TestHelper
+@testable import MapboxCoreNavigation
+
+private let customConfigKey = "com.mapbox.navigation.custom-config"
+
+class ConfigFactorySpy: ConfigFactory {
+    
+    static var passedCustomConfig: String?
+    
+    override class func build(for profile: SettingsProfile,
+                              config: NavigatorConfig,
+                              customConfig: String) -> ConfigHandle {
+        passedCustomConfig = customConfig
+        return super.build(for: profile, config: config, customConfig: customConfig)
+    }
+}
+
+class NativeHandlersFactoryTests: XCTestCase {
+    
+    var handlersFactory: NativeHandlersFactory!
+    
+    override func setUp() {
+        UserDefaults.standard.set(nil, forKey: customConfigKey)
+        handlersFactory = NativeHandlersFactory(tileStorePath: "tile store path",
+                                                credentials: Fixture.credentials,
+                                                configFactoryType: ConfigFactorySpy.self)
+    }
+    
+    func testDefaultCustomConfig() {
+        let expectedCustomConfig: [String: Any] = [
+            "features": [
+                "historyAutorecording": true
+            ]
+        ]
+        _ = handlersFactory.configHandle
+        let config = customConfig(from: ConfigFactorySpy.passedCustomConfig)
+        XCTAssertTrue(config == expectedCustomConfig)
+    }
+    
+    func testCustomConfigFromUserDefatuls() {
+        UserDefaults.standard.set([
+            "features": [
+                "custom_feature_key": "custom_feature_value"
+            ]
+        ], forKey: customConfigKey)
+        let expectedCustomConfig = [
+            "features": [
+                "historyAutorecording": true,
+                "custom_feature_key": "custom_feature_value"
+            ]
+        ]
+        _ = handlersFactory.configHandle
+        let config = customConfig(from: ConfigFactorySpy.passedCustomConfig)
+        XCTAssertTrue(config == expectedCustomConfig)
+    }
+    
+    func testUserDefaultsOverwritesDefaultCustomConfig() {
+        UserDefaults.standard.set([
+            "features": [
+                "historyAutorecording": false
+            ]
+        ], forKey: customConfigKey)
+        let expectedCustomConfig = [
+            "features": [
+                "historyAutorecording": false
+            ]
+        ]
+        _ = handlersFactory.configHandle
+        let config = customConfig(from: ConfigFactorySpy.passedCustomConfig)
+        XCTAssertTrue(config == expectedCustomConfig)
+    }
+    
+    // MARK: Helpers
+    
+    private func customConfig(from string: String?) -> [String: Any] {
+        let stringData = string?.data(using: .utf8) ?? Data()
+        let config = try? JSONSerialization.jsonObject(with: stringData, options: []) as? [String: Any]
+        return config ?? [:]
+    }
+}
