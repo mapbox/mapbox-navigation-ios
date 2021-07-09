9f9a568fe0a53a0b94fca2e757f50c4b3948b154
Aliaksandr Bialiauski <aliaksandr.bialiauski@mapbox.com>
2021-07-09 16:56:24 +0300

    Do not refresh route when it is completed

--- Sources/MapboxCoreNavigation/RouteController.swift
+++ Sources/MapboxCoreNavigation/RouteController.swift
@@ -274,8 +274,11 @@ open class RouteController: NSObject {
         if willReroute {
             reroute(from: location, along: routeProgress)
         }
-        // Check for faster route proactively (if reroutesProactively is enabled)
-        refreshAndCheckForFasterRoute(from: location, routeProgress: routeProgress)
+
+        if status.routeState != .complete {
+            // Check for faster route proactively (if reroutesProactively is enabled)
+            refreshAndCheckForFasterRoute(from: location, routeProgress: routeProgress)
+        }
     }
     
     private func subscribeNotifications() {
