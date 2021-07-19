82ab3f7e5a4914cd0d12312f869deac920d2ce5f
Maxim Makhun <maxim.makhun@mapbox.com>
2021-07-19 12:38:19 -0700

    Remove excessive import.

--- Tests/MapboxNavigationTests/CarPlayManagerTests.swift
+++ Tests/MapboxNavigationTests/CarPlayManagerTests.swift
@@ -8,8 +8,6 @@ import CarPlayTestHelper
 @testable import TestHelper
 @testable import MapboxNavigation
 
-import CarPlay
-
 // For some reason XCTest bundles ignore @available annotations and these tests are run on iOS < 12 :(
 // This is a bug in XCTest which will hopefully get fixed in an upcoming release.
 
