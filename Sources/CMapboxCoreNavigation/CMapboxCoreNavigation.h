c4a5fb75f49668e1c9987f3694111e26865af60d
Aliaksandr Bialiauski <aliaksandr.bialiauski@mapbox.com>
2021-08-12 12:38:25 +0300

    Remove MBXAccountsLoader type and CMapboxCoreNavigation target
    They are no longer needed.

--- Sources/CMapboxCoreNavigation/CMapboxCoreNavigation.h
+++
@@ -1,7 +0,0 @@
-#import <Foundation/Foundation.h>
-
-//! Project version number for CMapboxCoreNavigation.
-FOUNDATION_EXPORT double CMapboxCoreNavigationVersionNumber;
-
-//! Project version string for CMapboxCoreNavigation.
-FOUNDATION_EXPORT const unsigned char CMapboxCoreNavigationVersionString[];
