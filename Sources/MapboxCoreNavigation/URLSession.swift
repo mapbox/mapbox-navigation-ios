b2be87a5d6d6966e154488fd445737cb6d5013f6
Maxim Makhun <maxim.makhun@mapbox.com>
2021-09-08 13:41:47 -0400

    Fix bundle names retrieval in `URLSession.userAgent`. (#3335)

--- Sources/MapboxCoreNavigation/URLSession.swift
+++ Sources/MapboxCoreNavigation/URLSession.swift
@@ -8,27 +8,63 @@ extension URLSession {
      The user agent string for any HTTP requests performed directly within MapboxCoreNavigation or MapboxNavigation.
      */
     public static let userAgent: String = {
+        // Bundles in order from the application level on down
+        #if SWIFT_PACKAGE
         let bundles: [Bundle?] = [
-            // Bundles in order from the application level on down
+            .main,
+            .mapboxNavigationIfInstalled,
+            .mapboxCoreNavigation
+        ]
+        #else
+        let bundles: [Bundle?] = [
             .main,
             .mapboxNavigationIfInstalled,
             .mapboxCoreNavigation,
             .init(for: Directions.self),
         ]
+        #endif
+
         let bundleComponents = bundles.compactMap { (bundle) -> String? in
             guard let name = bundle?.object(forInfoDictionaryKey: "CFBundleName") as? String ?? bundle?.bundleIdentifier else { return nil }
+            
+            let defaultMapboxNavigationBundleName = "MapboxNavigation"
+            let defaultMapboxCoreNavigationBundleName = "MapboxCoreNavigation"
+            
+            #if SWIFT_PACKAGE
+            let mapboxNavigationName = "MapboxNavigation_MapboxNavigation"
+            #else
+            let mapboxNavigationName = defaultMapboxNavigationBundleName
+            #endif
+
+            #if SWIFT_PACKAGE
+            let mapboxCoreNavigationName = "MapboxNavigation_MapboxCoreNavigation"
+            #else
+            let mapboxCoreNavigationName = defaultMapboxCoreNavigationBundleName
+            #endif
+            
+            var bundleName: String {
+                switch name {
+                case mapboxNavigationName:
+                    return defaultMapboxNavigationBundleName
+                case mapboxCoreNavigationName:
+                    return defaultMapboxCoreNavigationBundleName
+                default:
+                    return name
+                }
+            }
+            
             var stringForShortVersion: String? {
                 switch name {
-                case "MapboxNavigation":
+                case mapboxNavigationName:
                     return Bundle.string(forMapboxNavigationInfoDictionaryKey: "CFBundleShortVersionString")
-                case "MapboxCoreNavigation":
+                case mapboxCoreNavigationName:
                     return Bundle.string(forMapboxCoreNavigationInfoDictionaryKey: "CFBundleShortVersionString")
                 default:
-                    return bundle?.object(forInfoDictionaryKey:"CFBundleShortVersionString") as? String
+                    return bundle?.object(forInfoDictionaryKey: "CFBundleShortVersionString") as? String
                 }
             }
             guard let version = stringForShortVersion else { return nil }
-            return "\(name)/\(version)"
+            return "\(bundleName)/\(version)"
         }
 
         let system: String
