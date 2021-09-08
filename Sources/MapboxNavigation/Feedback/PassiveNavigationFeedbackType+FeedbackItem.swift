1cedba516ea0ea69ec632b70ce6420f919a1e4f5
Minh Nguyễn <mxn@1ec5.org>
2021-09-08 00:59:27 -0700

    Copyedited feedback categories

--- Sources/MapboxNavigation/Feedback/PassiveNavigationFeedbackType+FeedbackItem.swift
+++ Sources/MapboxNavigation/Feedback/PassiveNavigationFeedbackType+FeedbackItem.swift
@@ -13,8 +13,8 @@ extension PassiveNavigationFeedbackType {
             return NSLocalizedString("PASSIVE_NAVIGATION_FEEDBACK_INCORRECT_VISUAL_STREET_NAME_INCORRECT", bundle: .mapboxNavigation, value: "Street name incorrect", comment: "Specific route feedback for incorrect street name.")
         case .roadIssue(subtype: .missingRoad):
             return NSLocalizedString("PASSIVE_NAVIGATION_FEEDBACK_ROAD_ISSUE_MISSING_ROAD", bundle: .mapboxNavigation, value: "Missing road", comment: "Feedback that map shows a non-existant road.")
-        case .roadIssue(subtype: .streetTemporaryBlockedOff):
-            return NSLocalizedString("PASSIVE_NAVIGATION_FEEDBACK_ROAD_ISSUE_STREET_TEMPORARY_BLOCKED_OFF", bundle: .mapboxNavigation, value: "Street temporary blocked off", comment: "Feedback that a street is temporary blocked off.")
+        case .roadIssue(subtype: .streetTemporarilyBlockedOff):
+            return NSLocalizedString("PASSIVE_NAVIGATION_FEEDBACK_ROAD_ISSUE_STREET_TEMPORARILY_BLOCKED_OFF", bundle: .mapboxNavigation, value: "Street temporarily blocked off", comment: "Feedback that a street is temporarily blocked off.")
         case .roadIssue(subtype: .streetPermanentlyBlockedOff):
             return NSLocalizedString("PASSIVE_NAVIGATION_FEEDBACK_ROAD_ISSUE_STREET_PERMANENTLY_BLOCKED_OFF", bundle: .mapboxNavigation, value: "Street permanently blocked off", comment: "Feedback that a street is permanently blocked off.")
         case .wrongTraffic(subtype: .congestion):
@@ -24,7 +24,7 @@ extension PassiveNavigationFeedbackType {
         case .wrongTraffic(subtype: .noTraffic):
             return NSLocalizedString("PASSIVE_NAVIGATION_FEEDBACK_WRONG_TRAFFIC_NO_TRAFFIC", bundle: .mapboxNavigation, value: "No traffic", comment: "Feedback that there is no traffic on the road.")
         case .incorrectVisual(subtype: .none):
-            return NSLocalizedString("PASSIVE_NAVIGATION_FEEDBACK_INCORRECT_VISUAL", bundle: .mapboxNavigation, value: "Incorrect visual", comment: "General category of feedback where something looks incorrect.")
+            return NSLocalizedString("PASSIVE_NAVIGATION_FEEDBACK_INCORRECT_VISUAL", bundle: .mapboxNavigation, value: "Looks incorrect", comment: "General category of feedback where something looks incorrect.")
         case .roadIssue(subtype: .none):
             return NSLocalizedString("PASSIVE_NAVIGATION_FEEDBACK_ROAD_ISSUE", bundle: .mapboxNavigation, value: "Road issue", comment: "General category of feedback where there is an issue on the road.")
         case .wrongTraffic(subtype: .none):
