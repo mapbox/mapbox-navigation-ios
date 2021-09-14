2c9747ef9dc27fe3c1d6997a03f4cb06f17c314e
Maxim Makhun <maxim.makhun@mapbox.com>
2021-09-14 12:29:51 -0700

    Update `RoadObjectType` documentation.

--- Sources/MapboxCoreNavigation/EHorizon/RoadObjectType.swift
+++ Sources/MapboxCoreNavigation/EHorizon/RoadObjectType.swift
@@ -2,31 +2,60 @@ import Foundation
 import MapboxDirections
 import MapboxNavigationNative
 
-/** Type of the road object */
+/**
+ Type of the road object.
+ */
 public enum RoadObjectType {
 
-    /** Road object represents some road incident */
+    /**
+     An alert providing information about incidents on a route. Incidents can include *congestion*,
+     *massTransit*, and more (see `Kind` for the full list of incident types).
+     */
     case incident(Incident?)
 
-    /** Road object represents some toll collection point */
+    /**
+     An alert describing a point along the route where a toll may be collected. Note that this does
+     not describe the entire toll road, rather it describes a booth or electronic gate where a toll
+     is typically charged. See `CollectionType`.
+     */
     case tollCollection(TollCollection?)
 
-    /** Road object represents some border crossing */
+    /**
+     An alert describing a country border crossing along the route. The alert triggers at the point
+     where the administrative boundary changes from one country to another. Two-letter and
+     three-letter ISO 3166-1 country codes are provided for the exiting country and the entering
+     country. See `BorderCrossingInfo`.
+     */
     case borderCrossing(BorderCrossing?)
 
-    /** Road object represents some tunnel */
+    /**
+     An alert describing a section of the route that continues through a tunnel. The alert begins at
+     the entrance of the tunnel and ends at the exit of the tunnel. For named tunnels, the tunnel name
+     is provided as part of `Tunnel.name`.
+     */
     case tunnel(Tunnel?)
 
-    /** Road object represents some service area */
+    /**
+     An alert about a rest area or service area accessible from the route. The alert marks the point
+     along the route where a driver can choose to pull off to access a rest stop. See `StopType`.
+     */
     case serviceArea(RestStop?)
 
-    /** Road object represents some restricted area */
+    /**
+     An alert about a segment of a route that includes a restriction. Restricted roads can include
+     private access roads or gated areas that can be accessed but are not open to vehicles passing
+     through.
+     */
     case restrictedArea
 
-    /** Road object represents some bridge */
+    /**
+     An alert about a segment of a route that includes a bridge.
+     */
     case bridge
 
-    /** Reserved for future use. */
+    /**
+     Reserved for future use.
+     */
     case userDefined
 
     init(_ native: MapboxNavigationNative.RoadObjectType) {
