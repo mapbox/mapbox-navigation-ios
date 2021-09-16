7e8dc4aed65efefeefab8aef949819956c4155f7
Maxim Makhun <maxim.makhun@mapbox.com>
2021-09-16 16:50:29 -0700

    Rename `RoadObjectPosition` to `RoadObject.Position`.

--- Sources/MapboxCoreNavigation/EHorizon/RoadObjectPosition.swift
+++ Sources/MapboxCoreNavigation/EHorizon/RoadObjectPosition.swift
@@ -2,28 +2,31 @@ import Foundation
 import CoreLocation
 import MapboxNavigationNative
 
-/**
- * Contains information about position of the point on the graph and
- * it's geo-position.
- */
-public struct RoadObjectPosition {
-
-    /** Position on the graph */
-    public let position: RoadGraph.Position
-
-    /** Geo-position of the object */
-    public let coordinate: CLLocationCoordinate2D
-
+extension RoadObject {
+    
     /**
-     Initializes a new `RoadObjectPosition` object with a given position on the graph and coordinate of the object.
+     * Contains information about position of the point on the graph and
+     * it's geo-position.
      */
-    public init(position: RoadGraph.Position, coordinate: CLLocationCoordinate2D) {
-        self.position = position
-        self.coordinate = coordinate
-    }
-
-    init(_ native: MapboxNavigationNative.Position) {
-        position = RoadGraph.Position(native.position)
-        coordinate = native.coordinate
+    public struct Position {
+        
+        /** Position on the graph */
+        public let position: RoadGraph.Position
+        
+        /** Geo-position of the object */
+        public let coordinate: CLLocationCoordinate2D
+        
+        /**
+         Initializes a new `RoadObject.Position` object with a given position on the graph and coordinate of the object.
+         */
+        public init(position: RoadGraph.Position, coordinate: CLLocationCoordinate2D) {
+            self.position = position
+            self.coordinate = coordinate
+        }
+        
+        init(_ native: MapboxNavigationNative.Position) {
+            position = RoadGraph.Position(native.position)
+            coordinate = native.coordinate
+        }
     }
 }
