d26119c8fb20e03b7fb16064886daf6923c4f791
Maxim Makhun <maxim.makhun@mapbox.com>
2021-10-11 14:41:30 -0700

    Simplify `UserHaloCourseView` implementation by removing `UserHaloStyleKitView` and using `CALayer` directly.

--- Tests/MapboxNavigationTests/UserHaloCourseViewSnapshotTests.swift
+++ Tests/MapboxNavigationTests/UserHaloCourseViewSnapshotTests.swift
@@ -19,8 +19,33 @@ class UserHaloCourseViewSnapshotTests: TestCase {
         userHaloCourseView.haloRingColor = .green
         userHaloCourseView.haloRadius = 45.0
         userHaloCourseView.haloBorderWidth = 5.0
-        userHaloCourseView.haloView.draw(frame)
+        userHaloCourseView.draw(frame)
         assertImageSnapshot(matching: userHaloCourseView, as: .image(precision: 0.95))
+    }
+    
+    func testUserHaloCourseViewMultipleDrawCycles() {
+        let frame = CGRect(x: 0.0, y: 0.0, width: 100.0, height: 100.0)
+        
+        let userHaloCourseView = UserHaloCourseView(frame: frame)
+        userHaloCourseView.haloColor = .blue.withAlphaComponent(0.2)
+        userHaloCourseView.haloRingColor = .green.withAlphaComponent(0.2)
+        userHaloCourseView.haloRadius = 45.0
+        userHaloCourseView.haloBorderWidth = 5.0
+        
+        for _ in 0...10 {
+            userHaloCourseView.draw(frame)
+        }
+        
+        XCTAssertEqual(userHaloCourseView.layer.sublayers?.count, 1, "UserHaloCourseView should contain only one layer.")
+        
+        assertImageSnapshot(matching: userHaloCourseView, as: .image(precision: 1.00))
+        
+        userHaloCourseView.haloColor = .red
+        userHaloCourseView.haloRingColor = .yellow
+        userHaloCourseView.haloRadius = 20.0
+        userHaloCourseView.haloBorderWidth = 10.0
+        
+        assertImageSnapshot(matching: userHaloCourseView, as: .image(precision: 1.00))
     }
     
     @available(iOS 13.0, *)
@@ -31,22 +56,22 @@ class UserHaloCourseViewSnapshotTests: TestCase {
         
         // Simulate `UserHaloCourseView` color appearance in light mode.
         let lightUserHaloCourseView = UserHaloCourseView(frame: frame)
-        lightUserHaloCourseView.haloView.overrideUserInterfaceStyle = .light
+        lightUserHaloCourseView.overrideUserInterfaceStyle = .light
         lightUserHaloCourseView.haloColor = haloColor
         lightUserHaloCourseView.haloRingColor = haloRingColor
         lightUserHaloCourseView.haloRadius = 45.0
         lightUserHaloCourseView.haloBorderWidth = 5.0
-        lightUserHaloCourseView.haloView.draw(frame)
+        lightUserHaloCourseView.draw(frame)
         assertImageSnapshot(matching: lightUserHaloCourseView, as: .image(precision: 0.95))
         
         // Simulate `UserHaloCourseView` color appearance in dark mode.
         let darkUserHaloCourseView = UserHaloCourseView(frame: frame)
-        darkUserHaloCourseView.haloView.overrideUserInterfaceStyle = .dark
+        darkUserHaloCourseView.overrideUserInterfaceStyle = .dark
         darkUserHaloCourseView.haloColor = haloColor
         darkUserHaloCourseView.haloRingColor = haloRingColor
         darkUserHaloCourseView.haloRadius = 45.0
         darkUserHaloCourseView.haloBorderWidth = 5.0
-        darkUserHaloCourseView.haloView.draw(frame)
+        darkUserHaloCourseView.draw(frame)
         assertImageSnapshot(matching: darkUserHaloCourseView, as: .image(precision: 0.95))
     }
 }
