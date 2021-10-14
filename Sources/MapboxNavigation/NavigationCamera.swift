20d6c990d28ee205d634dd5d16101d5f1136a39d
Maxim Makhun <maxim.makhun@mapbox.com>
2021-10-14 16:12:52 -0400

    Fix issue, which leads to inability to follow current location changes. (#3485)

--- Sources/MapboxNavigation/NavigationCamera.swift
+++ Sources/MapboxNavigation/NavigationCamera.swift
@@ -16,11 +16,13 @@ public class NavigationCamera: NSObject, ViewportDataSourceDelegate {
      */
     public required init(_ mapView: MapView, navigationCameraType: NavigationCameraType = .mobile) {
         self.mapView = mapView
-        self.viewportDataSource = NavigationViewportDataSource(mapView)
-        self.cameraStateTransition = NavigationCameraStateTransition(mapView)
-        self.type = navigationCameraType
+        viewportDataSource = NavigationViewportDataSource(mapView)
+        cameraStateTransition = NavigationCameraStateTransition(mapView)
+        type = navigationCameraType
         
         super.init()
+        
+        viewportDataSource.delegate = self
         
         setupGestureRecognizers()
         
