ecfd3aa7d0960c8bfbd78e6a415bc7aed180dabd
Jill Cardamon <jillian.cardamon@mapbox.com>
2021-10-15 13:41:49 -0400

    Fix CHANGELOG order, remove note from RoadObject, and add hyphons to notes.

--- Sources/MapboxCoreNavigation/EHorizon/DistancedRoadObject.swift
+++ Sources/MapboxCoreNavigation/EHorizon/DistancedRoadObject.swift
@@ -5,8 +5,6 @@ import MapboxNavigationNative
 /**
  * Contains information about distance to the road object of a concrete
  * type/shape (gantry, polygon, line, point etc.).
- *
- * note: The Mapbox Electronic Horizon feature of the Mapbox Navigation SDK is in public beta and is subject to changes, including its pricing. Use of the feature is subject to the beta product restrictions in the Mapbox Terms of Service. Mapbox reserves the right to eliminate any free tier or free evaluation offers at any time and require customers to place an order to purchase the Mapbox Electronic Horizon feature, regardless of the level of use of the feature.
  */
 public enum DistancedRoadObject {
     /**
--- Sources/MapboxCoreNavigation/EHorizon/ElectronicHorizonOptions.swift
+++ Sources/MapboxCoreNavigation/EHorizon/ElectronicHorizonOptions.swift
@@ -5,7 +5,7 @@ import MapboxNavigationNative
 /**
  Defines options for emitting `Notification.Name.electronicHorizonDidUpdatePosition`, `Notification.Name.electronicHorizonDidEnterRoadObject`, and `Notification.Name.electronicHorizonDidExitRoadObject` notifications while a `RouteController` or `PassiveLocationManager` is active.
  
- note: The Mapbox Electronic Horizon feature of the Mapbox Navigation SDK is in public beta and is subject to changes, including its pricing. Use of the feature is subject to the beta product restrictions in the Mapbox Terms of Service. Mapbox reserves the right to eliminate any free tier or free evaluation offers at any time and require customers to place an order to purchase the Mapbox Electronic Horizon feature, regardless of the level of use of the feature.
+ - note: The Mapbox Electronic Horizon feature of the Mapbox Navigation SDK is in public beta and is subject to changes, including its pricing. Use of the feature is subject to the beta product restrictions in the Mapbox Terms of Service. Mapbox reserves the right to eliminate any free tier or free evaluation offers at any time and require customers to place an order to purchase the Mapbox Electronic Horizon feature, regardless of the level of use of the feature.
  */
 public struct ElectronicHorizonOptions {
 
