7c50db6f21618bae3486515c8ed69e301e334e3d
Maxim Makhun <maxim.makhun@mapbox.com>
2022-01-24 13:04:23 -0800

    Simplify layout `InstructionsCardCell` by removing shadow.

--- Sources/MapboxNavigation/InstructionsCardCell.swift
+++ Sources/MapboxNavigation/InstructionsCardCell.swift
@@ -4,14 +4,15 @@ import MapboxDirections
 
 /// :nodoc:
 public class InstructionsCardCell: UICollectionViewCell {
+    
     public let container: InstructionsCardContainerView = .init()
     
-    override public init(frame: CGRect) {
+    public override init(frame: CGRect) {
         super.init(frame: frame)
         self.commonInit()
     }
     
-    required public init?(coder aDecoder: NSCoder) {
+    public required init?(coder aDecoder: NSCoder) {
         super.init(coder: aDecoder)
         self.commonInit()
     }
@@ -24,23 +25,22 @@ public class InstructionsCardCell: UICollectionViewCell {
     
     private func configureLayer() {
         backgroundColor = .clear
-        layer.shadowColor = UIColor.black.cgColor
-        layer.shadowOffset = CGSize(width: 1, height: 2)
-        layer.shadowRadius = 1
-        layer.shadowOpacity = 0.4
     }
     
     private func setupConstraints() {
         container.translatesAutoresizingMaskIntoConstraints = false
-        container.topAnchor.constraint(equalTo: topAnchor, constant: 2).isActive = true
+        container.topAnchor.constraint(equalTo: topAnchor).isActive = true
         container.leadingAnchor.constraint(equalTo: contentView.leadingAnchor).isActive = true
         container.trailingAnchor.constraint(equalTo: contentView.trailingAnchor).isActive = true
     }
-    public func configure(for step: RouteStep, distance: CLLocationDistance, instruction: VisualInstructionBanner? = nil, isCurrentCardStep: Bool = false) {
+    
+    public func configure(for step: RouteStep,
+                          distance: CLLocationDistance,
+                          instruction: VisualInstructionBanner? = nil,
+                          isCurrentCardStep: Bool = false) {
         container.updateInstruction(for: step,
                                     distance: distance,
                                     instruction: instruction,
                                     isCurrentCardStep: isCurrentCardStep)
     }
 }
-
