22da73f490631dc7c62d642cdbaacf399f90fe4c
udumft <viktor.kononov@mapbox.com>
2022-01-27 15:13:05 +0300

    vk-1324-reroute-controller: added RerouteController; wired up it's usage through RouteController, piped settings usage; temporary aiming custom Directions branch

--- Sources/MapboxCoreNavigation/Router.swift
+++ Sources/MapboxCoreNavigation/Router.swift
@@ -114,6 +114,8 @@ public protocol Router: CLLocationManagerDelegate {
     func userIsOnRoute(_ location: CLLocation) -> Bool
     func reroute(from: CLLocation, along: RouteProgress)
     
+    var initialManeuverAvoidanceRadius: TimeInterval { get set }
+    
     /**
      A radius around the current user position in which the API will avoid returning any significant maneuvers when rerouting.
      
