61143d3d27e3750896869eb1892733d5e1bfbb1e
Shan Ma <shanma@alumni.usc.edu>
2022-01-31 16:36:51 -0800

    Fix the tests.

--- Tests/MapboxNavigationTests/SpriteRepositoryTests.swift
+++ Tests/MapboxNavigationTests/SpriteRepositoryTests.swift
@@ -1,6 +1,7 @@
 import XCTest
-import TestHelper
+@testable import TestHelper
 @testable import MapboxNavigation
+@testable import MapboxCoreNavigation
 
 class SpriteRepositoryTests: TestCase {
     lazy var repository: SpriteRepository = {
@@ -80,7 +81,7 @@ class SpriteRepositoryTests: TestCase {
         XCTAssert(semaphoreResult == .success, "Semaphore timed out")
 
         guard let data = requestedData else {
-            XCTFail("Failed to download sprite metadata.")
+            XCTFail("Failed to download Sprite metadata.")
             return
         }
         
@@ -88,6 +89,37 @@ class SpriteRepositoryTests: TestCase {
         let expectedMetaData = SpriteMetaData(width: 156, height: 84, x: 1710, y: 1992, pixelRatio: 2, placeholder: [0,8,52,20], visible: true)
         let spriteMetaData = repository.metadataCache.spriteMetaData(forKey: dataKey)
         XCTAssertEqual(expectedMetaData, spriteMetaData, "Failed to retrieve metadata from cache.")
+    }
+    
+    func testUpdateRepository() {
+        let styleID = repository.styleURI.rawValue.components(separatedBy: "styles")[1]
+        guard let accessToken = NavigationSettings.shared.directions.credentials.accessToken,
+              let spriteRequestURL = URL(string: repository.baseURL + styleID + "/sprite@2x.png?access_token=" + accessToken),
+              let metadataRequestURL = URL(string: repository.baseURL + styleID + "/sprite@2x?access_token=" + accessToken) else {
+                  XCTFail("Failed to form request to update SpriteRepository")
+                  return
+              }
+        
+        ImageLoadingURLProtocolSpy.registerData(ShieldImage.i280.image.pngData()!, forURL: spriteRequestURL)
+        ImageLoadingURLProtocolSpy.registerData(Fixture.JSONFromFileNamed(name: "sprite-metadata"), forURL: metadataRequestURL)
+        
+        let dataKey = "default-3"
+        XCTAssertNil(repository.metadataCache.spriteMetaData(forKey:dataKey))
+        XCTAssertNil(repository.imageCache.image(forKey:"Sprite"))
+
+        let expectation = expectation(description: "Image Downloaded")
+        repository.updateRepository() {
+            expectation.fulfill()
+        }
+        wait(for: [expectation], timeout: 10.0)
+        
+        let sprite = repository.imageCache.image(forKey:"Sprite")
+        XCTAssertNotNil(sprite)
+        XCTAssertTrue((sprite?.isKind(of: UIImage.self))!, "Failed to update the Sprite.")
+        
+        let spriteMetadata = repository.metadataCache.spriteMetaData(forKey: dataKey)
+        let expectedMetaData = SpriteMetaData(width: 156, height: 84, x: 1710, y: 1992, pixelRatio: 2, placeholder: [0,8,52,20], visible: true)
+        XCTAssertEqual(spriteMetadata, expectedMetaData, "Failed to update the Sprite metadata.")
     }
 
 }
