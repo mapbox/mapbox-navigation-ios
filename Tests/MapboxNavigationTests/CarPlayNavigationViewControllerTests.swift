be6cd94d4b44d61561aae50ab295577630654c3d
Maxim Makhun <maxim.makhun@mapbox.com>
2022-02-02 16:20:36 -0800

    Remove `CPManeuverFake`. Improve readability of `testTravelEstimates`.

--- Tests/MapboxNavigationTests/CarPlayNavigationViewControllerTests.swift
+++ Tests/MapboxNavigationTests/CarPlayNavigationViewControllerTests.swift
@@ -8,64 +8,61 @@ import CarPlay
 import CarPlayTestHelper
 
 @available(iOS 12.0, *)
-fileprivate class CPManeuverFake: CPManeuver {
-    override init() {
-        super.init()
-    }
-    
-    required init?(coder aDecoder: NSCoder) {
-        fatalError("init(coder:) has not been implemented")
-    }
-}
-
-@available(iOS 12.0, *)
 class CarPlayNavigationViewControllerTests: TestCase {
-    func testCarplayDisplaysCorrectEstimates() {
-        //set up the litany of dependancies
-        let manager = CarPlayManager(routingProvider: MapboxRoutingProvider(.offline))
-        let options = NavigationRouteOptions(coordinates: [
+    
+    func testTravelEstimates() {
+        let navigationRouteOptions = NavigationRouteOptions(coordinates: [
             CLLocationCoordinate2D(latitude: 9.519172, longitude: 47.210823),
             CLLocationCoordinate2D(latitude: 9.52222, longitude: 47.214268),
             CLLocationCoordinate2D(latitude: 47.212326, longitude: 9.512569),
         ])
-        let routeResponse = Fixture.routeResponse(from: "multileg-route", options: options)
-        let navService = MapboxNavigationService(routeResponse: routeResponse, routeIndex: 0, routeOptions: options)
-        let interface = FakeCPInterfaceController(context: "test estimates display")
-        let mapSpy = MapTemplateSpy()
+        
+        let routeResponse = Fixture.routeResponse(from: "multileg-route",
+                                                  options: navigationRouteOptions)
+        
+        let navigationService = MapboxNavigationService(routeResponse: routeResponse,
+                                                        routeIndex: 0,
+                                                        routeOptions: navigationRouteOptions)
+        
+        let mapTemplate = MapTemplateSpy()
+        let navigationSession = CPNavigationSessionFake(maneuvers: [CPManeuver()])
+        mapTemplate.fakeSession = navigationSession
+        
+        let interfaceController = FakeCPInterfaceController(context: #function)
+        
+        let carPlayManager = CarPlayManager(routingProvider: MapboxRoutingProvider(.offline))
+        
+        let carPlayNavigationViewController = CarPlayNavigationViewController(navigationService: navigationService,
+                                                                              mapTemplate: mapTemplate,
+                                                                              interfaceController: interfaceController,
+                                                                              manager: carPlayManager)
         let trip = CPTrip(origin: MKMapItem(), destination: MKMapItem(), routeChoices: [])
-        let fakeManeuver = CPManeuverFake()
-        let fakeSession = CPNavigationSessionFake(maneuvers: [fakeManeuver])
-        mapSpy.fakeSession = fakeSession
-        let progress = navService.routeProgress
-        let firstCoordinate = progress.currentLeg.shape.coordinates.first!
-        let location = CLLocation(latitude: firstCoordinate.latitude, longitude: firstCoordinate.longitude)
+        carPlayNavigationViewController.startNavigationSession(for: trip)
         
-        //create the subject and notification
-        let subject = CarPlayNavigationViewController(navigationService: navService, mapTemplate: mapSpy, interfaceController: interface, manager: manager)
-        subject.startNavigationSession(for: trip)
-        let payload: [RouteController.NotificationUserInfoKey: Any] = [
-            .routeProgressKey: navService.routeProgress,
-            .locationKey: location,
-        ]
-        let fakeNotication = NSNotification(name: .routeControllerProgressDidChange, object: navService.router, userInfo: payload)
-        
-        //fire the fake notification
-        subject.progressDidChange(fakeNotication)
-        
-        //retreieve the update
-        guard let update = mapSpy.estimatesUpdate else {
-            XCTFail("The CPNVC needs to update the map template with new estimates when it recieves a progress update.")
+        guard let firstCoordinate = navigationService.routeProgress.currentLeg.shape.coordinates.first else {
+            XCTFail("First coorindate should be valid.")
             return
         }
+        let userInfo: [RouteController.NotificationUserInfoKey: Any] = [
+            .routeProgressKey: navigationService.routeProgress,
+            .locationKey: CLLocation(latitude: firstCoordinate.latitude, longitude: firstCoordinate.longitude),
+        ]
+        let progressDidChangeNotification = NSNotification(name: .routeControllerProgressDidChange,
+                                                           object: navigationService.router,
+                                                           userInfo: userInfo)
         
-        // establish a point of truth and fetch the answer from the update
-        let distanceTruth = Measurement(distance: progress.distanceRemaining).localized()
-        let estimateTruth = CPTravelEstimates(distanceRemaining: distanceTruth, timeRemaining: progress.durationRemaining)
-        let answer = update.0
+        carPlayNavigationViewController.progressDidChange(progressDidChangeNotification)
         
-        //verify answer is correct
-        let distanceEqual = answer.distanceRemaining.value == estimateTruth.distanceRemaining.value
-        let timeEqual = answer.timeRemaining.doubleValue == estimateTruth.timeRemaining.doubleValue
-        XCTAssert(distanceEqual && timeEqual, "The subject should update the CP Map Template based upon CPTravelEstimates derived from the entire route progress.")
+        let distanceRemaining = Measurement(distance: navigationService.routeProgress.distanceRemaining).localized()
+        let expectedTravelEstimates = CPTravelEstimates(distanceRemaining: distanceRemaining,
+                                                        timeRemaining: navigationService.routeProgress.durationRemaining)
+        let actualTravelEstimates = mapTemplate.estimatesUpdate?.0
+        
+        XCTAssertEqual(actualTravelEstimates?.distanceRemaining.value,
+                       expectedTravelEstimates.distanceRemaining.value,
+                       "Remaining distances should be equal.")
+        XCTAssertEqual(actualTravelEstimates?.timeRemaining.doubleValue,
+                       expectedTravelEstimates.timeRemaining.doubleValue,
+                       "Remaining times should be equal.")
     }
 }
