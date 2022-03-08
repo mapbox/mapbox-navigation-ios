4fc0c19c48be37f5e766f9ff2ac0d89d29bf4197
Shan Ma <shanma@alumni.usc.edu>
2022-03-08 12:46:40 -0800

    Aligh the margin of floating buttons with map attribution button during active navigation.

--- Sources/MapboxNavigation/NavigationViewLayout.swift
+++ Sources/MapboxNavigation/NavigationViewLayout.swift
@@ -281,10 +281,4 @@ extension NavigationView {
         compactConstraints.forEach({ $0.isActive = traitCollection.verticalSizeClass == .compact })
         regularConstraints.forEach({ $0.isActive = traitCollection.verticalSizeClass == .regular })
     }
-    
-    open override func traitCollectionDidChange(_ previousTraitCollection: UITraitCollection?) {
-        if previousTraitCollection == traitCollection { return }
-        
-        setupConstraints()
-    }
 }
