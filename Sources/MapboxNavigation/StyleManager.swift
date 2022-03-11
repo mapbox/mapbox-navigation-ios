00a7a0fa05ce89ec63aca34bc3e1091c6611e09c
Shan Ma <shanma@alumni.usc.edu>
2022-03-11 13:55:07 -0800

    Bring back the deprecated styleManager(_:viewForApplying:).

--- Sources/MapboxNavigation/StyleManager.swift
+++ Sources/MapboxNavigation/StyleManager.swift
@@ -13,6 +13,15 @@ public protocol StyleManagerDelegate: AnyObject, UnimplementedLogging {
     func location(for styleManager: StyleManager) -> CLLocation?
     
     /**
+     Asks the delegate for the view to be used when refreshing appearance.
+     
+     The default implementation of this method will attempt to cast the delegate to type
+     `UIViewController` and use its `view` property.
+     */
+    @available(*, deprecated, message: "All views appearance will be refreshed without the `UITextEffectsWindow`.")
+    func styleManager(_ styleManager: StyleManager, viewForApplying currentStyle: Style?) -> UIView?
+    
+    /**
      Informs the delegate that a style was applied.
      
      This delegate method is the equivalent of `Notification.Name.styleManagerDidApplyStyle`.
@@ -46,6 +55,17 @@ public extension StyleManagerDelegate {
      */
     func styleManagerDidRefreshAppearance(_ styleManager: StyleManager) {
         logUnimplemented(protocolType: StyleManagerDelegate.self, level: .debug)
+    }
+    
+    @available(*, deprecated, message: "All views appearance will be refreshed without the `UITextEffectsWindow`.")
+    func styleManager(_ styleManager: StyleManager, viewForApplying currentStyle: Style?) -> UIView? {
+        // Short-circuit refresh logic if the view hasn't yet loaded since we don't want the `self.view`
+        // call to trigger `loadView`.
+        if let vc = self as? UIViewController, vc.isViewLoaded {
+            return vc.view
+        }
+        
+        return nil
     }
 }
 
