3e21e6fcc91a29e90ef5867cc0198f852e20399e
Maxim Makhun <maxim.makhun@mapbox.com>
2022-03-31 13:15:50 -0700

    Add the ability to show `PreviewViewControllerExample` in settings menu.

--- PreviewViewControllerExample/AppDelegate.swift
+++ PreviewViewControllerExample/AppDelegate.swift
@@ -5,6 +5,13 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
     
     func application(_ application: UIApplication,
                      didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
+        
+        // Request authorization to user notifications to be able to list application in
+        // iOS settings menu.
+        UNUserNotificationCenter.current().requestAuthorization(options: [.alert, .sound, .badge]) { _, _ in
+            // No-op
+        }
+        
         return true
     }
     
