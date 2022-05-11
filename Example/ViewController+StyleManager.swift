319a0d2ca5f4f9bb9fbdde3928b578a327ca6ea7
Maxim Makhun <maxim.makhun@mapbox.com>
2022-05-10 18:02:44 -0700

    Fix an issue that occurs when style changes, but `NavigationMapView` is no longer available.

--- Example/ViewController+StyleManager.swift
+++ Example/ViewController+StyleManager.swift
@@ -4,12 +4,18 @@ import MapboxNavigation
 import MapboxMaps
 
 extension ViewController : StyleManagerDelegate {
+    
     func location(for styleManager: MapboxNavigation.StyleManager) -> CLLocation? {
+        // Sometimes when style changes user might be already actively navigating, this means that
+        // `navigationMapView` is no longer available.
+        if navigationMapView == nil { return nil }
+        
         if let location = navigationMapView.mapView.location.latestLocation?.location {
             return location
         } else if let location = CLLocationManager.init().location {
             return location
         }
+        
         return nil
     }
     
