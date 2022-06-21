21de1674b49dc826cdf1265358e3dd0454c2f424
Maxim Makhun <maxim.makhun@mapbox.com>
2022-06-21 10:28:12 -0700

    Simplify routes presentation with custom camera options on CarPlay.

--- Sources/MapboxNavigation/NavigationMapView.swift
+++ Sources/MapboxNavigation/NavigationMapView.swift
@@ -2125,19 +2125,11 @@ open class NavigationMapView: UIView {
         }
         
         let edgeInsets = safeArea + UIEdgeInsets.centerEdgeInsets
-        
-        let bearing: CGFloat?
-        // In case of CarPlay while previewing routes always use bearing that is set to zero.
-        if traitCollection.userInterfaceIdiom == .carPlay {
-            bearing = 0.0
-        } else {
-            bearing = customCameraOptions.flatMap({ $0.bearing }).map({ CGFloat($0) })
-        }
-        
+        let bearing = customCameraOptions.flatMap({ $0.bearing }).map({ CGFloat($0) })
         if let cameraOptions = mapView?.mapboxMap.camera(for: geometry,
-                                                            padding: customCameraOptions?.padding ?? edgeInsets,
-                                                            bearing: bearing,
-                                                            pitch: customCameraOptions?.pitch) {
+                                                         padding: customCameraOptions?.padding ?? edgeInsets,
+                                                         bearing: bearing,
+                                                         pitch: customCameraOptions?.pitch) {
             mapView?.camera.ease(to: cameraOptions, duration: animated ? 1.0 : 0.0)
         }
     }
