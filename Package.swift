a26ea36d35b787bdcdd828c672388995a42e3e36
Maxim Makhun <maxim.makhun@mapbox.com>
2022-08-08 09:56:32 -0600

    Pin to Mapbox Common `v21.3.0`.

--- Package.swift
+++ Package.swift
@@ -25,7 +25,8 @@ let package = Package(
         .package(name: "MapboxDirections", url: "https://github.com/mapbox/mapbox-directions-swift.git", from: "2.5.0"),
         .package(name: "MapboxMobileEvents", url: "https://github.com/mapbox/mapbox-events-ios.git", from: "1.0.0"),
         .package(name: "MapboxNavigationNative", url: "https://github.com/mapbox/mapbox-navigation-native-ios.git", from: "101.0.1"),
-        .package(name: "MapboxMaps", url: "https://github.com/mapbox/mapbox-maps-ios.git", .branch("maxim/mapbox-common-spm-update-to-v21.4.0")),
+        .package(name: "MapboxMaps", url: "https://github.com/mapbox/mapbox-maps-ios.git", from: "10.5.0"),
+        .package(name: "MapboxCommon", url: "https://github.com/mapbox/mapbox-common-ios.git", .exact("21.3.0")),
         .package(name: "Solar", url: "https://github.com/ceeK/Solar.git", from: "3.0.0"),
         .package(name: "MapboxSpeech", url: "https://github.com/mapbox/mapbox-speech-swift.git", from: "2.0.0"),
         .package(name: "Quick", url: "https://github.com/Quick/Quick.git", from: "3.1.2"),
@@ -40,6 +41,7 @@ let package = Package(
                 "MapboxDirections",
                 "MapboxMobileEvents",
                 "MapboxNavigationNative",
+                "MapboxCommon"
             ],
             exclude: ["Info.plist"],
             resources: [.copy("MBXInfo.plist")]),
@@ -51,6 +53,7 @@ let package = Package(
                 "MapboxMaps",
                 "MapboxSpeech",
                 "Solar",
+                "MapboxCommon"
             ],
             exclude: ["Info.plist"],
             resources: [.copy("MBXInfo.plist")]),
