7bcef21a9f55a772764d5c37bb08e5ed963a6943
Maxim Makhun <maxim.makhun@mapbox.com>
2022-09-12 12:10:23 -0700

    Update.

--- Package.swift
+++ Package.swift
@@ -6,7 +6,7 @@ import PackageDescription
 let package = Package(
     name: "MapboxNavigation",
     defaultLocalization: "en",
-    platforms: [.iOS(.v11)],
+    platforms: [.iOS(.v12)],
     products: [
         .library(
             name: "MapboxCoreNavigation",
