38068df84250ffad236eed1bf68bdf882bb5fd1c
Maxim Makhun <maxim.makhun@mapbox.com>
2022-09-23 13:55:02 -0700

    Update.

--- PreviewViewControllerExample/Transitions/PresentationAnimator.swift
+++ PreviewViewControllerExample/Transitions/PresentationAnimator.swift
@@ -14,34 +14,9 @@ class PresentationAnimator: NSObject, UIViewControllerAnimatedTransitioning {
             return
         }
         
-        // Replace already exisiting `NavigationMapView` from `NavigationViewController` with `NavigationMapView`
+        // Replace default `NavigationMapView` in `NavigationViewController` with `NavigationMapView`
         // that was used in `PreviewViewController`.
         toViewController.navigationView.navigationMapView = fromViewController.navigationView.navigationMapView
-        
-        toViewController.navigationMapView?.userLocationStyle = .courseView()
-        
-        // Switch navigation camera to active navigation mode.
-        toViewController.navigationMapView?.navigationCamera.viewportDataSource = NavigationViewportDataSource(toViewController.navigationView.navigationMapView.mapView,
-                                                                                                               viewportDataSourceType: .active)
-        
-        let navigationViewportDataSource = toViewController.navigationMapView?.navigationCamera.viewportDataSource as? NavigationViewportDataSource
-        navigationViewportDataSource?.options.followingCameraOptions.centerUpdatesAllowed = true
-        navigationViewportDataSource?.options.followingCameraOptions.bearingUpdatesAllowed = true
-        navigationViewportDataSource?.options.followingCameraOptions.pitchUpdatesAllowed = true
-        navigationViewportDataSource?.options.followingCameraOptions.paddingUpdatesAllowed = true
-        navigationViewportDataSource?.options.followingCameraOptions.zoomUpdatesAllowed = true
-        
-        toViewController.navigationMapView?.navigationCamera.follow()
-        
-        // Render part of the route that has been traversed with full transparency, to give the illusion of a disappearing route.
-        toViewController.routeLineTracksTraversal = true
-        
-        // Hide top and bottom container views before animating their presentation.
-        toViewController.navigationView.topBannerContainerView.isHidden = true
-        toViewController.navigationView.topBannerContainerView.show()
-        
-        toViewController.navigationView.bottomBannerContainerView.isHidden = true
-        toViewController.navigationView.bottomBannerContainerView.show()
         
         transitionContext.containerView.addSubview(toViewController.view)
         transitionContext.completeTransition(true)
