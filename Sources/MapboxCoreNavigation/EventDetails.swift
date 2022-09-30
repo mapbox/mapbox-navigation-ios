bf5b6b2d7ebcaeb729ccdcd06a0069f1a936f97c
Alexander Pristavko <alexander.pristavko@mapbox.com>
2022-09-30 22:18:00 +0800

    Cache UIDevice.machine property

--- Sources/MapboxCoreNavigation/EventDetails.swift
+++ Sources/MapboxCoreNavigation/EventDetails.swift
@@ -95,7 +95,7 @@ extension GlobalEventDetails {
     }
     var batteryLevel: Int { UIDevice.current.batteryLevel >= 0 ? Int(UIDevice.current.batteryLevel * 100) : -1 }
     var batteryPluggedIn: Bool { [.charging, .full].contains(UIDevice.current.batteryState) }
-    var device: String { UIDevice.current.machine }
+    var device: String { UIDeviceCache.machine }
     var operatingSystem: String { "\(ProcessInfo.systemName) \(ProcessInfo.systemVersion)" }
     var platform: String { ProcessInfo.systemName }
     var sdkVersion: String {
@@ -176,16 +176,14 @@ extension AVAudioSession {
     }
 }
 
-extension UIDevice {
-    @nonobjc var machine: String {
-        get {
-            var systemInfo = utsname()
-            uname(&systemInfo)
-            let machineMirror = Mirror(reflecting: systemInfo.machine)
-            return machineMirror.children.reduce("") { (identifier: String, element: Mirror.Child) in
-                guard let value = element.value as? Int8, value != 0 else { return identifier }
-                return identifier + String(UnicodeScalar(UInt8(value)))
-            }
+enum UIDeviceCache {
+    static let machine: String = {
+        var systemInfo = utsname()
+        uname(&systemInfo)
+        let machineMirror = Mirror(reflecting: systemInfo.machine)
+        return machineMirror.children.reduce("") { (identifier: String, element: Mirror.Child) in
+            guard let value = element.value as? Int8, value != 0 else { return identifier }
+            return identifier + String(UnicodeScalar(UInt8(value)))
         }
-    }
+    }()
 }
