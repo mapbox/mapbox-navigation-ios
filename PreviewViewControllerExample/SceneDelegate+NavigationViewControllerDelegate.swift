b7fa26af44e5115781d393962b407fcdb0513c50
Maxim Makhun <maxim.makhun@mapbox.com>
2022-10-10 17:46:04 -0700

    Update.

--- PreviewViewControllerExample/SceneDelegate+NavigationViewControllerDelegate.swift
+++ PreviewViewControllerExample/SceneDelegate+NavigationViewControllerDelegate.swift
@@ -6,18 +6,21 @@ extension SceneDelegate: NavigationViewControllerDelegate {
     public func navigationViewControllerDidDismiss(_ navigationViewController: NavigationViewController,
                                                    byCanceling canceled: Bool) {
         // Hide top and bottom banner containers and after that dismiss `NavigationViewController`.
-        navigationViewController.navigationView.topBannerContainerView.hide(duration: animationDuration,
+        navigationViewController.navigationView.topBannerContainerView.hide(animated: shouldAnimate,
+                                                                            duration: animationDuration,
                                                                             animations: {
             navigationViewController.navigationView.topBannerContainerView.alpha = 0.0
         })
         
-        navigationViewController.navigationView.bottomBannerContainerView.hide(duration: animationDuration,
+        navigationViewController.navigationView.bottomBannerContainerView.hide(animated: shouldAnimate,
+                                                                               duration: animationDuration,
                                                                                animations: {
             navigationViewController.navigationView.bottomBannerContainerView.alpha = 0.0
             navigationViewController.navigationView.speedLimitView.alpha = 0.0
             navigationViewController.navigationView.wayNameView.alpha = 0.0
             navigationViewController.navigationView.floatingStackView.alpha = 0.0
-        }, completion: { _ in
+        },
+                                                                               completion: { _ in
             navigationViewController.dismiss(animated: true) {
                 let navigationView = self.previewViewController.navigationView
                 let navigationMapView = navigationView.navigationMapView
@@ -36,18 +39,20 @@ extension SceneDelegate: NavigationViewControllerDelegate {
                 // Depending on currently presented banner perform required map or camera modifications.
                 if let routesPreviewViewController = self.previewViewController.topBanner(.bottomLeading) as? RoutesPreviewViewController {
                     let routeResponse = routesPreviewViewController.routesPreviewOptions.routeResponse
-                    self.previewViewController.showcase(routeResponse: routeResponse)
-                    
-                    navigationView.topBannerContainerView.show(duration: self.animationDuration)
-                    navigationView.bottomBannerContainerView.show(duration: self.animationDuration,
-                                                                  animations: { [weak self] in
-                        guard let self = self else { return }
-                        self.previewViewController.navigationView.floatingStackView.alpha = 1.0
-                    })
-                } else if self.previewViewController.topBanner(.bottomLeading) is DestinationPreviewViewController {
-                    navigationView.bottomBannerContainerView.show(duration: self.animationDuration)
-                    navigationView.topBannerContainerView.show(duration: self.animationDuration)
+                    self.previewViewController.showcase(routeResponse: routeResponse,
+                                                        duration: self.animationDuration)
                 }
+                
+                navigationView.topBannerContainerView.show(duration: self.animationDuration,
+                                                           animations: {
+                    navigationView.topBannerContainerView.alpha = 1.0
+                })
+                
+                navigationView.bottomBannerContainerView.show(duration: self.animationDuration,
+                                                              animations: {
+                    navigationView.floatingStackView.alpha = 1.0
+                    navigationView.bottomBannerContainerView.alpha = 1.0
+                })
             }
         })
     }
