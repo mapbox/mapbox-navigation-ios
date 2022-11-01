e1afc5bdf469c1ecef0e055c1c2265d20829fd97
Maxim Makhun <maxim.makhun@mapbox.com>
2022-11-01 13:34:46 -0700

    Add `PreviewViewControllerDelegate` documentation.

--- Sources/MapboxNavigation/PreviewViewControllerDelegate.swift
+++ Sources/MapboxNavigation/PreviewViewControllerDelegate.swift
@@ -3,18 +3,48 @@ import CoreLocation
 import MapboxDirections
 import MapboxCoreNavigation
 
-// :nodoc:
+/**
+ `PreviewViewControllerDelegate` allows to observe `Banner` presentation and dimissal events.
+ */
 public protocol PreviewViewControllerDelegate: AnyObject, UnimplementedLogging {
     
+    /**
+     Tells the delegate that the `Banner` is about to be presented on the screen.
+     
+     - parameter previewViewController: `PreviewViewController` instance that performs banner
+     presentation.
+     - parameter banner: `Banner` that will be presented.
+     */
     func previewViewController(_ previewViewController: PreviewViewController,
                                willPresent banner: Banner)
     
+    /**
+     Tells the delegate that the `Banner` was presented on the screen.
+     
+     - parameter previewViewController: `PreviewViewController` instance that performs banner
+     presentation.
+     - parameter banner: `Banner` that was presented.
+     */
     func previewViewController(_ previewViewController: PreviewViewController,
                                didPresent banner: Banner)
     
+    /**
+     Tells the delegate that the `Banner` will disappear from the screen.
+     
+     - parameter previewViewController: `PreviewViewController` instance that performs banner
+     dismissal.
+     - parameter banner: `Banner` that will be dismissed.
+     */
     func previewViewController(_ previewViewController: PreviewViewController,
                                willDismiss banner: Banner)
     
+    /**
+     Tells the delegate that the `Banner` disappeared from the screen.
+     
+     - parameter previewViewController: `PreviewViewController` instance that performs banner
+     dismissal.
+     - parameter banner: `Banner` that was dismissed.
+     */
     func previewViewController(_ previewViewController: PreviewViewController,
                                didDismiss banner: Banner)
 }
@@ -22,21 +52,33 @@ public protocol PreviewViewControllerDelegate: AnyObject, UnimplementedLogging {
 // :nodoc:
 public extension PreviewViewControllerDelegate {
     
+    /**
+     `UnimplementedLogging` prints a warning to standard output the first time this method is called.
+     */
     func previewViewController(_ previewViewController: PreviewViewController,
                                willPresent banner: Banner) {
         logUnimplemented(protocolType: PreviewViewControllerDelegate.self, level: .debug)
     }
     
+    /**
+     `UnimplementedLogging` prints a warning to standard output the first time this method is called.
+     */
     func previewViewController(_ previewViewController: PreviewViewController,
                                didPresent banner: Banner) {
         logUnimplemented(protocolType: PreviewViewControllerDelegate.self, level: .debug)
     }
     
+    /**
+     `UnimplementedLogging` prints a warning to standard output the first time this method is called.
+     */
     func previewViewController(_ previewViewController: PreviewViewController,
                                willDismiss banner: Banner) {
         logUnimplemented(protocolType: PreviewViewControllerDelegate.self, level: .debug)
     }
     
+    /**
+     `UnimplementedLogging` prints a warning to standard output the first time this method is called.
+     */
     func previewViewController(_ previewViewController: PreviewViewController,
                                didDismiss banner: Banner) {
         logUnimplemented(protocolType: PreviewViewControllerDelegate.self, level: .debug)
