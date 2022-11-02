b7221062b240383f3fac06649ae69faee9f7f660
Maxim Makhun <maxim.makhun@mapbox.com>
2022-11-01 21:08:32 -0700

    Add documentation for `DestinationOptions` and `DestinationDataSource`.

--- Sources/MapboxNavigation/DestinationDataSource.swift
+++ Sources/MapboxNavigation/DestinationDataSource.swift
@@ -1,7 +1,16 @@
 import UIKit
 
-// :nodoc:
+/**
+ Protocol, to which `Banner` instances should conform to, to provide the options that are required
+ for the destination presentation in the `PreviewViewController`.
+ 
+ By default Mapbox Navigation SDK provides `DestinationPreviewViewController` that conforms to this
+ protocol and allows to present banner that shows final destination related information.
+ */
 public protocol DestinationDataSource: AnyObject {
     
+    /**
+     Options that are required to present destination information.
+     */
     var destinationOptions: DestinationOptions { get }
 }
--- Sources/MapboxNavigation/DestinationOptions.swift
+++ Sources/MapboxNavigation/DestinationOptions.swift
@@ -1,25 +1,43 @@
 import CoreLocation
 import MapboxDirections
 
-// :nodoc:
+/**
+ Customization options for the destination(s) preview using `DestinationPreviewViewController` banner.
+ */
 public struct DestinationOptions {
     
-    // :nodoc:
+    /**
+     Primary text that is going to be shown in `DestinationPreviewViewController`. In case if
+     last waypoint in `DestinationOptions.waypoints` contains non-nil value `Waypoint.name` it'll be
+     used.
+     */
     public var primaryText: NSAttributedString?
     
-    // :nodoc:
+    /**
+     Array of waypoints which are presented.
+     */
     public let waypoints: [Waypoint]
     
-    // :nodoc:
+    /**
+     Array of coordinates which are presented.
+     */
     public let coordinates: [CLLocationCoordinate2D]
     
-    // :nodoc:
+    /**
+     Initializes a `DestinationOptions` struct.
+     
+     - paramater coordinates: List of coordinates which are presented.
+     */
     public init(coordinates: [CLLocationCoordinate2D]) {
         let waypoints = coordinates.map({ Waypoint(coordinate: $0) })
         self.init(waypoints: waypoints)
     }
     
-    // :nodoc:
+    /**
+     Initializes a `DestinationOptions` struct.
+     
+     - paramater waypoints: List of waypoints which are presented.
+     */
     public init(waypoints: [Waypoint]) {
         self.waypoints = waypoints
         self.coordinates = waypoints.map({ CLLocationCoordinate2D(latitude: $0.coordinate.latitude, longitude: $0.coordinate.longitude) })
