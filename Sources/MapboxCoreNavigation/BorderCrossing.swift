6b98b2cd314bf1415402f16149bc499171d795a1
Nastassia Makaranka <nastassia.makaranka@mapbox.com>
2022-11-02 17:55:09 +0100

    Make BorderCrossing constructor public (#4226)

--- Sources/MapboxCoreNavigation/BorderCrossing.swift
+++ Sources/MapboxCoreNavigation/BorderCrossing.swift
@@ -10,14 +10,23 @@ extension AdministrativeRegion {
 }
 
 /**
- `BorderCrossingInfo` encapsulates a border crossing, specifying crossing region codes.
+ `BorderCrossing` encapsulates a border crossing, specifying crossing region codes.
  */
 public struct BorderCrossing {
     public let from: AdministrativeRegion
     public let to: AdministrativeRegion
-    
+
+    /// Initializes a new `BorderCrossing` object.
+    /// - Parameters:
+    ///   - from: origin administrative region
+    ///   - to: destination administrative region
+    public init(from: AdministrativeRegion, to: AdministrativeRegion) {
+        self.from = from
+        self.to = to
+    }
+
     init(_ borderCrossing: BorderCrossingInfo) {
-        from = AdministrativeRegion(borderCrossing.from)
-        to = AdministrativeRegion(borderCrossing.to)
+        self.init(from: AdministrativeRegion(borderCrossing.from),
+                  to: AdministrativeRegion(borderCrossing.to))
     }
 }
