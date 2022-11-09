9f211d6885bd6c59fa71e51bcc926ce375efd5e2
Aliaksandr Bialiauski <413986+S2Ler@users.noreply.github.com>
2022-11-09 19:22:45 +0300

    Remove location parameter
    The location is already available in the status.

--- Sources/MapboxCoreNavigation/RouteController.swift
+++ Sources/MapboxCoreNavigation/RouteController.swift
@@ -518,7 +518,7 @@ open class RouteController: NSObject {
     }
     
     private func update(progress: RouteProgress, status: NavigationStatus, with location: CLLocation, rawLocation: CLLocation, upcomingRouteAlerts routeAlerts: [UpcomingRouteAlert], mapMatchingResult: MapMatchingResult, routeShapeIndex: Int) {
-        progress.updateDistanceTraveled(with: location, navigationStatus: status)
+        progress.updateDistanceTraveled(navigationStatus: status)
         progress.upcomingRouteAlerts = routeAlerts.map { RouteAlert($0) }
         progress.shapeIndex = routeShapeIndex
         
