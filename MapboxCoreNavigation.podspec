cea3cf569517808a19df39c243e3f5326feb476e
Maxim Makhun <maxim.makhun@mapbox.com>
2022-11-29 17:00:51 -0800

    Update.

--- MapboxCoreNavigation.podspec
+++ MapboxCoreNavigation.podspec
@@ -41,7 +41,7 @@ Pod::Spec.new do |s|
   s.module_name = "MapboxCoreNavigation"
 
   s.dependency "MapboxNavigationNative", "~> 101.0.1"
-  s.dependency "MapboxDirections", "~> 2.6.0"
+  s.dependency "MapboxDirections", "~> 2.7.0"
   s.dependency "MapboxMobileEvents", "~> 1.0"
 
   s.swift_version = "5.0"
--- Package.swift
+++ Package.swift
@@ -22,7 +22,7 @@ let package = Package(
         )
     ],
     dependencies: [
-        .package(name: "MapboxDirections", url: "https://github.com/mapbox/mapbox-directions-swift.git", .upToNextMinor(from: "2.6.0")),
+        .package(name: "MapboxDirections", url: "https://github.com/mapbox/mapbox-directions-swift.git", .upToNextMinor(from: "2.7.0")),
         .package(name: "MapboxMobileEvents", url: "https://github.com/mapbox/mapbox-events-ios.git", from: "1.0.0"),
         .package(name: "MapboxNavigationNative", url: "https://github.com/mapbox/mapbox-navigation-native-ios.git", from: "101.0.1"),
         .package(name: "MapboxMaps", url: "https://github.com/mapbox/mapbox-maps-ios.git", .upToNextMinor(from: "10.5.0")),
