5be4bb9e42126d074e888cea31037a9aa4d9ec07
Maxim Makhun <maxim.makhun@mapbox.com>
2022-12-12 09:55:32 -0800

    Update.

--- Package.swift
+++ Package.swift
@@ -22,7 +22,7 @@ let package = Package(
         )
     ],
     dependencies: [
-        .package(name: "MapboxDirections", url: "https://github.com/mapbox/mapbox-directions-swift.git", from: "2.8.0"),
+        .package(name: "MapboxDirections", url: "https://github.com/mapbox/mapbox-directions-swift.git", .branch("maxim/NAVIOS-328-add-support-for-sapa-facilities")),
         .package(name: "MapboxMobileEvents", url: "https://github.com/mapbox/mapbox-events-ios.git", from: "1.0.0"),
         .package(name: "MapboxNavigationNative", url: "https://github.com/mapbox/mapbox-navigation-native-ios.git", from: "123.0.0"),
         .package(name: "MapboxMaps", url: "https://github.com/mapbox/mapbox-maps-ios.git", from: "10.10.0"),
--- Sources/MapboxCoreNavigation/RestStop.swift
+++ Sources/MapboxCoreNavigation/RestStop.swift
@@ -7,9 +7,9 @@ extension RestStop {
     init(_ serviceArea: ServiceAreaInfo) {
         switch serviceArea.type {
         case .restArea:
-            self.init(type: .restArea, name: serviceArea.name)
+            self.init(type: .restArea, name: serviceArea.name, amenities: [])
         case .serviceArea:
-            self.init(type: .serviceArea, name: serviceArea.name)
+            self.init(type: .serviceArea, name: serviceArea.name, amenities: [])
         @unknown default:
             fatalError("Unknown ServiceAreaInfo type.")
         }
