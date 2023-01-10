89221474862d8a713cd13f101f2e2df21fda838c
sevastian.zhukov <sevastian.zhukov@mapbox.com>
2023-01-10 19:10:01 +0700

    test

---
+++ scripts/distribute-version.py
@@ -0,0 +1,13 @@
+import subprocess
+import time
+
+
+def push_pod(name):
+    while True:
+        status = subprocess.run('pod repo update && pod trunk push ' + name + '.podspec', shell=True)
+        if status.returncode == 0:
+            break
+        time.sleep(10)
+
+
+push_pod('MapboxNavigationTest')
