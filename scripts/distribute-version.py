8be83490bea74b58f19634301006fcba8af43fea
sevastian.zhukov <sevastian.zhukov@mapbox.com>
2023-01-11 19:19:42 +0700

    remove infinity loop, add max attempts, add exponential delay
    (cherry picked from commit 4f7e699b46bd08b99e088a3212e8901385f56e7b)

--- scripts/distribute-version.py
+++ scripts/distribute-version.py
@@ -2,13 +2,15 @@ import subprocess
 import time
 
 
-def push_pod(name):
-    while True:
+def push_pod(name, max_attempts):
+    for attempt in range(0, max_attempts):
         status = subprocess.run('pod repo update && pod trunk push ' + name + '.podspec', shell=True)
         if status.returncode == 0:
-            break
-        time.sleep(10)
+            return
+        time.sleep(60 * pow(2, attempt))
+    raise Exception('Maximum number of attempts have been made for ' + name)
 
 
-push_pod('MapboxCoreNavigation')
-push_pod('MapboxNavigation')
+push_pod('MapboxCoreNavigation', 5)
+time.sleep(60)
+push_pod('MapboxNavigation', 5)
