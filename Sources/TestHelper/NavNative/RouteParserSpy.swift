765ecaaf344c1c0289add5d1169d24d2cc4b9d6c
Udumft <viktor.kononov@mapbox.com>
2023-05-25 11:07:28 +0300

    vk/dependency-update: unit tests fix

--- Sources/TestHelper/NavNative/RouteParserSpy.swift
+++ Sources/TestHelper/NavNative/RouteParserSpy.swift
@@ -16,4 +16,15 @@ public final class RouteParserSpy: RouteParser  {
         return returnedRoutes != nil ? Expected(value: (returnedRoutes ?? []) as NSArray) :
             Expected(error: (returnedError ?? "") as NSString)
     }
+    
+    @_implementationOnly
+    public override class func parseDirectionsResponse(forResponseDataRef responseDataRef: DataRef,
+                                                request: String,
+                                                routeOrigin: RouterOrigin) -> Expected<NSArray, NSString> {
+        if returnedRoutes == nil && returnedError == nil {
+            return RouteParser.parseDirectionsResponse(forResponseDataRef: responseDataRef, request: request, routeOrigin: routeOrigin)
+        }
+        return returnedRoutes != nil ? Expected(value: (returnedRoutes ?? []) as NSArray) :
+        Expected(error: (returnedError ?? "") as NSString)
+    }
 }
