421000ac0a6c915095f2f882bb8289b7f2071979
Francisco-Costa98 <franciscomffcosta@hotmail.com>
2023-08-07 17:33:20 +0100

    updated linting

--- Sources/MapboxNavigation/Bundle.swift
+++ Sources/MapboxNavigation/Bundle.swift
@@ -13,9 +13,10 @@ extension Bundle {
             return .module
             #else
             var frameworkBundle = Bundle(for: NavigationViewController.self)
+            
             guard let resourceBundleURL = frameworkBundle.url(
             forResource: "MapboxNavigationResources", withExtension: "bundle")
-            else { fatalError("MapboxNavigationResources.bundle not found!") }
+                else { fatalError("MapboxNavigationResources.bundle not found!") }
             
             guard let resourceBundle = Bundle(url: resourceBundleURL)
                 else { fatalError("Cannot access MapboxNavigationResources.bundle!") }
