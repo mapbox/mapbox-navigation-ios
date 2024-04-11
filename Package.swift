8458e91ffff1b082329c1eec22a4af8216ed3fd5
Nastassia Makaranka <nastassia.makaranka@mapbox.com>
2024-04-12 00:52:28 +0200

    Use prod Common version

--- Package.swift
+++ Package.swift
@@ -6,7 +6,7 @@ import PackageDescription
 let version = "3.0.0-rc.1"
 
 let binaries = [
-    "MapboxCommon": "a8874af6acfafac0f5cf2c9a051967e4381bb7ed3707a44b9c6e6bef9d488456",
+    //"MapboxCommon": "a8874af6acfafac0f5cf2c9a051967e4381bb7ed3707a44b9c6e6bef9d488456",
     "MapboxCoreMaps": "e03cbb9f9c60dcaaa6f58b83d57d7796e95d5cd9ae4e47cc194b7c917c930e31",
     "MapboxDirections": "974559a90d6aba462bb0527001456dc16ffd3ae506791cfd3d61fc986fe02d0e",
     "MapboxMaps": "97485654d30264683df34e9cf0be5e4d09262759b3b1550ea21910172413e8fe",
@@ -24,10 +24,15 @@ let package = Package(
             targets: ["MapboxNavigationCoreWrapper"]
         ),
     ],
+    dependencies: [
+        .package(name: "MapboxCommon", url: "https://github.com/mapbox/mapbox-common-ios.git", from: "24.3.1"),
+    ],
     targets: [
         .target(
             name: "MapboxNavigationCoreWrapper",
-            dependencies: binaries.keys.map { .byName(name: $0) }
+            dependencies: binaries.keys.map { .byName(name: $0) } + [
+                "MapboxCommon"
+            ]
         )
     ] + binaryTargets()
 )
