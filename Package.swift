9dd17b279139713ac40187bf221fbbd69c519462
Nastassia Makaranka <nastassia.makaranka@mapbox.com>
2024-04-12 13:58:49 +0200

    Add MapboxNavigationUIKit target

--- Package.swift
+++ Package.swift
@@ -6,14 +6,13 @@ import PackageDescription
 let version = "3.0.0-rc.1"
 
 let binaries = [
-    //"MapboxCommon": "a8874af6acfafac0f5cf2c9a051967e4381bb7ed3707a44b9c6e6bef9d488456",
     "MapboxCoreMaps": "e03cbb9f9c60dcaaa6f58b83d57d7796e95d5cd9ae4e47cc194b7c917c930e31",
     "MapboxDirections": "974559a90d6aba462bb0527001456dc16ffd3ae506791cfd3d61fc986fe02d0e",
     "MapboxMaps": "97485654d30264683df34e9cf0be5e4d09262759b3b1550ea21910172413e8fe",
-    "MapboxNavigationNative": "743b54c3cbb92f77458cf3c29278b863e0b30b745f737606a29fc980a839fb20",
     "Turf": "2f5fffc7075f8582aca328f13b49e14cfb13d3ed1ee0789e53d657d827860b6f",
     "MapboxNavigationCore": "17f52c9aa1d941638489a3f2d55e8184ce17012c8eee0156e86cdcfbdb4bf189",
     "_MapboxNavigationUXPrivate": "1ee894eee848474826d5ab21761067d966e7343e0f436b985d21489fe6b2c3e2",
+    "MapboxNavigationUIKit": "1111111",
 ]
 
 let package = Package(
@@ -23,27 +22,48 @@ let package = Package(
             name: "MapboxNavigationCore",
             targets: ["MapboxNavigationCoreWrapper"]
         ),
+        .library(
+            name: "MapboxNavigationUIKit",
+            targets: ["MapboxNavigationUIKitWrapper"]
+        ),
     ],
     dependencies: [
         .package(name: "MapboxCommon", url: "https://github.com/mapbox/mapbox-common-ios.git", from: "24.3.1"),
+        .package(name: "MapboxNavigationNative", url: "https://github.com/mapbox/mapbox-navigation-native-ios.git", from: "305.0.0"),
     ],
     targets: [
         .target(
             name: "MapboxNavigationCoreWrapper",
             dependencies: binaries.keys.map { .byName(name: $0) } + [
-                "MapboxCommon"
+                "MapboxCommon",
+                "MapboxNavigationNative"
             ]
-        )
+        ),
+        .target(
+            name: "MapboxNavigationUIKitWrapper",
+            dependencies: [
+                "MapboxNavigationCoreWrapper"
+            ]
+        ),
     ] + binaryTargets()
 )
 
+let usesRemoteBinaries = true
+
 func binaryTargets() -> [Target] {
     return binaries.map { binaryName, checksum in
-        Target.binaryTarget(
-            name: binaryName,
-            url: "https://api.mapbox.com/downloads/v2/navsdk-v3-ios" +
-                "/releases/ios/packages/\(version)/\(binaryName).xcframework.zip",
-            checksum: checksum
-        )
+        if usesRemoteBinaries {
+            Target.binaryTarget(
+                name: binaryName,
+                url: "https://api.mapbox.com/downloads/v2/navsdk-v3-ios" +
+                    "/releases/ios/packages/\(version)/\(binaryName).xcframework.zip",
+                checksum: checksum
+            )
+        } else {
+            Target.binaryTarget(
+                name: binaryName,
+                path: "./XCFrameworks/\(binaryName).xcframework"
+            )
+        }
     }
 }
