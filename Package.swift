6e3f55830f53c5e27e42d837e7be4b7f43e15448
root <root@348777b556d6>
2024-05-24 13:54:13 +0000

    Update Nav v3 iOS dependencies 3.1.0

--- Package.swift
+++ Package.swift
@@ -3,16 +3,16 @@
 
 import PackageDescription
 
-let version = "3.1.0-rc.1"
+let version = "3.1.0"
 
 let binaries = [
-"MapboxCoreMaps": "73cc825f796586a821583a8c8ad39f5ec1cefcc56b3aaf611a5197dbca32b967",
-"MapboxDirections": "adc51709e37c92e3e548189dcf53bb5c6472f9c9aee68150018cdd860611c26f",
-"MapboxMaps": "056c4e484f996ce6977dcf5e9dc785e029124b584f70e6678cac86348e9f60d5",
-"Turf": "8309276d8b47c17d6225c66f88f63571db76e03fb4f61cf22483b05046ee26d4",
-"MapboxNavigationCore": "499bf8fad13f4b8a9a1e44911405aecb2c7848d83cf5048c294e06f1550412a2",
-"_MapboxNavigationUXPrivate": "d67f4e9bd2a46c493fee22431eb6eaa7d432a5759b124f0ee62f346a19220000",
-"MapboxNavigationUIKit": "97bb238bdc313a67e5b858305304f9ecb34c54050e029afa86fbba7463b0820d",
+"MapboxCoreMaps": "c713512cd14460733742e1262d2cbdec87eb547b14dd2927d7b3fc3d74719844",
+"MapboxDirections": "2058a3d6e67de92dd678f3aefd996025d09c5c0d9139a541cda9f25f576d4b76",
+"MapboxMaps": "2bbff17cee17d458a72e8202e74f31a906daaf632834cf4a118ada68a9e2e0fb",
+"Turf": "aa80c52c0d4b5fe9c48947056bee69792c1594700208f8301583781826e87f70",
+"MapboxNavigationCore": "4910fa45d1643f1002a1e7e937c734b5db015ae5082cece73410d4a09050fab7",
+"_MapboxNavigationUXPrivate": "af7558a09b488ee215d0fd9e6d0316d889383219e8473fc41c82e9c00549b5b7",
+"MapboxNavigationUIKit": "c9b42fda558b5d86012314a46b489113cee6ffd61f088af5a3799e7732549652",
 ]
 
 let package = Package(
@@ -29,8 +29,8 @@ let package = Package(
         ),
     ],
     dependencies: [
-        .package(url: "https://github.com/mapbox/mapbox-common-ios.git", exact: "24.4.0-rc.2"),
-        .package(url: "https://github.com/mapbox/mapbox-navigation-native-ios.git", exact: "309.0.0"),
+        .package(url: "https://github.com/mapbox/mapbox-common-ios.git", exact: "24.4.0"),
+        .package(url: "https://github.com/mapbox/mapbox-navigation-native-ios.git", exact: "310.0.1"),
     ],
     targets: [
         .target(
