5398080afec2b38d0c5a0c83cd8215bbd859e93d
Dmitry Volk <dmitry.volk@mapbox.com>
2024-10-21 13:55:27 +0200

    [ios] Bump dependencies for 3.5.0-beta.1 (#7061)

--- Sources/MapboxNavigationCore/Version.swift
+++ Sources/MapboxNavigationCore/Version.swift
@@ -1,6 +1,6 @@
 import Foundation
 
 extension Bundle {
-    public static let mapboxNavigationVersion: String = "3.4.0"
+    public static let mapboxNavigationVersion: String = "3.5.0-beta.1"
     public static let mapboxNavigationUXBundleIdentifier: String = "com.mapbox.navigationUX"
 }
