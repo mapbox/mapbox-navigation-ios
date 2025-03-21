5fadca9097da2dd6ebc169e2270bf81622df83d7
Artem Stepuk <7859322+ArtemStepuk@users.noreply.github.com>
2025-03-21 12:32:32 +0300

    [iOS][Navigation] Design System module. (#8525)

--- Sources/_MapboxNavigationHelpers/UI/UIColor++.swift
+++ Sources/_MapboxNavigationHelpers/UI/UIColor++.swift
@@ -1,22 +1,6 @@
 import UIKit
 
 extension UIColor {
-    convenience init(red: Int, green: Int, blue: Int, alpha: CGFloat = 1.0) {
-        assert(red >= 0 && red <= 255, "Invalid red component")
-        assert(green >= 0 && green <= 255, "Invalid green component")
-        assert(blue >= 0 && blue <= 255, "Invalid blue component")
-
-        self.init(red: CGFloat(red) / 255.0, green: CGFloat(green) / 255.0, blue: CGFloat(blue) / 255.0, alpha: 1.0)
-    }
-
-    public convenience init(rgb: Int, alpha: CGFloat = 1.0) {
-        self.init(
-            red: (rgb >> 16) & 0xFF,
-            green: (rgb >> 8) & 0xFF,
-            blue: rgb & 0xFF
-        )
-    }
-
     public var hexString: String? {
         var r: CGFloat = 0
         var g: CGFloat = 0
