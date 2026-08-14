919aa74fc37d113e0d99b47fd193081216784ff9
mapbox-ci <no-reply@mapbox.com>
2026-08-14 03:19:09 +0000

    [skip ci][no ci] Bump snapshot versions `SNAPSHOT-08-14--02-02.git-a6761f7`
    GitOrigin-RevId: 5a657e7eed279df4d29a5029467d478962b53579

--- Sources/MapboxNavigationCore/Version.swift
+++ Sources/MapboxNavigationCore/Version.swift
@@ -1,6 +1,6 @@
 import Foundation
 
 extension Bundle {
-    public static let mapboxNavigationVersion: String = "3.30.0-SNAPSHOT-08-13--08-25.git-3335259"
+    public static let mapboxNavigationVersion: String = "3.30.0-SNAPSHOT-08-14--02-02.git-a6761f7"
     public static let mapboxNavigationUXBundleIdentifier: String = "com.mapbox.navigationUX"
 }
