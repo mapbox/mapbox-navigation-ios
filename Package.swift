06a69f64076f8f0b0ec1d20bc4607dde82c434f8
Viktor Kononov <viktor.kononov@mapbox.com>
2026-08-17 15:35:37 +0200

    synced version 0.30.0-alpha.2
    GitOrigin-RevId: 2620af4049b01b470b46550f8ed8a3c079798c4f

--- Package.swift
+++ Package.swift
@@ -4,9 +4,9 @@
 import PackageDescription
 import Foundation
 
-let navNativeVersion: Version = "324.30.0-SNAPSHOT-08-14--15-01.git-222a96b"
-let mapsVersion: Version = "11.30.0-SNAPSHOT-08-14--15-01.git-222a96b"
-let navsdkVersion: Version = "0.30.0-SNAPSHOT-08-14--15-01.git-222a96b"
+let navNativeVersion: Version = "324.30.0-alpha.2"
+let mapsVersion: Version = "11.30.0-alpha.2"
+let navsdkVersion: Version = "0.30.0-alpha.2"
 
 let mapboxMapsPackage = mapsVersion.description.contains("SNAPSHOT")
     ? "mapbox-maps-ios-binary"
