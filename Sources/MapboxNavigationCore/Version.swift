f1d3b91cb9a1f5bceac97753422ae9d6c88b7c06
Viktor Kononov <viktor.kononov@mapbox.com>
2026-08-18 18:05:31 +0200

    synced version to 0.30.0-alpha.5
    GitOrigin-RevId: 2afd8cb2b9ff5b79523149cd2fddeeebafaa30c1

--- Sources/MapboxNavigationCore/Version.swift
+++ Sources/MapboxNavigationCore/Version.swift
@@ -1,6 +1,6 @@
 import Foundation
 
 extension Bundle {
-    public static let mapboxNavigationVersion: String = "3.30.0-alpha.4"
+    public static let mapboxNavigationVersion: String = "3.30.0-alpha.5"
     public static let mapboxNavigationUXBundleIdentifier: String = "com.mapbox.navigationUX"
 }
