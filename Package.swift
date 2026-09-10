d6a0ea778bdb475de86daaa80e157b3f5fb71ba7
mapbox-ci <no-reply@mapbox.com>
2026-09-10 16:28:09 +0000

    [skip ci][no ci] Bump snapshot versions `SNAPSHOT-09-10--15-31.git-4deb98a`
    GitOrigin-RevId: af4c17bae4f53cffafe578b457949c41342d0927

--- Package.swift
+++ Package.swift
@@ -4,9 +4,9 @@
 import PackageDescription
 import Foundation
 
-let navNativeVersion: Version = "324.32.0-SNAPSHOT-09-10--09-52.git-096bbf3"
-let mapsVersion: Version = "11.32.0-SNAPSHOT-09-10--09-52.git-096bbf3"
-let navsdkVersion: Version = "0.32.0-SNAPSHOT-09-10--09-52.git-096bbf3"
+let navNativeVersion: Version = "324.32.0-SNAPSHOT-09-10--15-31.git-4deb98a"
+let mapsVersion: Version = "11.32.0-SNAPSHOT-09-10--15-31.git-4deb98a"
+let navsdkVersion: Version = "0.32.0-SNAPSHOT-09-10--15-31.git-4deb98a"
 
 let mapboxMapsPackage = mapsVersion.description.contains("SNAPSHOT")
     ? "mapbox-maps-ios-binary"
--- Sources/MapboxNavigationCore/Version.swift
+++ Sources/MapboxNavigationCore/Version.swift
@@ -1,6 +1,6 @@
 import Foundation
 
 extension Bundle {
-    public static let mapboxNavigationVersion: String = "3.32.0-SNAPSHOT-09-10--09-52.git-096bbf3"
+    public static let mapboxNavigationVersion: String = "3.32.0-SNAPSHOT-09-10--15-31.git-4deb98a"
     public static let mapboxNavigationUXBundleIdentifier: String = "com.mapbox.navigationUX"
 }
